8a53b536d75fc4ce693d12a3b1d4ebe1d389792b
moklick <info@moritzklack.com>
2023-06-21 17:14:38 +0200

    feat(styleguide): use storybook, share tailwind config

---
+++ apps/xy-styleguide/tailwind.config.js
@@ -0,0 +1,16 @@
+import xyTailwindConfig from 'xy-tailwind-config';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  ...xyTailwindConfig,
+  theme: {
+    container: xyTailwindConfig.theme.container,
+    extend: {
+      ...xyTailwindConfig.theme.extend,
+      fontFamily: {
+        sans: ['NTDapper'],
+        mono: ['var(--font-roboto-mono)'],
+      },
+    },
+  },
+};
