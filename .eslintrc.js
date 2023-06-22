4a0e4af95f082d32f3be5f4d8cd3f94ebe7f9ac3
Christopher Möller <chrtze@gmail.com>
2023-06-22 10:53:28 +0200

    fix(build): import correct eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `xy-eslint-config`
-  extends: ['custom'],
+  extends: ['xy-eslint-config'],
   settings: {
     next: {
       rootDir: ['apps/*/'],
