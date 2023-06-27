caa0c145bd637ffe64b8e9e0acf6ef316705f87c
moklick <info@moritzklack.com>
2023-06-27 16:06:02 +0200

    feat(docs): add pro example viewer

--- apps/docs/src/components/pro-example-viewer.tsx
+++ apps/docs/src/components/pro-example-viewer.tsx
@@ -1,7 +1,18 @@
-export default function (props) {
+import Link from 'next/link';
+
+const baseUrl = 'https://xyflow-pro-examples.vercel.app/';
+
+export default function ({ slug }: { slug: string }) {
   return (
-    <div className="bg-red-100">
-      Not implemented yet: Pro Example Viewer: {props.slug}
+    <div className="bg-react p-4">
+      <div className="flex justify-between items-center	mb-4">
+        This is a Pro example. If you are subscribed to React Flow Pro you can
+        access the annotated source code.
+        <Link href={`https://pro.xyflow.com/examples/${slug}`} target="_blank">
+          View Source Code
+        </Link>
+      </div>
+      <iframe src={`${baseUrl}${slug}`} className="h-[860px] w-full bg-white" />
     </div>
   );
 }
