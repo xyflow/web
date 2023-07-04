0132a522df544e7011a15dece4f59874dc231135
moklick <info@moritzklack.com>
2023-07-04 13:34:01 +0200

    feat(xy-ui): add list wrapper

--- packages/xy-ui/index.tsx
+++ packages/xy-ui/index.tsx
@@ -1,5 +1,6 @@
+export * from './components/ui/accordion';
 export * from './components/ui/button';
 export * from './components/ui/card';
 export * from './components/ui/container';
+export * from './components/ui/list-wrapper';
 export * from './components/ui/select';
-export * from './components/ui/accordion';
