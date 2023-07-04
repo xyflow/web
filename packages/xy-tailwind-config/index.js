cf43624c9c508932a46336b49d7e363daca0dfc1
moklick <info@moritzklack.com>
2023-07-04 13:24:05 +0200

    feat(xy-ui): add accordion

--- packages/xy-tailwind-config/index.js
+++ packages/xy-tailwind-config/index.js
@@ -70,6 +70,20 @@ export default {
         sans: ['var(--font-ntdapper)'],
         mono: ['var(--font-roboto-mono)'],
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
   plugins: [],
--- packages/xy-ui/index.tsx
+++ packages/xy-ui/index.tsx
@@ -2,3 +2,4 @@ export * from './components/ui/button';
 export * from './components/ui/card';
 export * from './components/ui/container';
 export * from './components/ui/select';
+export * from './components/ui/accordion';
