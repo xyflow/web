3e966dc79a7b0b1c3e926cdca79365d70b6e8396
Christopher Möller <chrtze@gmail.com>
2023-07-11 13:41:49 +0200

    fix(nhost): update serverless webhook

--- apps/nhost-backend/functions/stripe/webhook.ts
+++ apps/nhost-backend/functions/stripe/webhook.ts
@@ -24,32 +24,32 @@ export default async function stripeWebhookHandler(
     return res.status(405).send({ message: 'Method not allowed.' });
   }
 
-  //  const sig = req.headers['stripe-signature'] as string;
+  const sig = req.headers['stripe-signature'] as string;
 
   try {
-    // const event = stripe.webhooks.constructEvent(
-    //   req.rawBody,
-    //   sig,
-    //   endpointSecret
-    // );
+    const event = stripe.webhooks.constructEvent(
+      req.rawBody,
+      sig,
+      endpointSecret
+    );
 
-    // // if (event.type === 'checkout.session.completed') {
-    // //   const checkoutSession = event.data.object as Stripe.Checkout.Session;
+    // if (event.type === 'checkout.session.completed') {
+    //   const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
-    // //   const userId = checkoutSession.client_reference_id;
-    // //   const userEmail = checkoutSession.customer_email;
-    // //   const customerId = checkoutSession.customer;
+    //   const userId = checkoutSession.client_reference_id;
+    //   const userEmail = checkoutSession.customer_email;
+    //   const customerId = checkoutSession.customer;
 
-    // //   await updateSubscription({ userId, userEmail, customerId });
-    // //   return res.status(200).send('Subscription updated!');
-    // // }
-
-    // if (relevantEvents.has(event.type)) {
-    //   const stripeEvent = event.data.object as Stripe.Subscription;
-
-    //   console.log('WEBHOOK CALLED:', event.data);
-    //   await handleSubscriptionChange(stripeEvent);
+    //   await updateSubscription({ userId, userEmail, customerId });
+    //   return res.status(200).send('Subscription updated!');
     // }
+
+    if (relevantEvents.has(event.type)) {
+      const stripeEvent = event.data.object as Stripe.Subscription;
+
+      console.log('WEBHOOK CALLED:', event.data);
+      await handleSubscriptionChange(stripeEvent);
+    }
 
     return res.status(200).send();
   } catch (err) {
