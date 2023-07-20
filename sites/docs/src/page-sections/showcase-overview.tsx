1b82c73ece4c7569ce9651b2c3dab393030c534e
Hayleigh Thompson <me@hayleigh.dev>
2023-07-20 13:35:59 +0100

    :sparkles: Expand showcase component to display a carousel of projects.

--- sites/docs/src/page-sections/showcase-overview.tsx
+++ sites/docs/src/page-sections/showcase-overview.tsx
@@ -1,13 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useState, useCallback, useEffect, ReactNode } from "react";
+import {
+  Button,
+  Container,
+  Heading,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+  Text,
+  cn,
+} from "xy-ui";
 
-import { Button, Container, Heading, Text, cn } from 'xy-ui';
-import useXYSite from '@/hooks/use-xy-site';
+import useXYSite from "@/hooks/use-xy-site";
+
+// ShowcaseOverview ------------------------------------------------------------
+
+export type ShowcaseOverviewProps = {
+  className?: string;
+  items?: ShowcaseItem[];
+};
+
+type ShowcaseItem = {
+  name: string;
+  text: string;
+  content: ReactNode;
+};
 
 export default function ShowcaseOverview({
   className,
-}: {
-  className?: string;
-}) {
+  items = [],
+}: ShowcaseOverviewProps) {
   const { site } = useXYSite();
 
   return (
@@ -17,8 +40,8 @@ export default function ShowcaseOverview({
           <div>
             <Text
               className={cn(
-                'font-bold mb-2',
-                site !== 'xyflow' ? `text-${site}` : 'text-gray-300'
+                "font-bold mb-2",
+                site !== "xyflow" ? `text-${site}` : "text-gray-300"
               )}
             >
               Project Showcase
@@ -39,8 +62,139 @@ export default function ShowcaseOverview({
           </div>
         </div>
 
-        <div className="h-[600px]"></div>
+        <div className="mt-8">
+          {items.length && (
+            <ShowcaseOverviewItems
+              items={items}
+              delay={5000}
+              start={items[0].name}
+            />
+          )}
+        </div>
       </div>
     </Container>
+  );
+}
+
+// ShowcaseOverviewItems -------------------------------------------------------
+
+type ShowcaseOverviewItemsProps = {
+  items: ShowcaseItem[];
+  start?: string;
+  delay?: number;
+};
+
+const activeBarColours = {
+  xyflow: "from-xyflow/40 to-xyflow/70",
+  react: "from-react/40 to-react/70",
+  svelte: "from-svelte/40 to-svelte/70",
+};
+
+function ShowcaseOverviewItems({
+  items,
+  delay,
+  start,
+}: ShowcaseOverviewItemsProps) {
+  const [active, setActive] = useState(start ?? items[0].name);
+  const [delta, setDelta] = useState(0);
+  const [shouldCycle, setShouldCycle] = useState(Boolean(delay));
+  const onValueChange = useCallback((value) => {
+    setActive(value);
+    setDelta(0);
+    setShouldCycle(false);
+  }, []);
+  const activeBarWidth = shouldCycle ? (delta / delay) * 100 : 100;
+
+  useEffect(() => {
+    if (!shouldCycle) {
+      const resume = setTimeout(() => {
+        setShouldCycle(true);
+      }, 1000 * 10);
+
+      return () => clearTimeout(resume);
+    }
+
+    window.requestAnimationFrame(() => {
+      if (delta >= delay) {
+        const index = items.findIndex((item) => item.name === active);
+        const nextIndex = index === items.length - 1 ? 0 : index + 1;
+
+        setActive(items[nextIndex].name);
+        setDelta(0);
+      } else {
+        setDelta(delta + 16.66);
+      }
+    });
+  }, [active, delta, shouldCycle]);
+
+  return (
+    <Tabs value={active} onValueChange={onValueChange} className="relative">
+      <div className="relative mx-auto aspect-video">
+        {items.map((item, index) => (
+          <TabsContent key={index} forceMount value={item.name}>
+            <div
+              className={cn(
+                "transition duration-300 motion-reduce:transition-none",
+                active === item.name
+                  ? "ease-out opacity-100"
+                  : "ease-in opacity-0"
+              )}
+            >
+              {item.content}
+            </div>
+          </TabsContent>
+        ))}
+      </div>
+
+      <TabsList className="flex justify-around gap-8 mt-8 bg-transparent">
+        {items.map((item, index) => (
+          <TabsTrigger key={index} value={item.name} className="w-full group">
+            <TabsActiveBar
+              isActive={item.name === active}
+              activeWidth={activeBarWidth}
+            />
+
+            <div
+              className={cn(
+                "text-muted transition duration-300 motion-reduce:transition-none",
+                "group-hover:opacity-100",
+                active === item.name ? " opacity-100" : " opacity-70"
+              )}
+            >
+              <Text className="my-2 font-mono font-bold text-center">
+                {item.name}
+              </Text>
+              <Text className="text-muted">{item.text}</Text>
+            </div>
+          </TabsTrigger>
+        ))}
+      </TabsList>
+    </Tabs>
+  );
+}
+
+// TabsActiveBar ---------------------------------------------------------------
+
+type TabsActiveBarProps = {
+  isActive: boolean;
+  activeWidth: number;
+};
+
+function TabsActiveBar({ isActive, activeWidth }: TabsActiveBarProps) {
+  const { site } = useXYSite();
+
+  return (
+    <div className="relative mb-8 h-1.5 rounded">
+      <div className="absolute w-full h-full rounded bg-black/20" />
+      {isActive && (
+        <div
+          style={{ width: `${activeWidth}%` }}
+          className={cn(
+            "absolute h-full rounded bg-gradient-to-r",
+            activeBarColours[site]
+          )}
+        />
+      )}
+    </div>
   );
 }
--- sites/docs/src/sites/react-flow-home.tsx
+++ sites/docs/src/sites/react-flow-home.tsx
@@ -1,37 +1,77 @@
-import Link from 'next/link';
-import { useSSG } from 'nextra/ssg';
-import { SparklesIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import Image from "next/image";
+import { useSSG } from "nextra/ssg";
+import { SparklesIcon } from "@heroicons/react/24/outline";
 
-import { Button, Heading, Text } from 'xy-ui';
-import BaseLayout from '@/layouts/base';
-import HeroFlow from '@/page-sections/hero-flow';
-import Stats, { StatsDisplay } from '@/page-sections/stats';
-import Features from '@/page-sections/features';
-import Showcase from '@/page-sections/showcase-overview';
-import Section from '@/page-sections/section';
-import GettingStarted from '@/page-sections/getting-started';
-import ClientLogos from '@/components/client-logos';
+import { Button, Heading, Text } from "xy-ui";
+import BaseLayout from "@/layouts/base";
+import HeroFlow from "@/page-sections/hero-flow";
+import Stats, { StatsDisplay } from "@/page-sections/stats";
+import Features from "@/page-sections/features";
+import Showcase from "@/page-sections/showcase-overview";
+import Section from "@/page-sections/section";
+import GettingStarted from "@/page-sections/getting-started";
+import ClientLogos from "@/components/client-logos";
 
 const features = [
   {
-    title: 'Ready out-of-the-box',
-    text: 'The things you need are already there: dragging nodes, zooming, planning, selecting multiple nodes, and adding/removing edges are all built-in.',
-    route: '/react-flow/docs',
+    title: "Ready out-of-the-box",
+    text: "The things you need are already there: dragging nodes, zooming, planning, selecting multiple nodes, and adding/removing edges are all built-in.",
+    route: "/react-flow/docs",
   },
   {
-    title: 'Powered by us. Designed by you.',
-    text: 'We play nice with Tailwind and old CSS. React Flow nodes are just React components. Create custom nodes to add interactive controls.',
-    route: '/react-flow/docs',
+    title: "Powered by us. Designed by you.",
+    text: "We play nice with Tailwind and old CSS. React Flow nodes are just React components. Create custom nodes to add interactive controls.",
+    route: "/react-flow/docs",
   },
   {
-    title: 'Ready for business',
-    text: 'Build with confidence. React Flow is written entirely in TypeScript. A comprehensive suite of Cypress tests helps us never miss a bug.',
-    route: '/react-flow/docs',
+    title: "Ready for business",
+    text: "Build with confidence. React Flow is written entirely in TypeScript. A comprehensive suite of Cypress tests helps us never miss a bug.",
+    route: "/react-flow/docs",
   },
   {
-    title: 'All the right plugins',
-    text: 'Make more advanced apps. Background, Minimap, Controls, Panel, Nodetoolbar, and Noderesizer.',
-    route: '/react-flow/docs',
+    title: "All the right plugins",
+    text: "Make more advanced apps. Background, Minimap, Controls, Panel, Nodetoolbar, and Noderesizer.",
+    route: "/react-flow/docs",
+  },
+];
+
+const showcases = [
+  {
+    name: "Stripe Docs",
+    text: "Diagrams for process documentation with interactive nodes",
+    content: (
+      <Image
+        className="rounded-xl"
+        src="/img/showcase/5e7c4463-7dc9-40e9-b3df-7433f6fc9abc.png"
+        alt="Stripe Docs"
+        fill
+      />
+    ),
+  },
+  {
+    name: "DoubleLoop",
+    text: "Node-based dashboard builder to monitor business metrics",
+    content: (
+      <Image
+        className="rounded-xl"
+        src="/img/showcase/8b4e2243-4e88-40be-89b8-4bfad13460f8.png"
+        alt="DoubleLoop"
+        fill
+      />
+    ),
+  },
+  {
+    name: "TypeForm",
+    text: "Interactive tool to build and visualize complex survey logic",
+    content: (
+      <Image
+        className="rounded-xl"
+        src="/img/showcase/0e29d66b-142b-42c4-a43c-6e3f6501d14b.png"
+        alt="TypeForm"
+        fill
+      />
+    ),
   },
 ];
 
@@ -46,12 +86,12 @@ export default function ReactFlowHome() {
         <Stats
           variant="react"
           stats={[
-            { label: 'Github Stars', value: `${(stars / 1000).toFixed(1)}k` },
+            { label: "Github Stars", value: `${(stars / 1000).toFixed(1)}k` },
             {
-              label: 'Weekly Installs',
+              label: "Weekly Installs",
               value: `${(downloads / 1000).toFixed(0)}k`,
             },
-            { label: 'License', value: 'MIT' },
+            { label: "License", value: "MIT" },
           ]}
           description="React Flow is a MIT-licensed open source library. You can help us to ensure the further development and maintenance by subscribing to React Flow Pro."
           link="/react-flow/pro"
@@ -73,14 +113,14 @@ export default function ReactFlowHome() {
         <ClientLogos />
       </Section>
 
-      <Showcase />
+      <Showcase items={showcases} />
 
       <Section className="lg:flex place-content-between">
         <div>
           <Heading size="md" className="font-bold">
             Some headline thing
           </Heading>
-          <Text className="mb-4 mt-2">
+          <Text className="mt-2 mb-4">
             Here is a text about ethical standards and stuff
           </Text>
           <Button asChild>
@@ -89,13 +129,13 @@ export default function ReactFlowHome() {
         </div>
         <div className="grid grid-cols-2 mt-10 lg:mt-0">
           {[
-            { label: 'Github Stars', value: `${(stars / 1000).toFixed(1)}k` },
+            { label: "Github Stars", value: `${(stars / 1000).toFixed(1)}k` },
             {
-              label: 'Weekly Installs',
+              label: "Weekly Installs",
               value: `${(downloads / 1000).toFixed(0)}k`,
             },
-            { label: 'License', value: 'MIT' },
-            { label: 'License', value: 'MITs' },
+            { label: "License", value: "MIT" },
+            { label: "License", value: "MITs" },
           ].map((s) => (
             <StatsDisplay
               key={`${s.label}-${s.value}`}
