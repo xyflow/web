d25322e52255b37a482d08fbefab39389ac79c15
Hayleigh Thompson <me@hayleigh.dev>
2023-07-27 16:08:03 +0100

    :bug: Fixed bug where blog posts that included 'react-flow' in the slug used the wrong header.

--- sites/docs/src/hooks/use-xy-site.ts
+++ sites/docs/src/hooks/use-xy-site.ts
@@ -1,36 +1,37 @@
-import { useMemo } from 'react';
-import { useRouter } from 'next/router';
+import { useMemo } from "react";
+import { useRouter } from "next/router";
 
 export default function useXYSite(): {
-  site: 'react' | 'svelte' | 'xyflow';
-  lib: 'React Flow' | 'Svelte Flow' | null;
+  site: "react" | "svelte" | "xyflow";
+  lib: "React Flow" | "Svelte Flow" | null;
   isOrg: boolean;
 } {
   const router = useRouter();
 
   const site = useMemo(() => {
-    if (router.pathname.includes('react-flow')) {
-      return 'react';
-    } else if (router.pathname.includes('svelte-flow')) {
-      return 'svelte';
+    const segments = router.pathname.split("/");
+    if (segments.includes("react-flow")) {
+      return "react";
+    } else if (segments.includes("svelte-flow")) {
+      return "svelte";
     } else {
-      return 'xyflow';
+      return "xyflow";
     }
   }, [router.pathname]);
 
   const lib = useMemo(() => {
-    if (site === 'xyflow') {
+    if (site === "xyflow") {
       return null;
     }
 
     return `${site.charAt(0).toUpperCase() + site.slice(1)} Flow` as
-      | 'React Flow'
-      | 'Svelte Flow';
+      | "React Flow"
+      | "Svelte Flow";
   }, [site]);
 
   return {
     site,
     lib,
-    isOrg: site === 'xyflow',
+    isOrg: site === "xyflow",
   };
 }
