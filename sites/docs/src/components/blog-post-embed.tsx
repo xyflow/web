35efd8029cf3f19c2c813a7160e807c8a62deaa0
Hayleigh Thompson <me@hayleigh.dev>
2023-07-28 15:09:15 +0100

    :construction: Tweaks and style adjustments for blog post embed components.

--- sites/docs/src/components/blog-post-embed.tsx
+++ sites/docs/src/components/blog-post-embed.tsx
@@ -1,36 +1,57 @@
-// Nextra's markdown rendering is pretty nice, but for certain types of embeddable
+// Nextra's markdown rendering is pretty nice, but for certain types of YoutubeEmbeddable
 // content like images and our interactive code viewer, we'd like to style them
 // a bit differently.
 //
-// All of the embeds in this file are designed to be used in our blog posts and
+// All of the YoutubeEmbeds in this file are designed to be used in our blog posts and
 // reflect our figma designs: occupying more horizontal space than the text
 // content and having a rounded border.
 //
 
 import NxImage from "next/image";
 import XyCodeViewer from "@/components/code-viewer";
+import ReactPlayer from "@/components/react-player-lazy";
 
 // IMAGES ----------------------------------------------------------------------
 
-export type WideImageProps = {
+export type ImageProps = {
   src: string;
   alt: string;
+  width: number;
+  height: number;
+  wide?: boolean;
   caption?: string;
+  attribution?: string;
 };
 
-export function WideImage({ src, alt, caption }: ImagPropse) {
+export function Image({
+  src,
+  alt,
+  width = 0,
+  height = 0,
+  wide,
+  caption,
+  attribution,
+}: ImageProps) {
   return (
-    <figure className={`relative h-full my-8 mx-0 ${negativeMargin}`}>
+    <figure className={`my-8 mx-0 ${wide && negativeMargin}`}>
       <NxImage
         src={src}
         alt={alt}
         // Setting width/height to 0 means that the image we can use classes to
         // size it instead.
-        width={0}
-        height={0}
+        width={width}
+        height={height}
         sizes="100vw"
         className="w-full h-auto rounded-xl"
       />
+      {attribution && (
+        <a
+          href={attribution}
+          className="block mt-2 text-xs text-right text-gray-400"
+        >
+          source: {attribution}
+        </a>
+      )}
       {caption && (
         <figcaption className="mx-12 mt-2 font-semibold text-gray-400 sm:mx-auto sm:w-3/4 lg:w-1/2">
           {caption}
@@ -47,6 +68,7 @@ export type CodeViewerProps = {
   additionalFiles?: string[];
   showEditor?: boolean;
   dependencies?: Record<string, string>;
+  isTypescript?: boolean;
 };
 
 export function CodeViewer({
@@ -54,6 +76,7 @@ export function CodeViewer({
   additionalFiles = [],
   showEditor = false,
   dependencies = {},
+  isTypescript = false,
 }: CodeViewerProps) {
   return (
     <div className={`relative h-full mx-0 ${negativeMargin}`}>
@@ -68,23 +91,30 @@ export function CodeViewer({
         additionalFiles={additionalFiles}
         showEditor={showEditor}
         dependencies={dependencies}
+        isTypescript={isTypescript}
       />
     </div>
   );
 }
 
-// EMBED -----------------------------------------------------------------------
+// YoutubeEmbed -----------------------------------------------------------------------
 
-export type EmbedProps = {
-  src: string;
+export type YoutubeEmbedProps = {
+  id: string;
 };
 
-export function Embed({ src }: EmbedProps) {
+export function YoutubeEmbed({ id }: YoutubeEmbedProps) {
   return (
     <div
-      className={`relative h-full my-8 mx-0 ${negativeMargin} rounded-xl bg-gray-50`}
+      className={`relative h-full aspect-video my-8 mx-0 ${negativeMargin} rounded-xl bg-gray-50`}
     >
-      <iframe src={src} className="h-[50vh] w-full" />
+      <ReactPlayer
+        controls
+        url={`https://www.youtube.com/watch?v=${id}`}
+        width="100%"
+        height="100%"
+        style={{ position: "absolute", top: 0, left: 0 }}
+      />
     </div>
   );
 }
