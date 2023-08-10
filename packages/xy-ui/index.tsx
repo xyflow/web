4a951ce682897ca778833a32648fa7f4240c858c
Hayleigh Thompson <me@hayleigh.dev>
2023-08-10 12:12:43 +0100

    :heavy_plus_sign: Add @radix-ui/react-checkbox dependency and component.

--- packages/xy-ui/index.tsx
+++ packages/xy-ui/index.tsx
@@ -1,16 +1,17 @@
-export * from './components/ui/accordion';
-export * from './components/ui/button';
-export * from './components/ui/card';
-export * from './components/ui/container';
-export * from './components/ui/heading';
-export * from './components/ui/input';
-export * from './components/ui/label';
-export * from './components/ui/list-wrapper';
-export * from './components/ui/radio-group';
-export * from './components/ui/select';
-export * from './components/ui/slider';
-export * from './components/ui/text';
+export * from "./components/ui/accordion";
+export * from "./components/ui/button";
+export * from "./components/ui/card";
+export * from "./components/ui/container";
+export * from "./components/ui/heading";
+export * from "./components/ui/input";
+export * from "./components/ui/label";
+export * from "./components/ui/list-wrapper";
+export * from "./components/ui/radio-group";
+export * from "./components/ui/select";
+export * from "./components/ui/slider";
+export * from "./components/ui/text";
+export * from "./components/ui/checkbox";
 
-export { cn } from './lib/utils';
+export { cn } from "./lib/utils";
 
-export * from './types';
+export * from "./types";
