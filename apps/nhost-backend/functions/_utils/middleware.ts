e83efa3f1f5e2ac9f66b29b41b555d29b5137edd
Christopher Möller <chrtze@gmail.com>
2023-08-11 23:14:18 +0200

    fix(platform): write middleware as non-async function

--- apps/nhost-backend/functions/_utils/middleware.ts
+++ apps/nhost-backend/functions/_utils/middleware.ts
@@ -1,39 +1,31 @@
-// import { Request, Response } from 'express';
-// import { getUserIdFromAuthToken } from './jwt';
-
-// export const authPost = (fn: any) => {
-//   const handler = async (req: Request, res: Response) => {
-//     res.setHeader('Access-Control-Allow-Credentials', 'true');
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//       'Access-Control-Allow-Methods',
-//       'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-//     );
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
-//     );
-
-//     if (req.method === 'OPTIONS') {
-//       return res.status(200).end();
-//     }
-
-//     if (req.method !== 'POST') {
-//       return res.status(405).send({ message: 'Method not allowed.' });
-//     }
-
-//     const userId = getUserIdFromAuthToken(req.headers.authorization);
-
-//     if (!userId) {
-//       return res.status(401).send({ message: 'Unauthorized.' });
-//     }
-
-//     return await fn(req, res, { userId });
-//   };
-
-//   return handler;
-// };
+import { Request, Response } from 'express';
+import { getUserIdFromAuthToken } from './jwt';
 
 export const authPost = (fn: any) => (req: Request, res: Response) => {
-  fn(req, res, { userId: 'userId' });
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Method not allowed.' });
+  }
+
+  const userId = getUserIdFromAuthToken(req.headers.authorization);
+
+  if (!userId) {
+    return res.status(401).send({ message: 'Unauthorized.' });
+  }
+
+  fn(req, res, { userId });
 };
