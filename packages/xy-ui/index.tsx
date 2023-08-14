0f12b8f75f0dbdf07b2f032559b14f3a1b47adc4
Christopher Möller <chrtze@gmail.com>
2023-08-14 13:11:08 +0200

    feat(xy-ui): add alert-dialog component (#47)
    * feat(xy-ui): add alert dialog component
    
    * feat(xy-ui): add alert-dialog component

--- packages/xy-ui/index.tsx
+++ packages/xy-ui/index.tsx
@@ -1,4 +1,5 @@
 export * from './components/ui/accordion';
+export * from './components/ui/alert-dialog';
 export * from './components/ui/button';
 export * from './components/ui/card';
 export * from './components/ui/container';
