2fbf359d548716bb867e8614daa9e0594de0ab1e
John Robb <johnhrobb@gmail.com>
2023-08-23 15:52:30 +0200

    Update section.tsx

--- sites/docs/src/page-sections/section.tsx
+++ sites/docs/src/page-sections/section.tsx
@@ -1,12 +1,12 @@
 import { type ReactNode } from 'react';
 import { cn } from 'xy-ui';
 
-type TextSectionProps = {
+type SectionProps = {
   children: ReactNode;
   className?: string;
 };
 
-export default function Section({ children, className }: TextSectionProps) {
+export default function Section({ children, className }: SectionProps) {
   return (
     <div className={cn('px-2 lg:px-4 my-16 lg:my-24', className)}>
       {children}
