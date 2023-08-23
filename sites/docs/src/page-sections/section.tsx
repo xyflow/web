22019c606fd513949a547738c990d7c117ce38e8
johnrobbjr <johnhrobb@gmail.com>
2023-08-23 11:34:22 +0200

    chore(text): change copy and style text to be more readable

--- sites/docs/src/page-sections/section.tsx
+++ sites/docs/src/page-sections/section.tsx
@@ -1,12 +1,12 @@
 import { type ReactNode } from 'react';
 import { cn } from 'xy-ui';
 
-type SectionProps = {
+type TextSectionProps = {
   children: ReactNode;
   className?: string;
 };
 
-export default function Section({ children, className }: SectionProps) {
+export default function Section({ children, className }: TextSectionProps) {
   return (
     <div className={cn('px-2 lg:px-4 my-16 lg:my-24', className)}>
       {children}
--- sites/docs/src/sites/open-source.tsx
+++ sites/docs/src/sites/open-source.tsx
@@ -1,6 +1,8 @@
 import { UserGroupIcon } from '@heroicons/react/24/outline';
 
 import { Heading, Text } from 'xy-ui';
+import Link from 'next/link';
+import Image from 'next/image';
 import BaseLayout from '@/layouts/base';
 import Hero from '@/page-sections/hero';
 import Section from '@/page-sections/section';
@@ -11,29 +13,57 @@ export default function OpenSource() {
       <Hero
         kicker="Open Source"
         kickerIcon={UserGroupIcon}
-        title="Contribute and help grow React Flow and Svelte Flow"
-        subtitle="React Flow was published under an MIT License in 2019."
+        title="How we do open source"
+        // subtitle="React Flow was published under an MIT License in 2019."
         align="center"
       />
 
-      <Section>
-        <Heading as="h2" size="sm">
-          We’ll keep our software MIT Licensed forever.
+      <Section className="max-w-screen-md mx-auto">
+        <Text className="text-lg leading-8">
+          We decided to open source the React Flow library in 2019. We started
+          seeing our first users, receiving lots of bug reports and
+          contributions early on, which allowed us to grow. More and more
+          companies and people started to use it, which led to more maintenance
+          work, to the point that we decided to shift our full attention to the
+          library in 2021. A few months later, we set up a business model to
+          support our ongoing work on the library. Now we’re happy maintainers
+          of one of the most popular node-based UI libraries :)
+        </Text>
+
+        <Heading as="h2" size="sm" className="mb-4 lg:mt-14">
+          MIT License
         </Heading>
-        <Text>
+        <Text className="text-lg leading-8 mb-4">
           We chose the MIT License because we believe in Free and Open Source
           Software. Having this license means that anyone can use, repurpose, or
           resell our docs, our blog posts, or our library. We love that it
           allows anyone to use xyFlow for their own projects, and we get to see
           people build things with xyFlow that we never would have imagined. The
           MIT License also allows companies to easily use the library without
-          having to hire a lawyer to interpret a custom license.
+          having to hire a lawyer to interpret a custom license. We’ll keep our
+          software MIT Licensed forever.
         </Text>
+        <Link href="" className="text-pink-500 underline text-lg">
+          Read our license
+        </Link>
 
-        <Heading as="h2" size="sm" className="mt-10 lg:mt-14">
-          We need funding to pay for the time it takes to care
+        <Heading as="h2" size="sm" className="mb-4 lg:mt-14">
+          Funding
         </Heading>
-        <Text>
+        <Text className="text-lg leading-8">
+          We need funding to pay for the time it takes to care for xyFlow and
+          the infrastructure around it. The way we do this is a thin-crust
+          open-core model:
+        </Text>
+        <Image
+          src="/img/open-source/thincrustopencore.png"
+          alt=""
+          width={500}
+          height={500}
+          className="my-10"
+        ></Image>
+
+        <Text className="text-lg leading-8 mb-4">
           We spend most of our time on the “core,” which is MIT Licensed
           (library, docs, github discussions, discord). We spend less time on
           the “crust,” which is the paid content and services (subscriber
@@ -43,6 +73,32 @@ export default function OpenSource() {
           everyone, and the financial burden of the library and ecosystem around
           it is placed on organizations who can afford to fund us (rather than
           individual developers).
+        </Text>
+        <Link href="" className="text-pink-500 underline text-lg">
+          Read about xyflow is funded
+        </Link>
+        <Heading as="h2" size="sm" className="mb-4 lg:mt-14">
+          Open Source Ecosystem
+        </Heading>
+        <Text className="text-lg leading-8 mb-4">
+          We give back to the Open Source projects that we use ourselves on{' '}
+          <Link
+            href="https://opencollective.com/webkid"
+            className="text-pink-500"
+          >
+            Open Collective
+          </Link>{' '}
+          and{' '}
+          <Link href="https://github.com/wbkd" className="text-pink-500">
+            Github
+          </Link>
+          .
+        </Text>
+        <Text className="text-lg leading-8 mb-4">
+          We offer free access to our Pro examples to non-commercial open source
+          projects and students who use xyFlow. If this is you, send us an email
+          to info@xyflow.dev from your student email or with a link to your open
+          source project repository.
         </Text>
       </Section>
     </BaseLayout>
