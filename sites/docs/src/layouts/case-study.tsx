19b0d510a1e0184138ff2f9b7c5568abbaecbd8c
johnrobbjr <johnhrobb@gmail.com>
2023-08-30 12:47:05 +0200

    fix spacing in case study header

--- sites/docs/src/layouts/case-study.tsx
+++ sites/docs/src/layouts/case-study.tsx
@@ -17,17 +17,20 @@ export default function CaseStudyLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <div className="max-w-3xl mx-auto">
-        <div className="flex mt-8 items-end">
+        <div className="flex mt-16 items-end">
           <Link href="." className="mr-1 text-md text-gray-500 font-normal">
             Case Studies
           </Link>
           <ChevronRightIcon className="h-5 w-5 mb-0.5 text-gray-500" />
           <Text className="ml-1 text-md font-medium">{frontMatter.client}</Text>
         </div>
-        <Heading size="lg" className="mt-16">
+        <Heading
+          size="lg"
+          className="mt-8 !leading-tight text-gray-900 text-[64px]"
+        >
           {frontMatter.title}
         </Heading>
-        <AuthorList authors={frontMatter.authors} className="mt-10" />
+        <AuthorList authors={frontMatter.authors} className="mt-6" />
       </div>
 
       <Image
