14633ab327e0eb21c78cbef1483791f06e7d4b86
Christopher Möller <chrtze@gmail.com>
2023-09-13 13:28:39 +0200

    fix(platform): fix ts build error

--- sites/pro-platform/src/components/Notification/not-subscribed.tsx
+++ sites/pro-platform/src/components/Notification/not-subscribed.tsx
@@ -1,5 +1,12 @@
-import Notification, { type NotificationProps } from '.';
+import Notification from '@/components/Notification';
 import { NotSubscribed } from '@/components/SubscriptionStatus';
+
+// @todo this should be imported from '.' but is causing a build error, not sure why
+export type NotificationProps = {
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  button?: { label: string; href: string };
+} & React.HTMLAttributes<HTMLDivElement>;
 
 export default function (props: NotificationProps) {
   return (
