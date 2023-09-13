630517475f082385ee4712b70e8641581458aad6
Hayleigh Thompson <me@hayleigh.dev>
2023-09-13 09:48:51 +0100

    :memo: Use the props table to document the Node type.

---
+++ sites/docs/src/pages/react-flow/api/types/Node.fields.ts
@@ -0,0 +1,33 @@
+import { type PropsTableProps } from '@/components/props-table';
+
+export const nodeFields: PropsTableProps = {
+  props: [
+    { name: 'id', type: 'string' },
+    { name: 'position', type: 'XYPosition' },
+    { name: 'data', type: 'T' },
+    { name: 'type', type: 'U' },
+    { name: 'sourcePosition', type: 'Position' },
+    { name: 'targetPosition', type: 'Position' },
+    { name: 'hidden', type: 'boolean' },
+    { name: 'selected', type: 'boolean' },
+    { name: 'dragging', type: 'boolean' },
+    { name: 'draggable', type: 'boolean' },
+    { name: 'selectable', type: 'boolean' },
+    { name: 'connectable', type: 'boolean' },
+    { name: 'resizing', type: 'boolean' },
+    { name: 'deletable', type: 'boolean' },
+    { name: 'dragHandle', type: 'string' },
+    { name: 'width', type: 'number | null' },
+    { name: 'height', type: 'number | null' },
+    { name: 'parentNode', type: 'string' },
+    { name: 'zIndex', type: 'number' },
+    { name: 'extent', type: '"parent" | CoordinateExtent' },
+    { name: 'expandParent', type: 'boolean' },
+    { name: 'positionAbsolute', type: 'XYPosition' },
+    { name: 'ariaLabel', type: 'string' },
+    { name: 'focusable', type: 'boolean' },
+    { name: 'origin', type: 'NodeOrigin' },
+    { name: 'style', type: 'CSSProperties' },
+    { name: 'className', type: 'string' },
+  ],
+};
