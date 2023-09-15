261d523ab12fcbf22875d660f7f25f444964d344
Hayleigh Thompson <me@hayleigh.dev>
2023-09-15 10:38:54 +0100

    :memo: Add links to github for all the things.

--- sites/docs/src/page-data/react-flow/api/types/Edge.fields.ts
+++ sites/docs/src/page-data/react-flow/api/types/Edge.fields.ts
@@ -1,31 +1,52 @@
 import { type PropsTableProps } from '@/components/props-table';
 
-const links = {
-  ProOptions: '#prooptions',
-  OnConnectStartParams: '#onconnectstartparams',
-  OnSelectionChangeParams: '#onselectionchangeparams',
-};
-
-export const edgeFields: PropsTableProps = {
+export const defaultEdgeFields: PropsTableProps = {
   props: [
     { name: 'id', type: 'string' },
-    { name: 'type?', type: 'string' },
+    { name: 'type', type: 'string' },
+    { name: 'style?', type: 'React.CSSProperties' },
+    { name: 'className?', type: 'string' },
     { name: 'source', type: 'string' },
     { name: 'target', type: 'string' },
-    { name: 'sourceHandle?', type: 'string | null' },
-    { name: 'targetHandle?', type: 'string | null' },
-    { name: 'animated?', type: 'boolean' },
-    { name: 'hidden?', type: 'boolean' },
-    { name: 'deletable?', type: 'boolean' },
-    { name: 'selectable?', type: 'boolean' },
-    { name: 'data?', type: 'EdgeData' },
-    { name: 'selected?', type: 'boolean' },
-    { name: 'markerStart?', type: 'string | EdgeMarker' },
-    { name: 'markerEnd?', type: 'string | EdgeMarker' },
+    { name: 'sourceNode?', type: 'Node' },
+    { name: 'targetNode?', type: 'Node' },
+    { name: 'sourceHandle', type: 'string | null' },
+    { name: 'targetHandle', type: 'string | null' },
+    { name: 'data', type: 'T' },
+    { name: 'hidden', type: 'boolean' },
+    { name: 'animated', type: 'boolean' },
+    { name: 'selected', type: 'boolean' },
+    { name: 'deletable', type: 'boolean' },
+    { name: 'selectable', type: 'boolean' },
+    { name: 'focusable', type: 'boolean' },
+    { name: 'updatable?', type: 'boolean | "source" | "target"' },
+    { name: 'markerStart', type: 'string | EdgeMarker' },
+    { name: 'markerEnd', type: 'string | EdgeMarker' },
     { name: 'zIndex?', type: 'number' },
-    { name: 'ariaLabel?', type: 'string' },
-    { name: 'interactionWidth?', type: 'number' },
-    { name: 'focusable?', type: 'boolean' },
+    { name: 'interactionWidth', type: 'number' },
+    { name: 'ariaLabel', type: 'string' },
+    { name: 'label?', type: 'string | React.ReactNode' },
+    { name: 'labelStyle?', type: 'React.CSSProperties' },
+    { name: 'labelShowBg?', type: 'boolean' },
+    { name: 'labelBgStyle?', type: 'React.CSSProperties' },
+    { name: 'labelBgPadding?', type: '[number, number]' },
+    { name: 'labelBgBorderRadius?', type: 'number' },
   ],
-  links: links,
+};
+
+export const smoothStepEdgeTypeFields: PropsTableProps = {
+  props: [
+    { name: 'type', type: '"smoothstep"' },
+    { name: 'pathOptions?', type: 'object' },
+    { name: 'pathOptions.offset?', type: 'number' },
+    { name: 'pathOptions.borderRadius?', type: 'number' },
+  ],
+};
+
+export const bezierEdgeTypeFields: PropsTableProps = {
+  props: [
+    { name: 'type', type: '"default"' },
+    { name: 'pathOptions?', type: 'object' },
+    { name: 'pathOptions.curvature?', type: 'number' },
+  ],
 };
