3b6b12eacd0893f0a17570cde9092aad3468d5fd
Christopher Möller <chrtze@gmail.com>
2023-09-19 13:15:22 +0200

    feat(packages): move pricing table into xy-ui package (#79)
    * feat(packages): move pricing table into xy-ui package
    
    * fix(packages): import type correctly
    
    * fix(docs): remove newsletter signup component

---
+++ apps/xy-styleguide/src/stories/PricingTable.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { PricingTable } from 'xy-ui';
+
+const meta = {
+  title: 'Example/PricingTable',
+  component: PricingTable,
+  tags: ['autodocs'],
+} satisfies Meta<typeof PricingTable>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'Default Pricing Table',
+  render: () => <PricingTable />,
+};
