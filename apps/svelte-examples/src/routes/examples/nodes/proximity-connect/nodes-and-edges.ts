3595ab82756faef064207f82f86b2655293df2a9
Peter <p.korcsak@googlemail.com>
2023-09-26 12:05:11 +0200

    fixed proximity connect example

--- apps/svelte-examples/src/routes/examples/nodes/proximity-connect/nodes-and-edges.ts
+++ apps/svelte-examples/src/routes/examples/nodes/proximity-connect/nodes-and-edges.ts
@@ -41,14 +41,14 @@ export const initialNodes: Node[] = [
 ];
 
 export const initialEdges: Edge[] = [
-  // {
-  //   id: 'e1-2',
-  //   source: '1',
-  //   target: '2'
-  // },
-  // {
-  //   id: 'e1-3',
-  //   source: '1',
-  //   target: '3'
-  // }
+  {
+    id: 'e1-2',
+    source: '1',
+    target: '2'
+  },
+  {
+    id: 'e1-3',
+    source: '1',
+    target: '3'
+  }
 ];
