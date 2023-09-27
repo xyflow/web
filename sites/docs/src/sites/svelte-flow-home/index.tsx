0d3c5e33fe113b97affd25383a411df16a849d9b
johnrobbjr <johnhrobb@gmail.com>
2023-09-27 11:34:19 +0200

    add svelte pre-footer element to svelte landing page

--- sites/docs/src/sites/svelte-flow-home/index.tsx
+++ sites/docs/src/sites/svelte-flow-home/index.tsx
@@ -1,5 +1,16 @@
-import { HeartIcon } from '@heroicons/react/24/outline';
+import { type ReactNode } from 'react';
+import Link from 'next/link';
+import {
+  HeartIcon,
+  AtSymbolIcon,
+  MusicalNoteIcon,
+  PuzzlePieceIcon,
+  CodeBracketIcon,
+  RocketLaunchIcon,
+} from '@heroicons/react/24/outline';
+import { ArrowRightCircleIcon } from '@heroicons/react/24/solid';
 
+import { Text, HeroIcon, ListWrapper } from 'xy-ui';
 import BaseLayout from '@/layouts/base';
 import HeroFlow from '@/page-sections/hero-flow';
 import Features from '@/page-sections/features';
@@ -69,6 +80,45 @@ export default function SvelteFlowHome() {
       <Section>
         <Features features={features} variant="svelte" />
       </Section>
+      <Section className="mb-8 lg:mb-8">
+        <ListWrapper
+          className="mt-32 max-w-lg mx-auto"
+          icon={RocketLaunchIcon}
+          iconClassName="text-svelte"
+          title="Let's get you started"
+        >
+          <ListItem
+            icon={CodeBracketIcon}
+            label="Getting Started Guide"
+            link="/"
+          />
+
+          <ListItem icon={AtSymbolIcon} label="Contact us" link="/" />
+        </ListWrapper>
+      </Section>
     </BaseLayout>
+  );
+}
+
+function ListItem({
+  label,
+  icon: Icon,
+  link,
+}: {
+  label: ReactNode;
+  icon: HeroIcon;
+  link?: string;
+}) {
+  const WrapperComponent = link ? Link : 'div';
+
+  return (
+    <WrapperComponent
+      href={link}
+      className="flex py-6 border-b border-solid border-gray-100"
+    >
+      <Icon className="w-6 h-6 mr-2" />
+      <Text className="font-bold">{label}</Text>
+      {link && <ArrowRightCircleIcon className="text-svelte w-6 h-6 ml-auto" />}
+    </WrapperComponent>
   );
 }
