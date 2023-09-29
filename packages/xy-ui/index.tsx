e7346843e66188b3e9b3c6ccd6281b16fd8d47e9
Hayleigh Thompson <me@hayleigh.dev>
2023-09-30 00:36:44 +0100

    :recycle: Move emoji component into xy-ui.

--- packages/xy-ui/index.tsx
+++ packages/xy-ui/index.tsx
@@ -3,11 +3,14 @@ export * from './components/ui/alert-dialog';
 export * from './components/ui/alert';
 export * from './components/ui/button';
 export * from './components/ui/card';
+export * from './components/ui/checkbox';
 export * from './components/ui/container';
+export * from './components/ui/emoji';
 export * from './components/ui/heading';
 export * from './components/ui/input';
 export * from './components/ui/label';
 export * from './components/ui/list-wrapper';
+export * from './components/ui/logo';
 export * from './components/ui/popover';
 export * from './components/ui/radio-group';
 export * from './components/ui/select';
@@ -15,11 +18,9 @@ export * from './components/ui/slider';
 export * from './components/ui/spinner';
 export * from './components/ui/tabs';
 export * from './components/ui/text';
-export * from './components/ui/checkbox';
-export * from './components/ui/logo';
-export * from './components/widgets/pricing-table';
-export * from './components/widgets/footer';
 export * from './components/widgets/code-viewer';
+export * from './components/widgets/footer';
+export * from './components/widgets/pricing-table';
 
 export { cn } from './lib/utils';
 
