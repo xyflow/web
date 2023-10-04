1ed930d02eeaf1ae298db45e36b5a33026e3dfad
Peter <p.korcsak@googlemail.com>
2023-10-04 16:02:28 +0200

    fix(svelte) finished easy-connect example

--- apps/svelte-examples/src/routes/examples/nodes/easy-connect/CustomNode.svelte
+++ apps/svelte-examples/src/routes/examples/nodes/easy-connect/CustomNode.svelte
@@ -27,7 +27,7 @@
     <!-- If handles are conditionally rendered and not present initially, you need to update the node internals https://reactflow.dev/docs/api/hooks/use-update-node-internals/
     In this case we don't need to use useUpdateNodeInternals, since !isConnecting is true at the beginning and all handles are rendered initially. -->
     {#if !isConnecting}
-      <Handle class="customHandle" position={Position.Right} type="source" />
+      <Handle class="customHandle" position={Position.Right} type="source" style="z-index: 1;" />
     {/if}
     <!-- <Handle class="customHandle" position={Position.Right} type="source" /> -->
 
