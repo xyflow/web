4d024f84c3f58f35744c1c2929c1e1b257c1c89b
Peter <p.korcsak@googlemail.com>
2023-10-05 12:31:58 +0200

    added connection-limit example

---
+++ apps/svelte-examples/src/routes/examples/nodes/connection-limit/CustomNode.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Position, type NodeProps } from '@xyflow/svelte';
+  import CustomHandle from './CustomHandle.svelte';
+  type $$Props = NodeProps;
+</script>
+
+<div class="customNode">
+  <CustomHandle type="target" position={Position.Left} />
+  <div>Connection Limit 1</div>
+</div>
+
+<style>
+  .customNode {
+    padding: 16px;
+    border: 1px solid black;
+  }
+</style>
