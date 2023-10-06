75bbe23d6a30620cf34208e22dae8774ae374b5e
Hayleigh Thompson <me@hayleigh.dev>
2023-10-06 15:01:01 +0100

    :recycle: Move showcase layout and hero component into xy-ui.

---
+++ packages/xy-ui/components/layout/showcase.tsx
@@ -0,0 +1,78 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {
+  BaseLayout,
+  ContentGrid,
+  ContentGridItem,
+  Hero,
+  ProjectPreview,
+} from '../../';
+
+export type ShowcaseLayoutProps = {
+  title: string;
+  subtitle: string;
+  showcases?: ShowcaseItem[];
+  footer?: React.ReactNode;
+};
+
+export type ShowcaseItem = {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+  tags: { id: string; name: string }[];
+};
+
+export function ShowcaseLayout({
+  title,
+  subtitle,
+  showcases = [],
+  footer,
+}: ShowcaseLayoutProps) {
+  return (
+    <BaseLayout>
+      <Hero
+        kicker="Showcase"
+        kickerIcon={MagnifyingGlassIcon}
+        title={title}
+        subtitle={subtitle}
+        align="center"
+      />
+
+      <ContentGrid className="mt-20">
+        {showcases.map((showcase) => (
+          <ContentGridItem key={showcase.id} route={showcase.url}>
+            <ProjectPreview
+              image={`/img/showcase/${showcase.image}`}
+              title={showcase.title}
+              subtitle={
+                <>
+                  {showcase.tags.map((tag) => (
+                    <span key={tag.id} className="mr-2">
+                      {tag.name}
+                    </span>
+                  ))}
+                </>
+              }
+              description={showcase.description}
+              linkLabel="Website"
+            />
+          </ContentGridItem>
+        ))}
+
+        <ContentGridItem
+          route="https://github.com/xyflow/web/issues/new"
+          className={showcases.length % 2 === 0 ? 'lg:col-span-2' : ''}
+        >
+          <ProjectPreview
+            title="Your project here?"
+            description="Have you built something exciting you want to show off? We want to feature it here!"
+            linkLabel="Open an issue on GitHub"
+          />
+        </ContentGridItem>
+      </ContentGrid>
+
+      {footer}
+    </BaseLayout>
+  );
+}
