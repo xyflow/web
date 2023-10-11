9d936539b62734508dfcede8c92b98fd0d121458
Christopher Möller <chrtze@gmail.com>
2023-10-11 14:04:41 +0200

    refactor(styling): adjust getting started widget

--- packages/xy-ui/components/widgets/getting-started/app-window.tsx
+++ packages/xy-ui/components/widgets/getting-started/app-window.tsx
@@ -6,13 +6,13 @@ type AppWindowProps = {
 
 function AppWindow({ children }: AppWindowProps) {
   return (
-    <div className="border border-solid border-gray-600 rounded-md max-w-3xl mx-auto bg-black">
+    <div className="border border-solid border-gray-700 rounded-md max-w-3xl mx-auto bg-black/90">
       <div className="flex p-4">
-        <div className="w-3 h-3 bg-red-500 rounded-full mr-2" />
-        <div className="w-3 h-3 bg-yellow-500 rounded-full mr-2" />
-        <div className="w-3 h-3 bg-green-500 rounded-full mr-2" />
+        <div className="w-3 h-3 bg-red-400 rounded-full mr-2" />
+        <div className="w-3 h-3 bg-yellow-400 rounded-full mr-2" />
+        <div className="w-3 h-3 bg-green-400 rounded-full mr-2" />
       </div>
-      <div className="px-4 py-2">{children}</div>
+      <div className="px-4 pl-8 pb-6 py-2">{children}</div>
     </div>
   );
 }
--- packages/xy-ui/components/widgets/getting-started/index.tsx
+++ packages/xy-ui/components/widgets/getting-started/index.tsx
@@ -2,6 +2,10 @@ import Link from 'next/link';
 
 import { Button, Text, Container, cn } from '../../..';
 import { AppWindow } from './app-window';
+import {
+  ArrowLongRightIcon,
+  ArrowRightIcon,
+} from '@heroicons/react/24/outline';
 
 type GettingStartedProps = {
   libraryName: string;
@@ -11,26 +15,29 @@ type GettingStartedProps = {
 function GettingStarted({ libraryName, packageName }: GettingStartedProps) {
   return (
     <Container variant="dark" className="mt-16 lg:mt-24">
-      <div className="p-14">
+      <div className="pt-14">
         <Text size="lg" className="font-bold mb-2 text-center text-primary">
           Getting Started with {libraryName}
         </Text>
         <Text className="text-center max-w-lg mx-auto mb-6 lg:mb-8">
-          Make sure you’ve installed npm, npmn or yarn. Then you can install
+          Make sure you’ve installed npm, pnpm or yarn. Then you can install{' '}
           {libraryName} via:
         </Text>
 
         <AppWindow>
           <pre className="!pl-0">
-            <code className="inline text-lg">
+            <code className="inline text-md">
               npm install <span className="text-primary">{packageName}</span>
             </code>
           </pre>
         </AppWindow>
 
-        <div className={cn('flex justify-center mt-10 ')}>
-          <Button variant="secondary" className="text-primary" asChild>
-            <Link href={`/learn`}>Get Started</Link>
+        <div className="flex justify-center mt-5 py-3 bg-black/90 border-t border-gray-700">
+          <Button variant="link" className="text-white font-bold" asChild>
+            <Link href={`/learn`}>
+              See full Quickstart guide{' '}
+              <ArrowLongRightIcon className="ml-2 w-4 h-4 stroke-2" />
+            </Link>
           </Button>
         </div>
       </div>
