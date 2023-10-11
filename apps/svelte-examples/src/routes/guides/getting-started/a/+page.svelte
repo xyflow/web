56ab4538e0724376afbd69717e325103f08f2aaa
moklick <info@moritzklack.com>
2023-10-11 15:44:08 +0200

    docs(svelte): add getting started

---
+++ apps/svelte-examples/src/routes/guides/getting-started/a/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { writable } from 'svelte/store';
+  import { SvelteFlow, Background, Controls } from '@xyflow/svelte';
+
+  import '@xyflow/svelte/dist/style.css';
+
+  const nodes = writable([]);
+  const edges = writable([]);
+</script>
+
+<main>
+  <SvelteFlow {nodes} {edges}>
+    <Background />
+    <Controls />
+  </SvelteFlow>
+</main>
+
+<style>
+  main {
+    height: 100vh;
+  }
+</style>
