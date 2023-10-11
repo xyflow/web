de0f859a13892c2131b402c3941051c2b4bd5a12
Hayleigh Thompson <me@hayleigh.dev>
2023-10-11 14:40:08 +0100

    :sparkles: Link to tutorials page from the home page.

--- sites/reactflow.dev/src/sites/tutorials.tsx
+++ sites/reactflow.dev/src/sites/tutorials.tsx
@@ -14,19 +14,19 @@ export default function CaseStudies() {
     <BaseLayout>
       <Hero
         title="Learn more with tutorials"
-        subtitle="Our tutorials provide step-by-step instructions for building MVP appds like mindmaps and audio playgrounds."
+        subtitle="Our tutorials provide step-by-step instructions for building MVP apps like mindmaps and audio playgrounds."
         kicker="Tutorials"
         kickerIcon={BookOpenIcon}
         align="center"
       />
       <ContentGrid className="mt-20">
-        {getMdxPagesUnderRoute('/tutorials').map((page) => {
+        {getMdxPagesUnderRoute('/learn/tutorials').map((page) => {
           return (
             <ContentGridItem key={page.route} route={page.route}>
               <ProjectPreview
                 image={page.frontMatter.image}
                 title={page.frontMatter?.title}
-                description={page.frontMatter?.description}
+                description={page.frontMatter?.intro}
                 authors={page.frontMatter?.authors}
                 kicker={page.frontMatter?.client}
               />
