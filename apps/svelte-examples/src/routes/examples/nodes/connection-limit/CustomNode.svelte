6a5e3c87b4edacbef0948b7f575264b525081ee9
Peter <p.korcsak@googlemail.com>
2023-10-12 16:44:21 +0200

    chore(svelte) fixed connectionLimit example

--- apps/svelte-examples/src/routes/examples/nodes/connection-limit/CustomNode.svelte
+++ apps/svelte-examples/src/routes/examples/nodes/connection-limit/CustomNode.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-  import { Position, type NodeProps } from '@xyflow/svelte';
-  import CustomHandle from './CustomHandle.svelte';
+  import { Position, type NodeProps, Handle, useEdges } from '@xyflow/svelte';
   type $$Props = NodeProps;
 
   export let id: $$Props['id'];
+
+  const edges = useEdges();
+  let isConnectable = true;
+
+  $: isConnectable = $edges.filter((edge) => edge.target === id || edge.source === id).length < 1;
 </script>
 
 <div class="customNode">
-  <CustomHandle type="target" position={Position.Left} nodeId={id} />
+  <Handle type="target" position={Position.Left} {isConnectable} />
   <div>Connection Limit 1</div>
 </div>
 
