89cd2b2ab19903f13d61d41125678909c1a68faf
moklick <info@moritzklack.com>
2023-10-12 17:12:41 +0200

    feat(svelte-docs): add theming

---
+++ apps/svelte-examples/src/routes/guides/theming/a/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { writable } from 'svelte/store';
+  import { SvelteFlow, Background, Controls, Position } from '@xyflow/svelte';
+
+  import '@xyflow/svelte/dist/style.css';
+
+  const nodeDefaults = {
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left
+  };
+
+  const nodes = writable([
+    {
+      id: '1',
+      position: { x: 0, y: 150 },
+      data: { label: 'default style 1' },
+      ...nodeDefaults
+    },
+    {
+      id: '2',
+      position: { x: 250, y: 0 },
+      data: { label: 'default style 2' },
+      ...nodeDefaults
+    },
+    {
+      id: '3',
+      position: { x: 250, y: 150 },
+      data: { label: 'default style 3' },
+      ...nodeDefaults
+    },
+    {
+      id: '4',
+      position: { x: 250, y: 300 },
+      data: { label: 'default style 4' },
+      ...nodeDefaults
+    }
+  ]);
+  const edges = writable([
+    {
+      id: 'e1-2',
+      source: '1',
+      target: '2',
+      animated: true
+    },
+    {
+      id: 'e1-3',
+      source: '1',
+      target: '3'
+    },
+    {
+      id: 'e1-4',
+      source: '1',
+      target: '4'
+    }
+  ]);
+</script>
+
+<main>
+  <SvelteFlow {nodes} {edges}>
+    <Background />
+    <Controls />
+  </SvelteFlow>
+</main>
+
+<style>
+  main {
+    height: 100vh;
+  }
+</style>
