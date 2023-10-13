27ef8b7637d8d5f4c432b875f99c8ba3ccc9b5b6
Hayleigh Thompson <me@hayleigh.dev>
2023-10-13 16:04:38 +0100

    :lipstick: Bring back header navbar.

--- sites/reactflow.dev/src/pages/_app.tsx
+++ sites/reactflow.dev/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import localFont from 'next/font/local';
 import { Fira_Mono } from 'next/font/google';
-import { useFathom } from 'xy-ui';
+import { Text, useFathom } from 'xy-ui';
 
 import 'styles/global.css';
 
@@ -27,12 +27,39 @@ const fathomOptions = {
   domains: ['reactflow.dev'],
 };
 
+const sites = [
+  { name: 'xyflow.com', href: 'https://xyflow.com' },
+  {
+    name: 'React Flow',
+    logo: '/img/react-logo.svg',
+    href: 'https://reactflow.dev',
+  },
+  {
+    name: 'Svelte Flow',
+    logo: '/img/svelte-logo.svg',
+    href: 'https://svelteflow.com',
+  },
+];
+
 export default function App({ Component, pageProps }) {
   useFathom(fathomOptions);
 
   return (
-    <main className={className}>
+    <div className={className}>
+      <header className="bg-black py-2 flex items-center justify-center gap-8">
+        {sites.map((site) => (
+          <Text size="xs" key={site.name}>
+            {site.logo && (
+              <img src={site.logo} className="w-3 h-3 mr-2 inline-block" />
+            )}
+            <a href={site.href} className="text-white hover:text-gray-200">
+              {site.name}
+            </a>
+          </Text>
+        ))}
+      </header>
+
       <Component {...pageProps} />
-    </main>
+    </div>
   );
 }
