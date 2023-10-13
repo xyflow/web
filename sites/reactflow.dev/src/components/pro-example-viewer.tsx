5ba79869c48341cc2373f4662c6ef4c8507d5842
Hayleigh Thompson <me@hayleigh.dev>
2023-10-13 11:18:41 +0100

    :art: Style the pro example viewer similar to our image gallery.

--- sites/reactflow.dev/src/components/pro-example-viewer.tsx
+++ sites/reactflow.dev/src/components/pro-example-viewer.tsx
@@ -1,19 +1,30 @@
-import Link from 'next/link';
+import { Container, Text, Button } from 'xy-ui';
 
 export default function ({ slug }: { slug: string }) {
   return (
-    <div className="bg-react p-4 my-4">
-      <div className="flex justify-between items-center	mb-4">
-        This is a Pro example. If you are subscribed to React Flow Pro you can
-        access the annotated source code.
-        <Link href={`https://pro.xyflow.com/examples/${slug}`} target="_blank">
-          View Source Code
-        </Link>
+    <Container variant="dark" className="mt-8">
+      <div className="p-14 space-y-8">
+        <iframe
+          src={`https://xyflow-react-pro-examples.vercel.app/${slug}`}
+          className="block h-[645px] w-full rounded-xl bg-white/10"
+        />
+
+        <div className="flex items-center space-between gap-32">
+          <Text className="mt-4 mb-4">
+            This is a Pro example! If you are subscribed to React Flow Pro you
+            can access the annotated source code over on our{' '}
+            <a className="underline" href="https://pro.xyflow.com">
+              Pro platform
+            </a>
+            .
+          </Text>
+          <Button variant="secondary" className="text-primary shrink-0">
+            <a href={`https://pro.xyflow.com/examples/${slug}`}>
+              View Source Code
+            </a>
+          </Button>
+        </div>
       </div>
-      <iframe
-        src={`https://xyflow-react-pro-examples.vercel.app/${slug}`}
-        className="h-[860px] w-full bg-white"
-      />
-    </div>
+    </Container>
   );
 }
