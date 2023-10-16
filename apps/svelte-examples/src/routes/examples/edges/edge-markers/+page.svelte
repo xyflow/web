ea4b42150bdf5e63c3a8f5f5c1e751fb44f2530b
Peter <p.korcsak@googlemail.com>
2023-10-16 09:44:22 +0200

    feat(svelte-flow) added edge markers examples

---
+++ apps/svelte-examples/src/routes/examples/edges/edge-markers/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { SvelteFlow, Background } from '@xyflow/svelte';
+  import type { Edge, Node } from '@xyflow/svelte';
+  import { writable } from 'svelte/store';
+
+  import '@xyflow/svelte/dist/style.css';
+
+  import { initialNodes, initialEdges } from './nodes-and-edges';
+  import Logo from './logo.svelte';
+
+  const nodes = writable<Node[]>(initialNodes);
+  const edges = writable<Edge[]>(initialEdges);
+</script>
+
+<div style="height:100vh;">
+  <SvelteFlow {nodes} {edges} fitView>
+    <Logo />
+    <Background />
+  </SvelteFlow>
+</div>
+
+<style>
+</style>
