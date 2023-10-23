ccb3004c75f250505c9217fd0722f38413cacd50
johnrobbjr <johnhrobb@gmail.com>
2023-10-23 12:33:51 +0200

    place used by logos in a grid

--- sites/reactflow.dev/src/components/client-logos.tsx
+++ sites/reactflow.dev/src/components/client-logos.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image';
 
 import { Text, cn } from 'xy-ui';
 
-import apple from '../../public/img/clients/apple.svg';
+import benchling from '../../public/img/clients/benchling.svg';
 import stripe from '../../public/img/clients/stripe.svg';
-import microsoft from '../../public/img/clients/microsoft.svg';
+import cartoworkflows from '../../public/img/clients/cartoworkflows.svg';
 import intuit from '../../public/img/clients/intuit.svg';
+import railway from '../../public/img/clients/railway.svg';
+import retool from '../../public/img/clients/retool.svg';
+import doubleloop from '../../public/img/clients/doubleloop.svg';
+import onesignal from '../../public/img/clients/onesignal.svg';
 
 export default function ClientLogos({
   title = 'Used By',
@@ -16,12 +20,23 @@ export default function ClientLogos({
 }) {
   return (
     <div className={cn('flex flex-col items-center w-full', className)}>
-      <Text variant="light">{title}</Text>
-      <div className="flex items-center space-x-12 mt-4">
-        <Image src={apple} alt="apple" className="h-7 w-auto" />
+      <Text variant="light" className="mb-8">
+        {title}
+      </Text>
+      <div className="grid grid-cols-4 gap-12 place-items-center">
+        {/* <div className="flex items-center space-x-12 mt-4"> */}
+        <Image src={benchling} alt="benchling" className="h-7 w-auto" />
         <Image src={stripe} alt="stripe" className="h-7 w-auto" />
-        <Image src={microsoft} alt="microsoft" className="h-7 w-auto" />
+        <Image
+          src={cartoworkflows}
+          alt="carto workflows"
+          className="h-7 w-auto"
+        />
         <Image src={intuit} alt="intuit" className="h-7 w-auto" />
+        <Image src={railway} alt="railway" className="h-7 w-auto" />
+        <Image src={retool} alt="retool" className="h-7 w-auto" />
+        <Image src={doubleloop} alt="doubleloop" className="h-7 w-auto" />
+        <Image src={onesignal} alt="onesignal" className="h-7 w-auto" />
       </div>
     </div>
   );
