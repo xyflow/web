d69a7a6678856844f9fb6132b070159489be227c
Peter <p.korcsak@googlemail.com>
2023-10-23 12:31:34 +0200

    added custom edges example

---
+++ apps/svelte-examples/src/routes/examples/edges/custom-edges/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { writable } from 'svelte/store';
+  import { SvelteFlow, Background, ConnectionMode } from '@xyflow/svelte';
+  import type { Edge, Node } from '@xyflow/svelte';
+
+  import '@xyflow/svelte/dist/style.css';
+
+  import { initialNodes, initialEdges } from './nodes-and-edges';
+
+  import ButtonEdge from './ButtonEdge.svelte';
+  import BiDirectionalEdge from './BiDirectionalEdge.svelte';
+  import BiDirectionalNode from './BiDirectionalNode.svelte';
+  import SelfConnectingEdge from './SelfConnectingEdge.svelte';
+
+  const nodes = writable<Node[]>(initialNodes);
+  const edges = writable<Edge[]>(initialEdges);
+
+  const nodeTypes = {
+    bidirectional: BiDirectionalNode
+  };
+
+  const edgeTypes = {
+    buttonedge: ButtonEdge,
+    bidirectional: BiDirectionalEdge,
+    selfconnecting: SelfConnectingEdge
+  };
+</script>
+
+<div style="height:100vh;">
+  <SvelteFlow {nodes} {nodeTypes} {edges} {edgeTypes} connectionMode={ConnectionMode.Loose} fitView>
+    <Background />
+  </SvelteFlow>
+</div>
