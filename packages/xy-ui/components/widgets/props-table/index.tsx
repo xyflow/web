16dbd8c85bcd9ede60ca223ad9fcc1d632a0f9e8
Christopher Möller <chrtze@gmail.com>
2023-10-23 16:17:38 +0200

    feat(props-table): collapse table on smaller screens (#180)
    * feat(props-table): collapse table on smaller screens
    
    * Rename Background.mdx to background.mdx
    
    * Rename Controls.mdx to controls.mdx
    
    * Rename Handle.mdx to handle.mdx
    
    * Rename MiniMap.mdx to mini-map.mdx
    
    * Rename mini-map.mdx to minimap.mdx
    
    * Rename Panel.mdx to panel.mdx
    
    * Rename Node.mdx to node.mdx
    
    * Rename Viewport.mdx to viewport.mdx
    
    * fix(reactflow): fix typo in hooks docs

--- packages/xy-ui/components/widgets/props-table/index.tsx
+++ packages/xy-ui/components/widgets/props-table/index.tsx
@@ -1,6 +1,6 @@
 import { useCallback } from 'react';
 import Link from 'next/link';
-import { Text } from '../../../.';
+import slugify from '@sindresorhus/slugify';
 
 type PropsTableProps = {
   props: {
@@ -8,20 +8,17 @@ type PropsTableProps = {
     type?: string;
     default?: string;
     description?: string;
+    example?: string;
   }[];
   links?: Record<string, string>;
   info?: Record<string, string>;
   deeplinkPrefix?: string;
 };
 
-function PropsTable({
-  props = [],
-  links = {},
-  deeplinkPrefix = '',
-}: PropsTableProps) {
+function PropsTable({ props: data, links = {} }: PropsTableProps) {
   // We can hide the default column entirely if none of the props have a default
   // value to document.
-  const shouldShowDefault = props.some((prop) => !!prop.default);
+  const showDefaultColumn = data.some((prop) => !!prop.default);
 
   // This function takes a string representing some type and attempts to turn any
   // types referenced inside into links, either internal or external.
@@ -44,108 +41,58 @@ function PropsTable({
   );
 
   return (
-    <table className="table-fixed w-full my-8 rounded-xl relative">
-      <thead>
-        <tr className="bg-gray-100">
-          <th className="px-2 py-2 w-[3%]" />
-          <th align="left" className="py-2 px-2 w-[27%]">
-            Name
-          </th>
-          <th
-            align="left"
-            className="py-2 px-2"
-            style={{ width: shouldShowDefault ? '50%' : '70%' }}
-          >
-            Type
-          </th>
-          {shouldShowDefault && (
-            <th align="left" className="py-2 px-2 hidden md:table-cell w-[20%]">
-              Default
-            </th>
-          )}
+    <table className="w-full my-8 text-sm">
+      <thead className="hidden lg:table-header-group text-left border-b border-gray-200">
+        <tr className="[&>th]:py-3">
+          <th>Name</th>
+          <th className="px-3">Type</th>
+          {showDefaultColumn && <th>Default</th>}
         </tr>
       </thead>
-
-      {/* This might seem like a hella contrived way to go about things but let
-          me break it down. For props with an optional description, we want to
-          render a second row underneath the usual defaults, and crucially we
-          want that row to span both the "type" and "default" columns.
-
-          That means we can't just plonk the description in an existing <td>. Your
-          first instinct might be to just render a fragment and produce two rows,
-          and that was mine too! 
-
-          It's super nit-picky but if you do this group-hover doesn't work correctly
-          and the description row doesn't turn grey when the user hovers over it.
-
-          It turns our it's semanticall valid to render as many <tbody>s as you 
-          like as long as they all appear after a <thead> and before a <tfoot>,
-          so that's what we have to do.
-
-          We need the markup to be semantically valid because next "helpfully"
-          does markup validation when statically generating pages.
-      */}
-      {props.map((prop) => {
-        const name = prop.name.toLowerCase().trim();
-        const id = deeplinkPrefix ? `${deeplinkPrefix}-${name}` : name;
+      {data.map((prop) => {
+        const slug = slugify(prop.name);
 
         return (
-          <tbody key={id} className="hover:bg-gray-50 group border-t">
-            {!prop.type && !prop.default ? (
-              <tr className="bg-gray-50 border-t">
-                <td className="px-2 w-8">
+          <tbody
+            key={slug}
+            id={slug}
+            className="group hover:bg-gray-50 max-lg:[&>tr]:block max-lg:block bg-gray-100 lg:bg-transparent rounded-xl border border-gray-200 lg:border-none mb-5"
+          >
+            <tr className="[&>td]:p-3 max-lg:[&>td]:block border-b border-gray-200">
+              <td className="lg:!pl-0">
+                <div className="relative">
                   <Link
-                    className={`invisible group-hover:visible text-primary`}
-                    href={`#${id}`}
+                    href={`#${slug}`}
+                    className="flex lg:w-[25px] items-center absolute right-0 text-lg font-black lg:right-auto lg:left-0 lg:top-1/2 lg:-translate-x-[22px] lg:-translate-y-1/2 group-hover:opacity-100 lg:opacity-0"
                   >
                     #
                   </Link>
-                </td>
-                <th
-                  colSpan={3}
-                  className="py-1 px-2 font-bold sticky top-0 text-left"
-                >
-                  {prop.name}
-                </th>
-              </tr>
-            ) : (
-              <>
-                <tr id={id}>
-                  <td className="px-2 w-8">
-                    <Link
-                      className={`invisible group-hover:visible text-primary`}
-                      href={`#${id}`}
-                    >
-                      #
-                    </Link>
-                  </td>
-                  <td className="flex justify-between py-1 px-2">
-                    <Text>{prop.name}</Text>
-                  </td>
-                  <td className="px-2 text-sm">
-                    <code>{linkify(prop.type)}</code>
-                  </td>
-                  {shouldShowDefault && (
-                    <td className="px-2 text-sm hidden md:table-cell">
-                      {prop.default ? (
-                        <code>{linkify(prop.default)}</code>
-                      ) : (
-                        '-'
-                      )}
-                    </td>
-                  )}
-                </tr>
-
+                  <code className="bg-gray-50 px-1.5 py-0.5 border border-gray-200 rounded-sm">
+                    {prop.name}
+                  </code>
+                </div>
+              </td>
+              <td className="px-3">
+                <div>
+                  <code className="break-all">{linkify(prop.type)}</code>
+                </div>
                 {prop.description && (
-                  <tr className="!border-0">
-                    <td className="px-2" colSpan={2} />
-                    <td className="px-2" colSpan={2}>
-                      <Text>{prop.description}</Text>
-                    </td>
-                  </tr>
+                  <div className="mt-2 max-w-md">{prop.description}</div>
                 )}
-              </>
-            )}
+                {prop.example && (
+                  <div className="bg-gray-50 border border-gray-200 rounded-lg mt-2">
+                    <pre>
+                      <code>{prop.example}</code>
+                    </pre>
+                  </div>
+                )}
+              </td>
+              {showDefaultColumn && (
+                <td>
+                  <code className="break-all">{linkify(prop.default)}</code>
+                </td>
+              )}
+            </tr>
           </tbody>
         );
       })}
