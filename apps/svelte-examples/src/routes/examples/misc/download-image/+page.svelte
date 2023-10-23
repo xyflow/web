302aaacd45c93e295cbb1a4a02639c2094aefd38
Peter <p.korcsak@googlemail.com>
2023-10-23 10:46:16 +0200

    added download image example

---
+++ apps/svelte-examples/src/routes/examples/misc/download-image/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { SvelteFlow, Background } from '@xyflow/svelte';
+  import type { Edge, Node } from '@xyflow/svelte';
+  import { writable } from 'svelte/store';
+
+  import '@xyflow/svelte/dist/style.css';
+
+  import { initialNodes, initialEdges } from './nodes-and-edges';
+  import DownloadButton from './DownloadButton.svelte';
+
+  const nodes = writable<Node[]>(initialNodes);
+  const edges = writable<Edge[]>(initialEdges);
+
+  const defaultEdgeOptions = {
+    animated: true,
+    type: 'smoothstep'
+  };
+</script>
+
+<div style="height:100vh;">
+  <SvelteFlow {nodes} {edges} {defaultEdgeOptions} fitView>
+    <DownloadButton />
+    <Background />
+  </SvelteFlow>
+</div>
---
+++ apps/svelte-examples/src/routes/examples/misc/download-image/DownloadButton.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { Panel, getRectOfNodes, getTransformForBounds, useNodes } from '@xyflow/svelte';
+  import { toPng } from 'html-to-image';
+
+  const nodes = useNodes();
+
+  const imageWidth = 1024;
+  const imageHeight = 768;
+
+  function handleClick() {
+    const nodesBounds = getRectOfNodes($nodes);
+    const transform = getTransformForBounds(nodesBounds, imageWidth, imageHeight, 0.5, 2.0, 0.2);
+
+    const viewport = document.querySelector<HTMLElement>('.svelte-flow__viewport');
+
+    if (viewport) {
+      toPng(viewport, {
+        backgroundColor: '#1a365d',
+        width: imageWidth,
+        height: imageHeight,
+        style: {
+          width: `${imageWidth}px`,
+          height: `${imageHeight}px`,
+          transform: `translate(${transform[0]}px, ${transform[1]}px) scale(${transform[2]})`
+        }
+      }).then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = 'svelte-flow.png';
+        link.href = dataUrl;
+        link.click();
+      });
+    }
+  }
+</script>
+
+<Panel position="top-right">
+  <button on:click={handleClick}>Donwload Image</button>
+</Panel>
