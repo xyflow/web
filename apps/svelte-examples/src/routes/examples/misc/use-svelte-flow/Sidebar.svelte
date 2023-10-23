fcb0856e6543d4347510d028acae78085530df2c
Peter <p.korcsak@googlemail.com>
2023-10-23 13:12:23 +0200

    fixed useSvelteFlow example

--- apps/svelte-examples/src/routes/examples/misc/use-svelte-flow/Sidebar.svelte
+++ apps/svelte-examples/src/routes/examples/misc/use-svelte-flow/Sidebar.svelte
@@ -1,47 +1,32 @@
 <script lang="ts">
-  import { useSvelteFlow } from '@xyflow/svelte';
+  import { useSvelteFlow, useNodes } from '@xyflow/svelte';
 
-  const {
-    zoomIn,
-    zoomOut,
-    setZoom,
-    fitView,
-    setCenter,
-    setViewport,
-    getViewport,
-    viewport,
-    nodes,
-  } = useSvelteFlow();
+  const { zoomIn, zoomOut, setZoom, fitView, setCenter, setViewport, getViewport, viewport } =
+    useSvelteFlow();
+
+  const nodes = useNodes();
 </script>
 
 <aside>
   <div class="label">Functions:</div>
   <button on:click={() => zoomIn()}>zoom in</button>
-  <button on:click={() => zoomOut({ duration: 1000 })}
-    >zoom out transition</button
-  >
+  <button on:click={() => zoomOut({ duration: 1000 })}>zoom out transition</button>
   <button on:click={() => setZoom(2)}>set zoom</button>
   <button on:click={() => fitView()}>fitView</button>
   <button on:click={() => setCenter(0, 0)}>setCenter 0, 0</button>
-  <button on:click={() => setViewport({ x: 100, y: 100, zoom: 2 })}
-    >setViewport</button
-  >
+  <button on:click={() => setViewport({ x: 100, y: 100, zoom: 2 })}>setViewport</button>
   <button on:click={() => console.log(getViewport())}>getViewport</button>
 
   <div class="label">Nodes:</div>
   {#each $nodes as node (node.id)}
     <div>
-      id: {node.id} | x: {node.position.x.toFixed(1)} y: {node.position.y.toFixed(
-        1
-      )}
+      id: {node.id} | x: {node.position.x.toFixed(1)} y: {node.position.y.toFixed(1)}
     </div>
   {/each}
 
   <div class="label">Viewport:</div>
   <div>
-    x: {$viewport.x.toFixed(1)} y: {$viewport.y.toFixed(1)} zoom: {$viewport.zoom.toFixed(
-      1
-    )}
+    x: {$viewport.x.toFixed(1)} y: {$viewport.y.toFixed(1)} zoom: {$viewport.zoom.toFixed(1)}
   </div>
 </aside>
 
---
+++ apps/svelte-examples/src/routes/examples/nodes/delete-middle-node/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { SvelteFlow, Background } from '@xyflow/svelte';
+  import type { Edge, Node } from '@xyflow/svelte';
+  import { writable } from 'svelte/store';
+
+  import '@xyflow/svelte/dist/style.css';
+
+  import { initialNodes, initialEdges } from './nodes-and-edges';
+
+  const nodes = writable<Node[]>(initialNodes);
+  const edges = writable<Edge[]>(initialEdges);
+
+  function onNodesDelete(e) {
+    console.log(e);
+  }
+</script>
+
+<div style="height:100vh;">
+  <SvelteFlow {nodes} {edges} on:nodedelete={onNodesDelete} fitView>
+    <Background />
+  </SvelteFlow>
+</div>
