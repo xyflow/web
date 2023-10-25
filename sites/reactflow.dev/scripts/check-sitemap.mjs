622bd4aa1969c0a1740f39ee9aab801b78e4c428
Christopher Möller <chrtze@gmail.com>
2023-10-25 14:30:05 +0200

    feat(redirects): add script to generate redirects

--- sites/reactflow.dev/scripts/check-sitemap.mjs
+++ sites/reactflow.dev/scripts/check-sitemap.mjs
@@ -1,34 +1,117 @@
 import { XMLParser } from 'fast-xml-parser';
 
-const SITEMAP_URL = 'https://reactflow.dev/sitemap.xml';
-const LEGACY_URL = 'https://reactflow.dev';
-const NEW_URL = 'https://xyflow-docs-git-staging-xyflow.vercel.app';
+const xmlParser = new XMLParser();
 
-const parser = new XMLParser();
+async function getSitePaths(url) {
+  const response = await fetch(url);
+  const sitemapXML = await response.text();
+  const { urlset } = xmlParser.parse(sitemapXML);
+  const paths = urlset.url.map((item) =>
+    item.loc
+      .replace('https://reactflow.dev', '')
+      .replace('https://xyflow.com', '')
+      .replace(/\/$/g, ''),
+  );
 
-async function fetchSitemap() {
-  const response = await fetch(SITEMAP_URL);
-  const text = await response.text();
-
-  return text;
+  return paths;
 }
 
 async function start() {
-  const sitemapXML = await fetchSitemap();
-  const { urlset } = parser.parse(sitemapXML);
-  const urls = urlset.url.map((item) => item.loc.replace(LEGACY_URL, NEW_URL));
-  let failedCount = 0;
+  const redirects = [];
 
-  for (const url of urls) {
-    const res = await fetch(url, { method: 'HEAD' });
+  let unmatched = await getSitePaths('https://reactflow.dev/sitemap.xml');
 
-    if (res.status !== 200) {
-      failedCount++;
-      console.log(url, res.status);
+  const redirect = (from, to, domain = '') => {
+    unmatched = unmatched.filter((path) => path !== from);
+
+    if (to) {
+      redirects.push({
+        source: from,
+        destination: `${domain}${to}`,
+        permanent: true,
+      });
     }
-  }
+  };
 
-  console.log(`Couldn't fetch ${failedCount} pages.`);
+  const rfPaths = await getSitePaths(
+    'https://reactflow-website.vercel.app/sitemap.xml',
+  );
+
+  const match = (originalPath, lookupPath) => {
+    const newPath = rfPaths.find((p) => p === lookupPath);
+
+    if (newPath) {
+      redirect(originalPath, newPath);
+    }
+  };
+
+  const xyPaths = await getSitePaths('https://xyflow.com/sitemap.xml');
+
+  unmatched.forEach((path) => {
+    // first filter out all the paths that haven't changed
+    if (rfPaths.includes(path)) {
+      console.log('path didnt change:', path);
+      return redirect(path);
+    }
+
+    // handle blog redirects
+    if (path.startsWith('/blog')) {
+      const newPath =
+        xyPaths.find((xyPath) => xyPath.includes(path)) ?? '/blog';
+      return redirect(path, newPath, 'https://xyflow.com');
+    }
+
+    match(path, path.replace('/docs/api', '/api-reference'));
+    match(path, path.replace('/docs/api/edges', '/api-reference/components'));
+    match(path, path.replace('/docs/api/nodes', '/api-reference/components'));
+    match(
+      path,
+      path.replace('/docs/api/plugin-components', '/api-reference/components'),
+    );
+    match(path, path.replace('/docs/examples', '/examples'));
+    match(path, path.replace('/docs/concepts', '/learn/concepts'));
+    match(path, path.replace('/docs/guides', '/learn/customization'));
+    match(
+      path,
+      path.replace('/docs/getting-started', '/learn/getting-started'),
+    );
+    match(path, path.replace('/docs/guides', '/learn/advanced-use'));
+    match(path, path.replace('/docs/guides', '/learn/troubleshooting'));
+    match(path, path.replace('/docs/guides', '/learn/layouting'));
+  });
+
+  // // redirects for the blog pages
+  // const blogPaths = unmatchedPaths.filter((path) => path.startsWith('/blog/'));
+
+  // blogPaths.forEach((blogPath) => {
+  //   // if the url doesn't exist, we just redirect to the blog page
+  //   const newPath =
+  //     xyFlowPaths.find((path) => path.includes(blogPath)) ?? '/blog';
+  //   const newUrl = `https://xyflow.com${newPath}`;
+
+  //   redirects.push({ source: blogPath, destination: newUrl, permanent: true });
+  // });
+
+  console.log(redirects);
+  console.log(unmatched);
+
+  // const oldSitemapXML = await fetchSitemap();
+  // const newSitemapXML = await fetchSitemap();
+
+  // const sitemapXML = await fetchSitemap();
+
+  // let failedCount = 0;
+
+  // for (const url of urls) {
+  //   const res = await fetch(url, { method: 'HEAD' });
+
+  //   if (res.status !== 200) {
+  //     failedCount++;
+  //     console.log(url, res.status);
+  //   }
+  // }
+
+  // console.log(`Couldn't fetch ${failedCount} pages.`);
 }
 
 start();
