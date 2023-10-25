11dc61b700807f817c5689f7700884b426d9ed9e
Hayleigh Thompson <me@hayleigh.dev>
2023-10-25 22:48:32 +0100

    :wrench: Add script to generate type-safe routes and check mdx files.

--- sites/svelteflow.dev/src/utils/index.ts
+++ sites/svelteflow.dev/src/utils/index.ts
@@ -1,20 +1,21 @@
 import { MdxFile } from 'nextra';
 import { getPagesUnderRoute } from 'nextra/context';
+import { type Route, type ExternalRoute, type InternalRoute } from './routes';
 
 export function isMdxPage(page: MdxFile | any): page is MdxFile {
   return page?.kind === 'MdxPage';
 }
 
-export function getMdxPagesUnderRoute(route: string) {
+export function getMdxPagesUnderRoute(route: InternalRoute) {
   return getPagesUnderRoute(route).filter(isMdxPage);
 }
 
 // used for pagination for blog and case studies to display prev and next post/ case study
-export function getPrevAndNextPagesByTitle(title, route: string) {
+export function getPrevAndNextPagesByTitle(title, route: InternalRoute) {
   const pages = getMdxPagesUnderRoute(route);
 
   const currentIndex = pages.findIndex(
-    (page) => page.frontMatter?.title === title
+    (page) => page.frontMatter?.title === title,
   );
   const prevIndex = currentIndex === 0 ? pages.length - 1 : currentIndex - 1;
   const nextIndex = currentIndex === pages.length - 1 ? 0 : currentIndex + 1;
@@ -24,3 +25,5 @@ export function getPrevAndNextPagesByTitle(title, route: string) {
 
   return [prevPage, nextPage];
 }
+
+export type { Route, ExternalRoute, InternalRoute };
