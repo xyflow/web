d3266fe731918bdadd2c5797a2f8b3142db9e8af
Hayleigh Thompson <me@hayleigh.dev>
2023-11-03 00:00:33 +0000

    :bug: Fix broken resize-rotate example.

---
+++ sites/reactflow.dev/src/components/example-viewer/example-flows/ResizeRotate/nodes-edges.js
@@ -0,0 +1,32 @@
+import { Position } from 'reactflow';
+
+export const nodes = [
+  {
+    id: '1',
+    position: { x: 0, y: 0 },
+    data: { label: 'Node 1', resizable: true },
+    type: 'resizeRotate',
+    targetPosition: Position.Left,
+    sourcePosition: Position.Right,
+    selected: true,
+    style: { width: 180, height: 100 },
+  },
+  {
+    id: '2',
+    position: { x: 300, y: 0 },
+    data: { label: 'Node 2', rotatable: true },
+    type: 'resizeRotate',
+    targetPosition: Position.Left,
+    sourcePosition: Position.Right,
+    style: { width: 180, height: 100 },
+  },
+];
+
+export const edges = [
+  {
+    id: '1->2',
+    source: '1',
+    target: '2',
+    type: 'smoothstep',
+  },
+];
