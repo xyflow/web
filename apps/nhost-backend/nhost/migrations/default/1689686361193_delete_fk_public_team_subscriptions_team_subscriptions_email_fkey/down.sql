15d07924b1ea150da605281205aedf972b1b3476
Christopher Möller <chrtze@gmail.com>
2023-11-06 13:59:14 +0100

    feat(repo): remove pro platform and nhost backend from the repo

--- apps/nhost-backend/nhost/migrations/default/1689686361193_delete_fk_public_team_subscriptions_team_subscriptions_email_fkey/down.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."team_subscriptions"
-  add constraint "team_subscriptions_email_fkey"
-  foreign key ("email")
-  references "auth"."users"
-  ("email") on update restrict on delete restrict;
--- apps/nhost-backend/nhost/migrations/default/1689686361193_delete_fk_public_team_subscriptions_team_subscriptions_email_fkey/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."team_subscriptions" drop constraint "team_subscriptions_email_fkey";
