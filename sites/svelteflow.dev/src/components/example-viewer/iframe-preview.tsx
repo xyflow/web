b40a17d5303775b6cb795345b42567e0354aeafa
Peter <p.korcsak@googlemail.com>
2023-11-06 13:17:59 +0100

    fix(svelteflow.dev)  preview deployment links

--- sites/svelteflow.dev/src/components/example-viewer/iframe-preview.tsx
+++ sites/svelteflow.dev/src/components/example-viewer/iframe-preview.tsx
@@ -7,7 +7,7 @@ export default function ({
 }) {
   console.log(process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL);
   const fullPath = process.env.NEXT_PUBLIC_VERCEL_GIT_PULL_REQUEST_ID
-    ? `xyflow-svelte-examples-git-${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}-xyflow.vercel.app/${path}`
+    ? `https://xyflow-svelte-examples-git-${process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}-xyflow.vercel.app/${path}`
     : `${process.env.NEXT_PUBLIC_SVELTE_EXAMPLES_URL}${path}`;
   return (
     <iframe
