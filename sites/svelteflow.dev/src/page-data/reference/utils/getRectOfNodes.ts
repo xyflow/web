045db57739d90ae9a9412b30f554b7dd4be895f2
Peter <p.korcsak@googlemail.com>
2023-12-05 14:06:58 +0100

    Added references for util functions in svelte, closes #262

---
+++ sites/svelteflow.dev/src/page-data/reference/utils/getRectOfNodes.ts
@@ -0,0 +1,10 @@
+import { type PropsTableProps } from 'xy-shared';
+
+export const signature: PropsTableProps = {
+  props: [
+    { name: 'Params' },
+    { name: 'nodes', type: 'Node[]' },
+    { name: 'Returns' },
+    { name: '', type: 'Rect' },
+  ],
+};
