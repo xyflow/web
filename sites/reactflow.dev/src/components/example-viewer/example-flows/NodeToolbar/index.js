c103afe71a23769dfcfc555eb506587bc399a383
Hayleigh Thompson <me@hayleigh.dev>
2024-01-18 14:30:10 +0000

    :memo: Create a node toolbar example.

---
+++ sites/reactflow.dev/src/components/example-viewer/example-flows/NodeToolbar/index.js
@@ -0,0 +1,90 @@
+import React, { useCallback, useState } from 'react';
+import ReactFlow, {
+  ReactFlowProvider,
+  Panel,
+  NodeToolbar,
+  Position,
+  useNodesState,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  {
+    id: '1',
+    position: { x: 0, y: 0 },
+    type: 'node-with-toolbar',
+    data: { label: 'Select me to show the toolbar' },
+  },
+];
+
+const nodeTypes = {
+  'node-with-toolbar': NodeWithToolbar,
+};
+
+function NodeWithToolbar({ data }) {
+  return (
+    <>
+      <NodeToolbar
+        isVisible={data.forceToolbarVisible || undefined}
+        position={data.toolbarPosition}
+      >
+        <button>cut</button>
+        <button>copy</button>
+        <button>paste</button>
+      </NodeToolbar>
+      <div className="react-flow__node-default">{data?.label}</div>
+    </>
+  );
+}
+
+function Flow() {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const setPosition = useCallback(
+    (pos) =>
+      setNodes((nodes) =>
+        nodes.map((node) => ({
+          ...node,
+          data: { ...node.data, toolbarPosition: pos },
+        })),
+      ),
+    [setNodes],
+  );
+  const forceToolbarVisible = useCallback((enabled) =>
+    setNodes((nodes) =>
+      nodes.map((node) => ({
+        ...node,
+        data: { ...node.data, forceToolbarVisible: enabled },
+      })),
+    ),
+  );
+
+  return (
+    <ReactFlowProvider>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        nodeTypes={nodeTypes}
+        fitView
+        preventScrolling={false}
+      >
+        <Panel>
+          <h3>Node Toolbar position:</h3>
+          <button onClick={() => setPosition(Position.Top)}>top</button>
+          <button onClick={() => setPosition(Position.Right)}>right</button>
+          <button onClick={() => setPosition(Position.Bottom)}>bottom</button>
+          <button onClick={() => setPosition(Position.Left)}>left</button>
+          <h3>Override Node Toolbar visibility</h3>
+          <label>
+            <input
+              type="checkbox"
+              onChange={(e) => forceToolbarVisible(e.target.checked)}
+            />
+            <span>Always show toolbar</span>
+          </label>
+        </Panel>
+      </ReactFlow>
+    </ReactFlowProvider>
+  );
+}
+
+export default Flow;
