c1f1dd8467169a36028f1a366b6ca30387c9dfa6
peterkogo <peter@xyflow.com>
2024-02-26 16:16:51 +0100

    fixed dagre tree example

--- sites/reactflow.dev/src/components/example-viewer/example-flows/DagreTree/index.js
+++ sites/reactflow.dev/src/components/example-viewer/example-flows/DagreTree/index.js
@@ -33,22 +33,24 @@ const getLayoutedElements = (nodes, edges, direction = 'TB') => {
 
   dagre.layout(dagreGraph);
 
-  nodes.forEach((node) => {
+  const newNodes = nodes.map((node) => {
     const nodeWithPosition = dagreGraph.node(node.id);
-    node.targetPosition = isHorizontal ? 'left' : 'top';
-    node.sourcePosition = isHorizontal ? 'right' : 'bottom';
-
-    // We are shifting the dagre node position (anchor=center center) to the top left
-    // so it matches the React Flow node anchor point (top left).
-    node.position = {
-      x: nodeWithPosition.x - nodeWidth / 2,
-      y: nodeWithPosition.y - nodeHeight / 2,
+    const newNode = {
+      ...node,
+      targetPosition: isHorizontal ? 'left' : 'top',
+      sourcePosition: isHorizontal ? 'right' : 'bottom',
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      position: {
+        x: nodeWithPosition.x - nodeWidth / 2,
+        y: nodeWithPosition.y - nodeHeight / 2,
+      },
     };
 
-    return node;
+    return newNode;
   });
 
-  return { nodes, edges };
+  return { nodes: newNodes, edges };
 };
 
 const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
