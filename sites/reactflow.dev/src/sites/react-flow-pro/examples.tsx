933e3432df64070de6ff5460a5d2fc19589a56d8
Christopher Möller <chrtze@gmail.com>
2024-03-05 17:04:14 +0100

    refactor(react-docs): style pro examples page

--- sites/reactflow.dev/src/sites/react-flow-pro/examples.tsx
+++ sites/reactflow.dev/src/sites/react-flow-pro/examples.tsx
@@ -26,6 +26,7 @@ import { getMdxPagesUnderRoute } from '@/utils';
 import Image from 'next/image';
 import { ReactNode } from 'react';
 import { HeartIcon } from '@heroicons/react/24/solid';
+import starsvg from '../../../public/img/pro/star.svg';
 
 function getProExamplesUnderRoute(route) {
   return getMdxPagesUnderRoute(route).filter(
@@ -68,24 +69,29 @@ export default function ProExamples() {
     <BaseLayout>
       <Hero
         title={
-          <span>
-            Upgrade your apps with React Flow{' '}
-            <span className="text-primary">Pro</span> Examples
-          </span>
+          <div className="mt-6">
+            Upgrade your apps with React Flow <StarText /> Examples
+          </div>
         }
         subtitle="Get 10 advanced React Flow code examples to use in your node-based UIs, crafted by the React Flow core team."
         action={
-          <div className="flex gap-2">
-            <Button asChild size="lg">
+          <div className="flex gap-2 items-center">
+            <Button
+              variant="secondary"
+              className="text-black"
+              asChild
+              size="lg"
+            >
               <Link href={`${process.env.NEXT_PUBLIC_PRO_PLATFORM_URL}/signup`}>
                 See Subscriptions
               </Link>
             </Button>
             <Button asChild size="lg" variant="pro">
               <Link href="/pro/pricing">
-                <SparklesIcon className="w-5 h-5 mr-2" /> Get Started
+                <SparklesIcon className="w-5 h-5 mr-2" /> Sign Up
               </Link>
             </Button>
+            <GradientText />
           </div>
         }
       >
@@ -96,38 +102,45 @@ export default function ProExamples() {
           height={478}
         />
       </Hero>
-      <ContentGrid className="lg:grid-cols-4">
-        <GridItem
-          title="By the creators of React Flow"
-          text="Feature-complete and crafted by the core team"
-          // @ts-ignore
-          icon={Logo}
-        />
-        <GridItem
-          title="Downloadable Vite apps and guides"
-          text="Step-by-step instructions on how each example works"
-          icon={ArrowDownCircleIcon}
-        />
-        <GridItem
-          title="Regularly Added and Updated"
-          text="Subscribers are the first to get new pro examples and refactors"
-          icon={PlusCircleIcon}
-        />
-        <GridItem
-          title="Subscribe for one, use them forever"
-          text="Download the pro examples and use them anywhere, anytime"
-          icon={StarIcon}
-        />
-      </ContentGrid>
+      <Section>
+        <ContentGrid className="lg:grid-cols-4 border-t-0 gap-12">
+          <GridItem
+            title="By the creators of React Flow"
+            text="Feature-complete and crafted by the core team"
+            // @ts-ignore
+            icon={Logo}
+          />
+          <GridItem
+            title="Downloadable Vite apps and guides"
+            text="Step-by-step instructions on how each example works"
+            icon={ArrowDownCircleIcon}
+          />
+          <GridItem
+            title="Regularly Added and Updated"
+            text="Subscribers are the first to get new pro examples and refactors"
+            icon={PlusCircleIcon}
+          />
+          <GridItem
+            title="Subscribe for one, use them forever"
+            text="Download the pro examples and use them anywhere, anytime"
+            icon={StarIcon}
+          />
+        </ContentGrid>
+      </Section>
+
+      <hr className="border-gray-100" />
 
       <Section>
         <Container variant="dark">
-          <iframe
-            src={`${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/shapes`}
-            className="block h-[645px] w-full bg-white"
+          <Image
+            src={`${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/shapes/thumbnail.jpg`}
+            width={1200}
+            height={600}
+            objectFit="contain"
+            alt="Shapes Example Preview"
           />
         </Container>
-        <div className="flex mt-4 gap-2">
+        <div className="flex mt-10 gap-2">
           <Heading size="md">Shapes</Heading>
           <div className="max-w-lg ml-auto">
             <Text>
@@ -137,13 +150,14 @@ export default function ProExamples() {
               drag-and-drop new nodes onto the canvas, change the color with a
               toolbar, plus dark and light mode.
             </Text>
-            <div className="flex gap-2 mt-4">
+            <div className="flex gap-2 mt-4 items-center">
               <Button asChild size="lg">
                 <Link href="/examples/nodes/shapes">Demo</Link>
               </Button>
               <Button asChild size="lg" variant="pro">
                 <Link href="/pro/pricing">Sign Up</Link>
               </Button>
+              <GradientText />
             </div>
           </div>
         </div>
@@ -180,10 +194,10 @@ export default function ProExamples() {
             </Text>
             <div className="flex gap-2 mt-4">
               <Button asChild size="lg">
-                <Link href="/examples/nodes/shapes">Demo</Link>
+                <Link href="/examples/nodes/shapes">Try it out</Link>
               </Button>
               <Button asChild size="lg" variant="pro">
-                <Link href="/pro/pricing">Sign Up</Link>
+                <Link href="/pro/pricing">See subscription plans</Link>
               </Button>
             </div>
           </div>
@@ -192,6 +206,7 @@ export default function ProExamples() {
 
       <Section>
         <ListWrapper
+          className="max-w-lg mx-auto"
           title="Are you a student, educator, or open source developer?"
           subtitle="Get all of these Pro examples for free"
         >
@@ -199,6 +214,7 @@ export default function ProExamples() {
             title="For education purposes"
             text="Email us at info@xyflow.com using your university email address"
           />
+          <hr className="my-5" />
           <ListItem
             title="For non-commercial open source projects"
             text="Contact us with the link to the github or gitlab repository"
@@ -230,11 +246,9 @@ function GridItem({
   links?: Array<{ route: string; linkName: string; target?: string }>;
 }) {
   return (
-    <ContentGridItem>
+    <div>
       <Icon className="text-primary w-8 h-8" />
-      <Heading size="sm" className="mt-2 mb-4" as="p">
-        {title}
-      </Heading>
+      <div className="text-3xl font-bold mt-2 mb-4">{title}</div>
       <Text variant="light" className="text-md">
         {text}
       </Text>
@@ -257,6 +271,37 @@ function GridItem({
             );
           })}
       </div>
-    </ContentGridItem>
+    </div>
+  );
+}
+
+function StarText() {
+  return (
+    <span className="inline-flex relative text-transparent bg-clip-text bg-gradient-to-r from-primary to-[#FF99C7]">
+      <span>Pro</span>
+      <Image
+        src={starsvg}
+        alt="star"
+        className="absolute h-5 w-5 left-[40%] opacity-50"
+      />
+      <Image
+        src={starsvg}
+        alt="star"
+        className="absolute h-4 w-4 -right-[10px] top-1/2 opacity-20"
+      />
+      <Image
+        src={starsvg}
+        alt="star"
+        className="absolute h-3 w-3 left-[16px] bottom-[6px] opacity-70"
+      />
+    </span>
+  );
+}
+
+function GradientText() {
+  return (
+    <span className="text-sm text-transparent bg-clip-text bg-gradient-to-r from-[#FA3C92] to-[#969696]">
+      to get a free pro example
+    </span>
   );
 }
