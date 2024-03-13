3ada68a676ee758c902ad12c97b0ebdcfd77fd05
peterkogo <peter@xyflow.com>
2024-03-13 17:12:36 +0100

    added page for edge label

---
+++ sites/svelteflow.dev/src/page-data/reference/components/EdgeLabel.ts
@@ -0,0 +1,9 @@
+import { type PropsTableProps } from 'xy-shared';
+
+export const edgeLabelProps: PropsTableProps = {
+  props: [
+    { name: 'style?', type: 'string' },
+    { name: 'x?', type: 'number' },
+    { name: 'y?', type: 'number' },
+  ],
+};
