049bc17c8fbdb30027af9d8179245e9f2fa545a7
moklick <info@moritzklack.com>
2024-04-24 15:14:12 +0200

    style(feat-overview): cleanup

--- sites/reactflow.dev/src/components/example-viewer/example-flows/Overview/index.js
+++ sites/reactflow.dev/src/components/example-viewer/example-flows/Overview/index.js
@@ -60,21 +60,6 @@ const OverviewFlow = () => {
       <MiniMap zoomable pannable nodeClassName={nodeClassName} />
       <Controls />
       <Background />
-
-      <svg>
-        <defs>
-          <linearGradient id="Gradient1">
-            <stop class="stop1" offset="0%" />
-            <stop class="stop2" offset="50%" />
-            <stop class="stop3" offset="100%" />
-          </linearGradient>
-          <linearGradient id="Gradient2" x1="0" x2="0" y1="0" y2="1">
-            <stop offset="0%" stop-color="red" />
-            <stop offset="50%" stop-color="black" stop-opacity="0" />
-            <stop offset="100%" stop-color="blue" />
-          </linearGradient>
-        </defs>
-      </svg>
     </ReactFlow>
   );
 };
