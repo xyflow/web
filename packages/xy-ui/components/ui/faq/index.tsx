378a97a1e4a2344ee9e816a2eee90635df80abd6
johnrobbjr <johnhrobb@gmail.com>
2024-06-10 17:07:30 +0200

    add video to FAQ component with captions on

--- packages/xy-ui/components/ui/faq/index.tsx
+++ packages/xy-ui/components/ui/faq/index.tsx
@@ -1,4 +1,5 @@
 import { InformationCircleIcon } from '@heroicons/react/24/outline';
+import { ReactPlayer } from '../../../../xy-shared';
 
 import {
   Accordion,
@@ -26,6 +27,29 @@ export function FAQ({
       iconClassName="text-react"
       className={className}
     >
+      <div
+        style={{ position: 'relative', paddingTop: '56.25%' }}
+        className="mb-8"
+      >
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=jm_UoZXEEnU"
+          width="100%"
+          height="100%"
+          style={{ position: 'absolute', top: 0, left: 0 }}
+          config={{
+            youtube: {
+              embedOptions: {
+                playerVars: {
+                  color: 'white',
+                  cc_load_policy: 1,
+                  cc_lang_pref: 'en',
+                  controls: 1,
+                },
+              },
+            },
+          }}
+        />
+      </div>
       <Accordion type="multiple">
         {items.map((item, index) => (
           <AccordionItem
