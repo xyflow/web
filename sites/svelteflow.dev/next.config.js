753a9eddf0b6ce2d51c01d5ff2f92fcd998aad11
moklick <info@moritzklack.com>
2024-07-03 11:39:42 +0200

    refactor(sites): use package.json for lib versions

--- sites/svelteflow.dev/next.config.js
+++ sites/svelteflow.dev/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
+const SVELTE_FLOW_VERSION = require('./package.json').dependencies[
+  '@xyflow/svelte'
+]?.replace('^', '');
+
 const nextConfig = {
   // Configure pageExtensions to include md and mdx
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
   // Optionally, add any other Next.js config below
   reactStrictMode: true,
   transpilePackages: ['@xyflow/xy-ui', 'xy-shared'],
+  env: {
+    SVELTE_FLOW_VERSION,
+  },
 };
 
 const withNextra = require('nextra')({
