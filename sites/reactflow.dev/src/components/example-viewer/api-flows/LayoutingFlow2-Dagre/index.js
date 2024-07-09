b5f72560d9be55d7c18f23d1ce27b7e2027d000f
moklick <info@moritzklack.com>
2024-07-09 14:45:00 +0200

    chore(react): v12 updates

--- sites/reactflow.dev/src/components/example-viewer/api-flows/LayoutingFlow2-Dagre/index.js
+++ sites/reactflow.dev/src/components/example-viewer/api-flows/LayoutingFlow2-Dagre/index.js
@@ -12,13 +12,18 @@ import {
 import { initialNodes, initialEdges } from './nodes-edges.js';
 import '@xyflow/react/dist/style.css';
 
-const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
-
 const getLayoutedElements = (nodes, edges, options) => {
+  const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
   g.setGraph({ rankdir: options.direction });
 
   edges.forEach((edge) => g.setEdge(edge.source, edge.target));
-  nodes.forEach((node) => g.setNode(node.id, node));
+  nodes.forEach((node) =>
+    g.setNode(node.id, {
+      ...node,
+      width: node.measured?.width ?? 0,
+      height: node.measured?.height ?? 0,
+    }),
+  );
 
   Dagre.layout(g);
 
@@ -27,8 +32,8 @@ const getLayoutedElements = (nodes, edges, options) => {
       const position = g.node(node.id);
       // We are shifting the dagre node position (anchor=center center) to the top left
       // so it matches the React Flow node anchor point (top left).
-      const x = position.x - node.width / 2;
-      const y = position.y - node.height / 2;
+      const x = position.x - (node.measured?.width ?? 0) / 2;
+      const y = position.y - (node.measured?.height ?? 0) / 2;
 
       return { ...node, position: { x, y } };
     }),
@@ -43,6 +48,7 @@ const LayoutFlow = () => {
 
   const onLayout = useCallback(
     (direction) => {
+      console.log(nodes);
       const layouted = getLayoutedElements(nodes, edges, { direction });
 
       setNodes([...layouted.nodes]);
