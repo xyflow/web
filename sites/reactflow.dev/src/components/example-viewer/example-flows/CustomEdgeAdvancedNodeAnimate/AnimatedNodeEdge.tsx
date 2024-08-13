5bf80f2733e84b654b20a589988276c8b3a9a36e
Hayleigh Thompson <me@hayleigh.dev>
2024-08-13 11:42:13 +0100

    :memo: Create animated edges example(s).

---
+++ sites/reactflow.dev/src/components/example-viewer/example-flows/CustomEdgeAdvancedNodeAnimate/AnimatedNodeEdge.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useMemo } from 'react';
+import {
+  BaseEdge,
+  Edge,
+  EdgeProps,
+  getBezierPath,
+  useReactFlow,
+  updateNode,
+} from '@xyflow/react';
+
+export type AnimatedNodeEdge = Edge<{ node: string }, 'animatedNode'>;
+
+export function AnimatedNodeEdge({
+  id,
+  data = { node: '' },
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+}: EdgeProps<AnimatedNodeEdge>) {
+  const { getNode, updateNode } = useReactFlow();
+  const [edgePath] = getBezierPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+  const selector = useMemo(
+    () => `.react-flow__node[data-id="${data.node}"]`,
+    [data.node],
+  );
+
+  useEffect(() => {
+    const node = document.querySelector(selector) as HTMLElement;
+
+    if (!node) return;
+
+    node.style.offsetPath = `path('${edgePath}')`;
+    node.style.offsetRotate = '0deg';
+    node.style.offsetAnchor = 'center';
+
+    let wasDraggable = getNode(data.node).draggable;
+
+    updateNode(data.node, { draggable: false });
+
+    return () => {
+      node.style.offsetPath = 'none';
+      updateNode(data.node, { draggable: wasDraggable });
+    };
+  }, [selector, edgePath]);
+
+  useEffect(() => {
+    const node = document.querySelector(selector) as HTMLElement;
+
+    if (!node) return;
+
+    const keyframes = [{ offsetDistance: '0%' }, { offsetDistance: '100%' }];
+    const animation = node.animate(keyframes, {
+      duration: 2000,
+      direction: 'alternate',
+      iterations: Infinity,
+    });
+
+    return () => {
+      animation.cancel();
+    };
+  }, [selector]);
+
+  return <BaseEdge id={id} path={edgePath} />;
+}
