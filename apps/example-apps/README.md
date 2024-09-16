e2ccc0fecd7bdf5ba1125644c441b2dabcd1b8e8
Hayleigh Thompson <me@hayleigh.dev>
2024-09-16 13:52:19 +0100

    :recycle: Write a custom vite plugin to handle source.json generation.

--- apps/example-apps/README.md
+++ apps/example-apps/README.md
@@ -4,6 +4,21 @@
 
 ### `pnpm dev`
 
+Starts the vite dev server, with hot reloading. We have a custom plugin configured
+to re-generate the `source.json` file whenever an example is updated, but this
+will *not* hot-reload any external apps that are consuming it.
+
+Also note that changing an example without triggering a hot-reload (eg you have
+navigated to example `A` and then changed some files in example `B`) this wil
+*not* trigger a re-generate of the `source.json` file.
+
 ### `pnpm scaffold`
 
+You can run this script to generate a new React or Svelte example under a given
+path. It will scaffold out all the files necessary for a new example, including
+the entry `index.html` and a minimal React or Svelte component.
+
 ### `pnpm build`
+
+Runs the `vite build` command. The same plugin that we use to generate the
+`source.json` files during dev will be used here at the beginning of the build.
--- apps/example-apps/vite.config.ts
+++ apps/example-apps/vite.config.ts
@@ -1,6 +1,10 @@
 import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite';
+import * as Fs from 'node:fs';
+import * as Path from 'node:path';
+import * as Process from 'node:process';
+
+import { defineConfig, Plugin } from 'vite';
 import { globSync } from 'glob';
 import { resolve } from 'node:path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
@@ -9,7 +13,7 @@ const examplesGlob = './{react,svelte}/**/index.html';
 const examples = globSync(examplesGlob);
 
 export default defineConfig({
-  plugins: [svelte(), react()],
+  plugins: [generatePublicAssets(), svelte(), react()],
   server: {
     host: '0.0.0.0',
     cors: true,
@@ -25,3 +29,75 @@ export default defineConfig({
     },
   },
 });
+
+//
+
+function generatePublicAssets(): Plugin {
+  return {
+    name: 'generate-public-assets',
+    options() {
+      walkExamples(Path.join(Process.cwd(), 'react'));
+      walkExamples(Path.join(Process.cwd(), 'svelte'));
+    },
+    handleHotUpdate({ modules }) {
+      if (!modules.length) return modules;
+      let dir = Path.dirname(modules[0].file!);
+
+      while (!Fs.existsSync(Path.join(dir, 'index.html'))) {
+        console.log(dir);
+        dir = Path.join(dir, '..');
+      }
+
+      walkExamples(dir);
+
+      return modules;
+    },
+  };
+}
+
+const out = Path.join(Process.cwd(), 'public');
+const { dependencies } = JSON.parse(
+  Fs.readFileSync(Path.join(Process.cwd(), 'package.json'), 'utf-8'),
+);
+
+function walkExamples(dir: string) {
+  // If the directory contains a `dependencies.json` file, we know it's an example.
+  // In that case we recursively read every file in the directory and construct
+  // a JSON object mapping file paths to their content.
+  if (Fs.existsSync(Path.join(dir, 'dependencies.json'))) {
+    const relative = Path.relative(Process.cwd(), dir);
+    const source = { files: {}, dependencies: {} };
+
+    for (const file of Fs.readdirSync(dir, {
+      recursive: true,
+      encoding: 'utf-8',
+    })) {
+      const content = Fs.readFileSync(Path.join(dir, file), 'utf-8');
+
+      if (file == 'dependencies.json') {
+        for (const pkg of JSON.parse(content)) {
+          if (!dependencies[pkg]) continue;
+
+          source.dependencies[pkg] = dependencies[pkg];
+        }
+      } else {
+        source.files[file] = content;
+      }
+    }
+
+    const json = JSON.stringify(source, null, 2);
+
+    Fs.mkdirSync(Path.join(out, relative), { recursive: true });
+    Fs.writeFileSync(Path.join(out, relative, 'source.json'), json);
+  }
+
+  // Otherwise, we recursively walk any subdirectories until we find an example
+  // or hit a dead end.
+  else {
+    for (const file of Fs.readdirSync(dir, { withFileTypes: true })) {
+      if (file.isDirectory()) {
+        walkExamples(Path.join(dir, file.name));
+      }
+    }
+  }
+}
