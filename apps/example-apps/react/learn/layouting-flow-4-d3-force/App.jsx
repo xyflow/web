0bae01678c9de7772568dbdc996234fe4f610854
Hayleigh Thompson <me@hayleigh.dev>
2024-10-01 09:15:09 +0100

    :bug: Fixed a bug where d3-force example was not interactive in some browsers.

--- apps/example-apps/react/learn/layouting-flow-4-d3-force/App.jsx
+++ apps/example-apps/react/learn/layouting-flow-4-d3-force/App.jsx
@@ -5,7 +5,7 @@ import {
   forceX,
   forceY,
 } from 'd3-force';
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 import {
   ReactFlow,
   ReactFlowProvider,
@@ -33,6 +33,20 @@ const useLayoutedElements = () => {
   const { getNodes, setNodes, getEdges, fitView } = useReactFlow();
   const initialized = useNodesInitialized();
 
+  // You can use these events if you want the flow to remain interactive while
+  // the simulation is running. The simulation is typically responsible for setting
+  // the position of nodes, but if we have a reference to the node being dragged,
+  // we use that position instead.
+  const draggingNodeRef = useRef(null);
+  const dragEvents = useMemo(
+    () => ({
+      start: (_event, node) => (draggingNodeRef.current = node),
+      drag: (_event, node) => (draggingNodeRef.current = node),
+      stop: () => (draggingNodeRef.current = null),
+    }),
+    [],
+  );
+
   return useMemo(() => {
     let nodes = getNodes().map((node) => ({
       ...node,
@@ -44,7 +58,7 @@ const useLayoutedElements = () => {
 
     // If React Flow hasn't initialized our nodes with a width and height yet, or
     // if there are no nodes in the flow, then we can't run the simulation!
-    if (!initialized || nodes.length === 0) return [false, {}];
+    if (!initialized || nodes.length === 0) return [false, {}, dragEvents];
 
     simulation.nodes(nodes).force(
       'link',
@@ -58,20 +72,26 @@ const useLayoutedElements = () => {
     // running and progresses the simulation one step forward each time.
     const tick = () => {
       getNodes().forEach((node, i) => {
-        const dragging = Boolean(
-          document.querySelector(`[data-id="${node.id}"].dragging`),
-        );
+        const dragging = draggingNodeRef.current?.id === node.id;
 
         // Setting the fx/fy properties of a node tells the simulation to "fix"
         // the node at that position and ignore any forces that would normally
         // cause it to move.
-        nodes[i].fx = dragging ? node.position.x : null;
-        nodes[i].fy = dragging ? node.position.y : null;
+        if (dragging) {
+          nodes[i].fx = draggingNodeRef.current.position.x;
+          nodes[i].fy = draggingNodeRef.current.position.y;
+        } else {
+          delete nodes[i].fx;
+          delete nodes[i].fy;
+        }
       });
 
       simulation.tick();
       setNodes(
-        nodes.map((node) => ({ ...node, position: { x: node.x, y: node.y } })),
+        nodes.map((node) => ({
+          ...node,
+          position: { x: node.fx ?? node.x, y: node.fy ?? node.y },
+        })),
       );
 
       window.requestAnimationFrame(() => {
@@ -79,31 +99,43 @@ const useLayoutedElements = () => {
         // positions before we fit the viewport to the new layout.
         fitView();
 
-        // If the simulation hasn't be stopped, schedule another tick.
+        // If the simulation hasn't been stopped, schedule another tick.
         if (running) tick();
       });
     };
 
     const toggle = () => {
+      if (!running) {
+        getNodes().forEach((node, index) => {
+          let simNode = nodes[index];
+          Object.assign(simNode, node);
+          simNode.x = node.position.x;
+          simNode.y = node.position.y;
+        });
+      }
       running = !running;
       running && window.requestAnimationFrame(tick);
     };
 
     const isRunning = () => running;
 
-    return [true, { toggle, isRunning }];
-  }, [initialized]);
+    return [true, { toggle, isRunning }, dragEvents];
+  }, [initialized, dragEvents, getNodes, getEdges, setNodes, fitView]);
 };
 
 const LayoutFlow = () => {
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
   const [edges, , onEdgesChange] = useEdgesState(initialEdges);
-  const [initialized, { toggle, isRunning }] = useLayoutedElements();
+  const [initialized, { toggle, isRunning }, dragEvents] =
+    useLayoutedElements();
 
   return (
     <ReactFlow
       nodes={nodes}
       edges={edges}
+      onNodeDragStart={dragEvents.start}
+      onNodeDrag={dragEvents.drag}
+      onNodeDragStop={dragEvents.stop}
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
     >
