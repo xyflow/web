8af6e64baed10c8a5bb795e2a8aa4072cf475f3c
Christopher Möller <chrtze@gmail.com>
2024-10-14 16:54:30 +0200

    feat(components): configure tracking

--- apps/ui-components/middleware.ts
+++ apps/ui-components/middleware.ts
@@ -1,14 +1,16 @@
 import type { NextRequest } from "next/server";
 import { track } from "@vercel/analytics/server";
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  await track("ui-components", {
-    url: request.url,
-  });
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/registry") && pathname.endsWith(".json")) {
+    await track("ui-components", {
+      url: pathname,
+    });
+  }
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/registry/:path*",
 };
