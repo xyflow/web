19cd1ab6c7113ee09049736ffa0e26a7d5b10a04
peterkogo <peter@xyflow.com>
2024-10-17 11:22:29 +0200

    moved fonts to xy-shared

--- sites/reactflow.dev/src/pages/_app.tsx
+++ sites/reactflow.dev/src/pages/_app.tsx
@@ -1,28 +1,10 @@
 import { useRouter } from 'next/router';
-import localFont from 'next/font/local';
-import { Fira_Mono } from 'next/font/google';
 import { cn } from '@xyflow/xy-ui';
+
 import { useFathom } from 'xy-shared';
+import { ntDapperFont, fontClassNames } from 'xy-shared/fonts';
 
 import 'styles/global.css';
-
-const ntDapperFont = localFont({
-  src: [
-    { path: '../styles/fonts/NTDapper-regular.woff2', weight: '400' },
-    { path: '../styles/fonts/NTDapper-medium.woff2', weight: '500' },
-    { path: '../styles/fonts/NTDapper-bold.woff2', weight: '700' },
-    { path: '../styles/fonts/NTDapper-black.woff2', weight: '900' },
-  ],
-  variable: '--font-ntdapper',
-});
-
-const firaMonoFont = Fira_Mono({
-  weight: ['400'],
-  subsets: ['latin'],
-  variable: '--font-firamono',
-});
-
-const className = `${ntDapperFont.variable} ${firaMonoFont.variable} font-sans`;
 
 const fathomOptions = {
   id: 'LXMRMWLB',
@@ -42,7 +24,7 @@ export default function App({ Component, pageProps }) {
           font-family: ${ntDapperFont.style.fontFamily}, sans-serif;
         }
       `}</style>
-      <div className={cn(className, routeSegment)}>
+      <div className={cn(fontClassNames, routeSegment)}>
         <Component {...pageProps} />
       </div>
     </>
--- sites/xyflow.com/src/pages/_app.tsx
+++ sites/xyflow.com/src/pages/_app.tsx
@@ -1,26 +1,7 @@
-import localFont from 'next/font/local';
-import { Fira_Mono } from 'next/font/google';
 import { useFathom } from 'xy-shared';
+import { ntDapperFont, fontClassNames } from 'xy-shared/fonts';
 
 import 'styles/global.css';
-
-const ntDapperFont = localFont({
-  src: [
-    { path: '../styles/fonts/NTDapper-regular.woff2', weight: '400' },
-    { path: '../styles/fonts/NTDapper-medium.woff2', weight: '500' },
-    { path: '../styles/fonts/NTDapper-bold.woff2', weight: '700' },
-    { path: '../styles/fonts/NTDapper-black.woff2', weight: '900' },
-  ],
-  variable: '--font-ntdapper',
-});
-
-const firaMonoFont = Fira_Mono({
-  weight: ['400'],
-  subsets: ['latin'],
-  variable: '--font-firamono',
-});
-
-const className = `${ntDapperFont.variable} ${firaMonoFont.variable} font-sans`;
 
 const fathomOptions = {
   id: 'JQTCQNLV',
@@ -36,7 +17,7 @@ export default function App({ Component, pageProps }) {
           font-family: ${ntDapperFont.style.fontFamily}, sans-serif;
         }
       `}</style>
-      <div className={className}>
+      <div className={fontClassNames}>
         <Component {...pageProps} />
       </div>
     </>
