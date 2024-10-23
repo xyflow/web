18ca8fff0354d757b498e9fe17a900daafd39307
peterkogo <peter@xyflow.com>
2024-10-23 18:13:27 +0200

    remove falsey export

--- packages/xy-shared/index.tsx
+++ packages/xy-shared/index.tsx
@@ -35,7 +35,6 @@ export * from './components/node-wrapper';
 export * from './components/timeline-event';
 export * from './components/sidebar-title';
 export * from './components/remote-content';
-export * from './components/head';
 
 export * from './lib/utils';
 
