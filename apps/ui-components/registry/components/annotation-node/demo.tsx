8ed4466364c74a4fa4711d18e53b667b55e0427e
Abbey Yacoe <abbey@xyflow.com>
2024-10-29 13:59:09 +0100

    feat(ui-components): connect annotations to parent nodes

--- apps/ui-components/registry/components/annotation-node/demo.tsx
+++ apps/ui-components/registry/components/annotation-node/demo.tsx
@@ -7,35 +7,69 @@ const nodeTypes = {
   annotationNode: AnnotationNode,
 };
 
+
 const defaultNodes = [
   {
-    id: "1",
+    id: '1a',
+    type: 'input',
+    data: { label: 'Node 1' },
     position: { x: 0, y: 0 },
+  },
+  {
+    id: "1b",
+    position: { x: -150, y: -50 },
+    parentId: "1a",
     data: {
       level: 1,
       label:
         'Annotate your flows any way you like.',
       arrowStyle: {
-        right: 0,
+        right: 30,
         bottom: 0,
-        transform: 'translate(-30px,10px) rotate(-80deg)',
+        transform: 'rotate(-60deg)',
       },
       arrow: "⤹",
     },
     type: "annotationNode",
   },
   {
-    id: "2",
-    position: { x: 150, y: 75 },
-    data: { label: 'Default Node'},
-    type: "default",
+    id: '2a',
+    data: { label: 'Node 2' },
+    position: { x: -100, y: 120 },
   },
+  {
+    id: '3a',
+    data: { label: 'Node 3' },
+    position: { x: 100, y: 120 },
+  },
+  {
+    id: "3b",
+    position: { x: 100, y: -120 },
+    parentId: "3a",
+    data: {
+      level: 2,
+      label:
+        'Connect annotations to nodes to adjust interactively.',
+      arrowStyle: {
+        right: 20,
+        bottom: -10,
+        transform: 'rotate(-150deg) scaleY(-1)',
+      },
+      arrow: "⤹",
+    },
+    type: "annotationNode",
+  },
+];
+
+const defaultEdges = [
+  { id: 'e1-2', source: '1a', target: '2a' },
+  { id: 'e1-3', source: '1a', target: '3a' },
 ];
 
 export default function AnnotationNodeDemo() {
   return (
     <div className="h-full w-full">
-      <ReactFlow nodeTypes={nodeTypes} defaultNodes={defaultNodes} fitView>
+      <ReactFlow nodeTypes={nodeTypes} defaultNodes={defaultNodes} defaultEdges={defaultEdges} fitView>
         <Background />
       </ReactFlow>
     </div>
