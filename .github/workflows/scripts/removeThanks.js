3e525b623e5aed20b36ac5391fd03192e3e2a6e6
peterkogo <peter@xyflow.com>
2024-10-30 13:33:03 +0100

    remove unused scripts

--- .github/workflows/scripts/removeThanks.js
+++
@@ -1,8 +0,0 @@
-const regex =
-  /Thanks \[@(?:moklick|peterkogo|hayleigh-dot-dev|chrtze|bcakmakoglu)\]\(.*\)! \- */g;
-
-(async () => {
-  const body = process.env.BODY;
-  const newBody = body.replaceAll(regex, '');
-  console.log(newBody);
-})();
--- .github/workflows/scripts/removeUpdateDeps.js
+++
@@ -1,7 +0,0 @@
-const regex = /- *Updated dependencies.*/gs;
-
-(async () => {
-  const body = process.env.BODY;
-  const newBody = body.replaceAll(regex, '');
-  console.log(newBody);
-})();
