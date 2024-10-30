617dfde2a7c55c5b94c7b44451215397f1673b60
Christopher Möller <chrtze@gmail.com>
2024-10-30 14:10:58 +0100

    refactor(components): put components into subfolders

---
+++ sites/reactflow.dev/src/pages/components/nodes/_meta.tsx
@@ -0,0 +1,8 @@
+export default {
+  tooltip: 'Tooltip',
+  placeholder: 'Placeholder',
+  'database-schema': 'Database Schema',
+  annotation: 'Annotation',
+  'labeled-group': 'Group with Label',
+  'base-node': 'Base Node',
+};
