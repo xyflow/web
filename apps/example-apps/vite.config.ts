cb1b5f0f5b33d1dc6c97259687e154877b60c197
Abbey Yacoe <abbey@xyflow.com>
2024-11-05 14:20:52 +0100

    fix(overview-example): test changing vite preprosser options

--- apps/example-apps/vite.config.ts
+++ apps/example-apps/vite.config.ts
@@ -31,6 +31,13 @@ export default defineConfig({
       },
     },
   },
+  css: {
+    preprocessorOptions: {
+      css: {
+        order: 'prepend', // Ensures Vite processes CSS in a consistent order
+      },
+    },
+  },
 });
 
 //
