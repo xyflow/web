657f5a454f4a67e92156eb4b201f9ba3bc8ecc5c
Abbey Yacoe <abbey@xyflow.com>
2024-11-05 13:01:17 +0100

    fix(annotation-node): add optional parameters; fix type definitions

--- apps/ui-components/registry/components/annotation-node/index.tsx
+++ apps/ui-components/registry/components/annotation-node/index.tsx
@@ -3,24 +3,22 @@ import { CSSProperties } from "react";
 
 type AnnotationNode = Node<{
   label: string;
-  level: number;
+  level?: number;
   arrow: string;
-  arrowStyle: CSSProperties; 
+  arrowStyle?: CSSProperties; 
 }>;
 
 export function AnnotationNode({ data }: NodeProps<AnnotationNode>) {
   return (
     <div className="relative flex max-w-[180px] items-start p-2 text-sm text-secondary-foreground">
-      <div className="mr-1 leading-snug">{data.level}.</div>
+      {typeof data.level === 'number' && (
+        <div className="mr-1 leading-snug">{data.level}.</div>
+      )}
       <div className="leading-snug">{data.label}</div>
       {data.arrowStyle && (
         <div
           className="absolute text-2xl"
-          style={{
-            right: data.arrowStyle.right,
-            bottom: data.arrowStyle.bottom,
-            transform: data.arrowStyle.transform,
-          }}
+          style={ data.arrowStyle }
         >
           {data.arrow}
         </div>
