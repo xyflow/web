8776b78fa5075b0432f2472cdac49d5fd7ca9349
Abbey Yacoe <abbey@xyflow.com>
2024-11-05 15:25:42 +0100

    fix(overview-example): try loading css in html

--- apps/example-apps/react/examples/misc/overview/index.jsx
+++ apps/example-apps/react/examples/misc/overview/index.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-import './index.css';
+// import './index.css';
 
 const container = document.querySelector('#app');
 const root = createRoot(container);
