e9cc0793da617b2ca5213b1a19dd0a01416c46d5
Abbey Yacoe <abbey@xyflow.com>
2024-11-12 16:27:28 +0100

    Update index.ts

--- packages/xy-shared/utils/index.ts
+++ packages/xy-shared/utils/index.ts
@@ -1,4 +1,4 @@
 export const examplesUrl =
   process.env.VERCEL_ENV === 'preview' && process.env.VERCEL_GIT_COMMIT_REF
     ? `https://example-apps-git-${process.env.VERCEL_GIT_COMMIT_REF}-xyflow.vercel.app`
-    : process.env.NEXT_PUBLIC_EXAMPLES_URL;
+    : process.env.NEXT_PUBLIC_EXAMPLES_URL || 'https://example-apps.xyflow.com';
