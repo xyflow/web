dea8ce5bfa8b1af4ef1a9eac9016c3caea186fc7
Abbey Yacoe <abbey@xyflow.com>
2024-11-12 12:48:58 +0100

    Update vite.config.ts

--- apps/example-apps/vite.config.ts
+++ apps/example-apps/vite.config.ts
@@ -10,7 +10,7 @@ import { resolve } from 'node:path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // make index.css entrypoints, too to fix css import odering issues
-const examplesGlob = './{react,svelte}/**/{index.html, index.css}';
+const examplesGlob = './{react,svelte}/**/{index.html,index.css}';
 const examples = globSync(examplesGlob);
 
 export default defineConfig({
