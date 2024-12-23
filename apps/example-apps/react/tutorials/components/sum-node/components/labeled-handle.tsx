864448e3aa684e00b6d3ed1ed441612d52f31350
Hayleigh Thompson <me@hayleigh.dev>
2024-12-23 23:31:40 +0100

    :sparkles: Create examples for components guide.

---
+++ apps/example-apps/react/tutorials/components/sum-node/components/labeled-handle.tsx
@@ -0,0 +1,46 @@
+'use client';
+import React from 'react';
+import { cn } from '../lib/utils';
+import type { HandleProps } from '@xyflow/react';
+
+import { BaseHandle } from './base-handle';
+
+function getFlexDirection(position: string) {
+  const flexDirection =
+    position === 'top' || position === 'bottom' ? 'flex-col' : 'flex-row';
+
+  switch (position) {
+    case 'bottom':
+    case 'right':
+      return flexDirection + '-reverse justify-end';
+    default:
+      return flexDirection;
+  }
+}
+
+const LabeledHandle = React.forwardRef<
+  HTMLDivElement,
+  HandleProps &
+    React.HTMLAttributes<HTMLDivElement> & {
+      title: string;
+      handleClassName?: string;
+      labelClassName?: string;
+    }
+>(({ className, labelClassName, title, position, ...props }, ref) => (
+  <div
+    ref={ref}
+    title={title}
+    className={cn(
+      'relative flex items-center',
+      getFlexDirection(position),
+      className,
+    )}
+  >
+    <BaseHandle position={position} {...props} />
+    <label className={`px-3 text-foreground ${labelClassName}`}>{title}</label>
+  </div>
+));
+
+LabeledHandle.displayName = 'LabeledHandle';
+
+export { LabeledHandle };
