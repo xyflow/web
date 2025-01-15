f24ccce8bf21afd6789802e54bb247ea3f3d9368
peterkogo <peter@xyflow.com>
2025-01-15 13:21:35 +0100

    fix some example docs and custom node example

--- apps/example-apps/svelte/guides/custom-nodes/a/App.svelte
+++ apps/example-apps/svelte/guides/custom-nodes/a/App.svelte
@@ -1,21 +1,28 @@
+<script module>
+  export class SharedState {
+    color = $state('#ff0000');
+  }
+</script>
+
 <script>
-  import { writable } from 'svelte/store';
   import { SvelteFlow } from '@xyflow/svelte';
 
   import ColorPickerNode from './ColorPickerNode.svelte';
 
   import '@xyflow/svelte/dist/style.css';
 
-  const nodes = writable([
+  const sharedState = new SharedState();
+
+  let nodes = $state.raw([
     {
       id: '1',
       type: 'colorPicker',
-      data: { color: writable('#ff4000') },
+      data: { sharedState },
       position: { x: 0, y: 0 },
     },
   ]);
 
-  const edges = writable([]);
+  let edges = $state.raw([]);
 
   const nodeTypes = {
     colorPicker: ColorPickerNode,
--- apps/example-apps/svelte/guides/custom-nodes/a/ColorPickerNode.svelte
+++ apps/example-apps/svelte/guides/custom-nodes/a/ColorPickerNode.svelte
@@ -1,25 +1,16 @@
 <script lang="ts">
-  import type { Writable } from 'svelte/store';
-  import { Handle, Position, type NodeProps } from '@xyflow/svelte';
+  import { Handle, Node, Position, type NodeProps } from '@xyflow/svelte';
+  import { SharedState } from './App.svelte';
 
-  type $$Props = NodeProps;
-
-  export let data: { color: Writable<string> };
-
-  const { color } = data;
+  let { data }: NodeProps<Node<{ sharedState: SharedState }>> = $props();
 </script>
 
 <div class="colorpicker">
   <Handle type="target" position={Position.Left} />
   <div>
-    color: <strong>{$color}</strong>
+    color: <strong>{data.sharedState.color}</strong>
   </div>
-  <input
-    class="nodrag"
-    type="color"
-    on:input={(evt) => data.color.set(evt.target?.value)}
-    value={$color}
-  />
+  <input class="nodrag" type="color" bind:value={data.sharedState.color} />
   <Handle type="source" position={Position.Right} />
 </div>
 
