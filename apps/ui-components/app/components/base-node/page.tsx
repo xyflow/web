25bf4b161b5bc0a3ac19d37a67b95911a3dbc538
Abbey Yacoe <abbey@xyflow.com>
2025-01-30 15:08:41 +0100

    refactor(base-node): adjust component/app code display

--- apps/ui-components/app/components/base-node/page.tsx
+++ apps/ui-components/app/components/base-node/page.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import { Background, ReactFlow } from "@xyflow/react";
 import DemoWrapper from "@/components/demo-wrapper";
-import Demo from "@/registry/components/base-node/demo";
+import BaseNode from "@/registry/components/base-node/demo";
+
+const nodeTypes = {
+  baseNode: BaseNode,
+};
+
+const defaultNodes = [
+  {
+    id: "1",
+    position: { x: 200, y: 200 },
+    data: {},
+    type: "baseNode",
+  },
+];
 
 export default function DemoPage() {
   return (
     <DemoWrapper>
-      <Demo />
+      <div className="h-full w-full">
+        <ReactFlow defaultNodes={defaultNodes} nodeTypes={nodeTypes} fitView>
+          <Background />
+        </ReactFlow>
+      </div>
     </DemoWrapper>
   );
 }
--- apps/ui-components/registry/components/base-node/demo.tsx
+++ apps/ui-components/registry/components/base-node/demo.tsx
@@ -1,48 +1,14 @@
-"use client";
+import { memo } from "react";
 
-import {
-  Background,
-  Handle,
-  NodeProps,
-  Position,
-  ReactFlow,
-  Node,
-} from "@xyflow/react";
+import { NodeProps } from "@xyflow/react";
 import { BaseNode } from "@/registry/components/base-node";
 
-const defaultNodes = [
-  {
-    id: "1",
-    position: { x: 0, y: 0 },
-    type: "customNode",
-    data: {
-      label: "Custom Node",
-    },
-  },
-];
-
-const nodeTypes = {
-  customNode: CustomNode,
-};
-
-function CustomNode({ selected, data }: NodeProps<Node<{ label: string }>>) {
+const CustomNode = memo(({ selected }: NodeProps) => {
   return (
     <BaseNode selected={selected}>
-      <>
-        {data.label}
-        <Handle type="source" position={Position.Right} />
-        <Handle type="target" position={Position.Left} />
-      </>
+      <div>Base Node</div>
     </BaseNode>
   );
-}
+});
 
-export default function Demo() {
-  return (
-    <div className="h-full w-full">
-      <ReactFlow defaultNodes={defaultNodes} nodeTypes={nodeTypes} fitView>
-        <Background />
-      </ReactFlow>
-    </div>
-  );
-}
+export default CustomNode;
