f0fb645524f597e9e5474264779a660f85d35b2c
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-10 21:36:28 +0700

    survey 2024

---
+++ sites/reactflow.dev/src/app/developer-survey-2024/page.client.tsx
@@ -0,0 +1,6 @@
+'use client'
+
+import { Widget } from '@typeform/embed-react';
+export function Page() {
+  return <Widget id="ZhfGz9nZ" className="h-screen w-screen" />;
+}
---
+++ sites/reactflow.dev/src/app/developer-survey-2024/page.tsx
@@ -0,0 +1,7 @@
+import { Metadata } from 'next';
+export { Page as default } from './page.client';
+
+export const metadata: Metadata = {
+  title: 'React flow Developer Survey 2024'
+}
+
