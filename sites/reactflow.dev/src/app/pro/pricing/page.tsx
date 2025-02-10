cfd8e1e0a7f027d32940f33fb3cd9f013dbb9c03
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-10 22:35:18 +0700

    pro/pricing

--- sites/reactflow.dev/src/app/pro/pricing/page.tsx
+++ sites/reactflow.dev/src/app/pro/pricing/page.tsx
@@ -1,6 +1,12 @@
 import { PricingTable } from '@xyflow/xy-ui';
 import { BaseLayout, Hero, SubscribeSection } from 'xy-shared';
 import { SparklesIcon } from '@heroicons/react/24/outline';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'React Flow Pro Pricing',
+  description: 'Pricing plans for a React Flow Pro subscription.'
+}
 
 export default function ReactFlowProPricing() {
   return (
@@ -9,7 +15,7 @@ export default function ReactFlowProPricing() {
         title="Pricing"
         subtitle="Your subscription goes directly towards the development and maintenance of React Flow and allows us to keep the library independent and open source."
         kicker="React Flow Pro"
-        kickerIcon={SparklesIcon}
+        kickerIcon={<SparklesIcon/>}
         align="center"
         backgroundVariant="image"
       />
