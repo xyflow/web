7ceaae55d40b4ee57ace0326d61075600fbc38ba
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-12 21:43:35 +0700

    more

--- sites/reactflow.dev/src/utils/index.ts
+++ sites/reactflow.dev/src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './routes';
 export * from './get-last-changelog';
 export * from './fetch-github-npm-stats';
+export * from './fetch-shadcn-component';
 // export * from './use-is-pro'; // DO NOT EXPORT
