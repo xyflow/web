b1df7c2146f477926940e046b4f0cb107c53802e
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-16 21:01:46 +0700

    icon.ico and apple-icon.png

--- sites/xyflow.com/theme.config.tsx
+++ sites/xyflow.com/theme.config.tsx
@@ -1,13 +1,7 @@
-import { useConfig } from 'nextra-theme-docs';
-
 const baseUrl =
   process.env.NODE_ENV === 'production'
     ? 'https://xyflow.com'
     : 'http://localhost:3001';
-
-const faviconUrl = `${baseUrl}/img/favicon.ico`;
-const faviconAppletouchUrl = `${baseUrl}/img/apple-touch-icon.png`;
-const faviconSvgUrl = `${baseUrl}/img/favicon.svg`;
 
 export default {
   // this is necessary to hide the github icon
@@ -16,8 +10,6 @@ export default {
     toggleButton: false,
   },
   head() {
-    const { frontMatter } = useConfig();
-
     const hasImage =
       frontMatter.image && frontMatter.imageWidth && frontMatter.imageHeight;
 
@@ -26,27 +18,12 @@ export default {
       width: frontMatter.imageWidth,
       height: frontMatter.imageHeight,
     };
-
-    // We are not allowed to render components inside head!
-    // https://github.com/shuding/nextra/issues/3529
     return (
       <>
-        <link rel="icon" href={faviconUrl} />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={faviconAppletouchUrl}
-        />
-        <link rel="icon" href={faviconSvgUrl} type="image/svg+xml" />
-
-        {ogImage && (
-          <>
-            <meta property="og:image" content={ogImage.url} />
-            <meta property="og:image:alt" content="Teaser" />
-            <meta property="og:image:width" content={ogImage.width} />
-            <meta property="og:image:height" content={ogImage.height} />
-          </>
-        )}
+        <meta property="og:image" content={ogImage.url} />
+        <meta property="og:image:alt" content="Teaser" />
+        <meta property="og:image:width" content={ogImage.width} />
+        <meta property="og:image:height" content={ogImage.height} />
       </>
     );
   },
