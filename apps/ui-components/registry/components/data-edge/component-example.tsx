f50ad52ab686c64329e3e0d2e82d576f12cad104
Abbey Yacoe <abbey@xyflow.com>
2025-02-18 15:17:03 +0100

    refactor(data-edge): simplify demo component

--- apps/ui-components/registry/components/data-edge/component-example.tsx
+++ apps/ui-components/registry/components/data-edge/component-example.tsx
@@ -1,44 +1,29 @@
-"use client";
+import { Handle, NodeProps, Position, useReactFlow, Node } from "@xyflow/react";
 
-import { Handle, Node, NodeProps, Position, useReactFlow } from "@xyflow/react";
-
-import { useCallback, memo } from "react";
+import { memo } from "react";
 import { BaseNode } from "@/registry/components/base-node";
-import { Button } from "@/components/ui/button";
-import { Minus, Plus } from "lucide-react";
+import { Slider } from "@/components/ui/slider";
 
 export type CounterNodeType = Node<{ value: number }>;
 
 export const CounterNode = memo(({ id, data }: NodeProps<CounterNodeType>) => {
   const { updateNodeData } = useReactFlow();
-  const handleIncr = useCallback(() => {
-    updateNodeData(id, ({ data }) => {
-      if ("value" in data && typeof data.value === "number") {
-        return { ...data, value: data.value + 1 };
-      }
-
-      return data;
-    });
-  }, [id, updateNodeData]);
-  const handleDecr = useCallback(() => {
-    updateNodeData(id, ({ data }) => {
-      if ("value" in data && typeof data.value === "number") {
-        return { ...data, value: data.value - 1 };
-      }
-
-      return data;
-    });
-  }, [id, updateNodeData]);
 
   return (
-    <BaseNode className="flex items-center gap-4">
-      <Button onClick={handleDecr} className="nopan nodrag size-6 p-1">
-        <Minus />
-      </Button>
-      <pre>{String(data.value).padStart(2, " ")}</pre>
-      <Button onClick={handleIncr} className="nopan nodrag size-6 p-1">
-        <Plus />
-      </Button>
+    <BaseNode>
+      <Slider
+        value={[data.value]}
+        min={0}
+        max={100}
+        step={1}
+        className="nopan nodrag w-24"
+        onValueChange={([value]) => {
+          updateNodeData(id, (node) => ({
+            ...node.data,
+            value,
+          }));
+        }}
+      />
       <Handle type="source" position={Position.Bottom} />
     </BaseNode>
   );
