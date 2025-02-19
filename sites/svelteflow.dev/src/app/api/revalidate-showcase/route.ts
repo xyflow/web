42a4f3c6b1383cd8ec7a1194e3dcb11e35fe2b33
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-02-19 09:00:04 +0700

    fix api routes

--- sites/svelteflow.dev/src/app/api/revalidate-showcase/route.ts
+++ sites/svelteflow.dev/src/app/api/revalidate-showcase/route.ts
@@ -1,11 +1,13 @@
-export async function GET(req: Request) {
+import { revalidatePath } from 'next/cache';
+
+export async function GET(_req: Request) {
   try {
     // This should be the actual path not a rewritten path
     // e.g. for "/posts/[id]" this should be "/posts/1"
-    // TODO: await res.revalidate('/showcase');
+    await revalidatePath('/showcase');
     return Response.json({ revalidated: true });
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.error(error);
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     return new Response('Failed to revalidate', { status: 500 });
