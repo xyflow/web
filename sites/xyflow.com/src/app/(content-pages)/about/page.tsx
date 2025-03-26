289203c3025f160b07b1e48f17ef6b2c943b93f3
Christopher Möller <chrtze@gmail.com>
2025-03-26 13:30:35 +0100

    change about texts, add collaborator images

--- sites/xyflow.com/src/app/(content-pages)/about/page.tsx
+++ sites/xyflow.com/src/app/(content-pages)/about/page.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { Metadata } from 'next';
-import { UserGroupIcon } from '@heroicons/react/24/outline';
+import { UserGroupIcon, GlobeEuropeAfricaIcon } from '@heroicons/react/24/outline';
 import { ArrowRightCircleIcon } from '@heroicons/react/24/solid';
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
@@ -19,6 +19,8 @@ import moritz from '../../../../public/img/moritz.jpg';
 import peter from '../../../../public/img/peter.jpg';
 import burak from '../../../../public/img/burak.jpg';
 import abbey from '../../../../public/img/abbey.jpg';
+import dima from '../../../../public/img/dima.jpg';
+import usman from '../../../../public/img/usman.jpg';
 
 export const metadata: Metadata = {
   title: 'About Us',
@@ -77,7 +79,8 @@ const Page: FC = () => {
         <TeamCard
           name="Christopher"
           teamPic={christopher}
-          description="Christopher doesn't remember anything before 2015. He woke up on the shores of the Spree, where he wandered into the offices of the newspaper Zeit and they gave him a job as a front-end developer, mistaking him for the potential new hire. He quickly rose through the ranks there. Now he codes at xyflow, hoping one day he will remember how he got here, and what the meaning of all this is anyway."
+          role="Co-Founder"
+          description="Christopher is handling support, finances, and strategy. He keeps things organized and makes sure everything runs smoothly behind the scenes."
           links={[
             { linkName: 'Twitter', route: 'https://twitter.com/chrtze' },
             { linkName: 'Github', route: 'https://github.com/chrtze' },
@@ -85,31 +88,34 @@ const Page: FC = () => {
         />
         <TeamCard
           name="Moritz"
+          role="Co-Founder"
           teamPic={moritz}
-          description="Moritz was raised in the depths of Teutoburger Wald by a pack of wolves, learning how to hunt, survive, and develop front-end applications. He abandoned his pack after a disagreement in 2019, where he fled to the streets of Berlin. He now maintains React Flow, and dreams of one day rejoining his pack."
+          description="Passionate about open-source development, Moritz is responsible for maintaining and improving all of xyflow's open-source projects. His technical expertise keeps the project moving forward."
           links={[
             { linkName: 'Twitter', route: 'https://twitter.com/moklick' },
             { linkName: 'Github', route: 'https://github.com/moklick' },
           ]}
         />
         <TeamCard
-          name="Peter"
-          teamPic={peter}
-          description="Peter has been directing a mockumentary of the xyflow team a la The Office since he broke into our building during the summer of 2023. Luckily for our team, the footage will never be released since he never asked us to sign a talent waiver, and none of us have the heart to tell him. Once he's done editing the footage (he keeps saying 'one more week'), we're hoping he'll make some contributions to the Svelte Flow library and docs."
-          links={[{ linkName: 'Github', route: 'https://github.com/peterkogo' }]}
+          name="Abbey"
+          role="Software Engineer"
+          teamPic={abbey}
+          description="Since joining in 2024, Abbey has been focused on refining xyflow's documentation, building React Flow components and improving the overall experience of our websites. She is dedicated to making xyflow more accessible and user-friendly."
+          links={[{ linkName: 'Github', route: 'https://github.com/printerscanner' }]}
         />
         <TeamCard
-          name="Abbey"
-          teamPic={abbey}
-          description="Abbey spent the first twenty years of her career finely crafting an innovative sea bass fishing technique. The world of sea bass became all too political for her, but luckily she found xyflow, which she wrongly assumed was another commercial fishing company. She is still figuring out how to turn on a computer, but once she does, she is excited to start contributing to the team."
-          links={[{ linkName: 'Github', route: 'https://github.com/printerscanner' }]}
+          name="Peter"
+          role="Software Engineer"
+          teamPic={peter}
+          description="Peter joined xyflow in 2023, bringing his enthusiasm for Svelte and React development. He primarily works on Svelte Flow and his dedication ensures the project stays polished and responsive to user needs."
+          links={[{ linkName: 'Github', route: 'https://github.com/peterkogo' }]}
         />
       </ContentGrid>
 
       <div className="flex justify-center mt-16 lg:mt-32">
         <div className="max-w-3xl w-full">
           <div className="flex justify-center items-center mb-2">
-            <UserGroupIcon className="w-8 h-8" />
+            <GlobeEuropeAfricaIcon className="w-8 h-8" />
           </div>
           <Heading className="text-center">Collaborators</Heading>
           <Text size="lg" className="text-center text-light  mt-2">
@@ -121,9 +127,9 @@ const Page: FC = () => {
       <ContentGrid className="mt-16 lg:mt-20 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
         <TeamCard
           name="Burak"
+          role="Maintainer of Vue Flow"
           teamPic={burak}
-          description="Burak created Vue Flow while aboard a pirate ship, hoping the motley crew could better organize themselves with a node-based cleaning schedule. Even after his ship was commandeered on the shores of Hamburg, he continues to seek those harsh bone-breaking conditions on land through Brazilian Jiu Jitsu, skateboarding, and open source software development.
-          "
+          description="Burak is the maintainer of Vue Flow, bringing his expertise to xyflow’s ecosystem. He actively supports users on Discord and GitHub, helping developers troubleshoot issues and improve their workflow with Vue Flow."
           links={[
             { linkName: 'Github', route: 'https://github.com/bcakmakoglu' },
             {
@@ -134,31 +140,37 @@ const Page: FC = () => {
         />
         <TeamCard
           name="Usman"
-          teamPic={burak}
-          description="Burak created Vue Flow while aboard a pirate ship, hoping the motley crew could better organize themselves with a node-based cleaning schedule. Even after his ship was commandeered on the shores of Hamburg, he continues to seek those harsh bone-breaking conditions on land through Brazilian Jiu Jitsu, skateboarding, and open source software development.
-          "
-          links={[
-            { linkName: 'Github', route: 'https://github.com/bcakmakoglu' },
-            {
-              linkName: 'Website',
-              route: 'https://www.scheissaufdeinmarketingdusalamiwurst.de/',
-            },
-          ]}
+          role="Discord Moderator"
+          teamPic={usman}
+          description="Working with xyflow since 2025, Usman plays a key role in community support. He provides assistance on Discord and creates video content that helps users understand and make the most of React Flow."
+          links={[{ linkName: 'Github', route: 'https://github.com/usmanabdurrehman' }]}
         />
         <TeamCard
-          name="Facu"
-          teamPic={burak}
-          description="Burak created Vue Flow while aboard a pirate ship, hoping the motley crew could better organize themselves with a node-based cleaning schedule. Even after his ship was commandeered on the shores of Hamburg, he continues to seek those harsh bone-breaking conditions on land through Brazilian Jiu Jitsu, skateboarding, and open source software development.
-          "
+          name="Dima"
+          role="Maintainer of Nextra"
+          teamPic={dima}
+          description="As the maintainer of Nextra, the framework powering xyflow’s documentation, Dima continuously enhances the platform. He works on adding new features to xyflow’s websites, ensuring a smooth and user-friendly experience."
           links={[
-            { linkName: 'Github', route: 'https://github.com/bcakmakoglu' },
+            { linkName: 'Github', route: 'https://github.com/dimaMachina' },
             {
               linkName: 'Website',
-              route: 'https://www.scheissaufdeinmarketingdusalamiwurst.de/',
+              route: 'https://dimamachina.com/',
             },
           ]}
         />
       </ContentGrid>
+
+      <div className="flex justify-center mt-16 lg:mt-32">
+        <div className="max-w-3xl w-full">
+          <div className="flex justify-center items-center mb-2">
+            <UserGroupIcon className="w-8 h-8" />
+          </div>
+          <Heading className="text-center">Work with us!</Heading>
+          <Text size="lg" className="text-center text-light  mt-2">
+            Besides our core team, we are working with these external contributors
+          </Text>
+        </div>
+      </div>
     </BaseLayout>
   );
 };
@@ -166,34 +178,36 @@ const Page: FC = () => {
 function TeamCard({
   name,
   description,
+  role,
   links,
   teamPic,
 }: {
   name: string;
+  role: string;
   description: string;
   teamPic?: StaticImageData;
-
   links?: Array<{ route: string; linkName: string }>;
 }) {
   return (
-    <ContentGridItem>
+    <ContentGridItem className="flex flex-col px-8 py-10 lg:py-16">
       {teamPic && (
-        <Image
-          src={teamPic}
-          alt={name}
-          className="mb-8 lg:-mt-8"
-          width="260"
-          height="304"
-        />
+        <div className="mb-8 lg:-mt-8 w-full aspect-[3.5/4] max-w-[320px] relative">
+          <Image
+            objectFit="cover"
+            src={teamPic}
+            alt={name}
+            className="mb-8 lg:-mt-8"
+            fill
+          />
+        </div>
       )}
-      {!teamPic && <div className="mb-8 lg:-mt-8" style={{ width: 260, height: 304 }} />}
-
       <Heading as="p" size="sm" className="mb-4">
         {name}
       </Heading>
-      <Text className="mb-4">{description}</Text>
+      <Text className="mb-4">{role}</Text>
+      <Text className="mb-8">{description}</Text>
 
-      <div className="flex items-center space-x-4 mt-8">
+      <div className="flex items-center space-x-4 mt-auto">
         {links &&
           links.map((link) => {
             return (
