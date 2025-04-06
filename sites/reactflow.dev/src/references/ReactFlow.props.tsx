b50b036141a5cd29b1a93dd6ba6f3eb7699796d8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-04-06 03:06:10 +0200

    commonProps

--- sites/reactflow.dev/src/references/ReactFlow.props.tsx
+++ sites/reactflow.dev/src/references/ReactFlow.props.tsx
@@ -1,4 +1,3 @@
-import { type PropsTableProps } from 'xy-shared';
 import type { FC } from 'react';
 import { useMDXComponents as getMDXComponents } from '@/mdx-components';
 
@@ -133,12 +132,11 @@ export const ReactFlowAPIProps: FC<{ group: keyof typeof FIELDS | 'common' }> = 
       .flat()
       .map((v) => `"${v}"`)
       .join('|');
+    const groupedProps = "Omit<React.ComponentProps<'div'>, 'onError'>"
 
     myType = `
-type GroupedProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'onError'>
-      
-type $ = Omit<ReactFlowProps, ${omittedFields} | keyof GroupedProps> & {
-  '...props': GroupedProps
+type $ = Omit<ReactFlowProps, ${omittedFields} | keyof ${groupedProps}> & {
+  '...props': ${groupedProps}
 }`;
   } else {
     const pickedFields = FIELDS[group].map((v) => `"${v}"`).join('|');
@@ -157,123 +155,4 @@ ${myType}
 export default $`}
     />
   );
-};
-
-export const commonProps: PropsTableProps = {
-  props: [
-    {
-      name: 'nodes',
-      type: 'Node[]',
-      default: '[]',
-      description: 'An array of nodes to render in a controlled flow.',
-    },
-    {
-      name: 'edges',
-      type: 'Edge[]',
-      default: '[]',
-      description: 'An array of edges to render in a controlled flow.',
-    },
-    {
-      name: 'defaultNodes',
-      type: 'Node[]',
-      default: '[]',
-      description: 'The initial nodes to render in an uncontrolled flow.',
-    },
-    {
-      name: 'defaultEdges',
-      type: 'Edge[]',
-      default: '[]',
-      description: 'The initial edges to render in an uncontrolled flow.',
-    },
-    {
-      name: 'onNodesChange',
-      type: '(changes: NodeChange[]) => void',
-      description: `Use this event handler to add interactivity to a controlled
-      flow. It is called on node drag, select, and move.`,
-    },
-    {
-      name: 'onEdgesChange',
-      type: '(changes: EdgeChange[]) => void',
-      description: `Use this event handler to add interactivity to a controlled
-      flow. It is called on edge select and remove.`,
-    },
-    {
-      name: 'onConnect',
-      type: '(connection: Connection) => void',
-      description: `When a connection line is completed and two nodes are connected
-      by the user, this event fires with the new connection. You can use the
-      addEdge utility to convert the connection to a complete edge.`,
-    },
-    {
-      name: 'nodeTypes',
-      type: 'Record<String, React.ComponentType<NodeProps>>',
-      default: `{
-  input: InputNode,
-  default: DefaultNode,
-  output: OutputNode
-  group: GroupNode
-}`,
-      description: `If you want to use custom nodes in your flow, you need to let
-      React Flow know about them. When rendering a new node, React Flow will look
-      up that node's type in this object and render the corresponding component.`,
-    },
-    {
-      name: 'edgeTypes',
-      type: 'Record<String, React.ComponentType<EdgeProps>>',
-      default: `{
-  default: BezierEdge,
-  straight: StraightEdge,
-  step: StepEdge,
-  smoothstep: SmoothStepEdge
-  simplebezier: SimpleBezier
-}`,
-      description: `As with node types, this prop lets you use custom edges in
-      your flow by mapping edge types to React components.`,
-    },
-    {
-      name: 'nodeOrigin',
-      type: '[number, number]',
-      default: '[0,0]',
-      description: `The origin of the node to use when placing it in the flow
-      or looking up its x and y position. An origin of [0,0] means that a node's
-      top left corner will be placed at the x and y position.`,
-    },
-    {
-      name: 'nodeDragThreshold',
-      type: 'number',
-      default: '1',
-      description: `
-          With a threshold greater than zero you can delay node drag events. If threshold equals 1,
-          you need to drag the node 1 pixel before a drag event is fired. 1 is the default values, so
-          clicks don't trigger drag events.
-        `,
-    },
-    {
-      name: 'nodeClickDistance',
-      type: 'number',
-      default: `0`,
-      description: `The max distance between mousedown/up that will trigger a click.`,
-    },
-    {
-      name: 'paneClickDistance',
-      type: 'number',
-      default: `0`,
-      description: `The max distance between mousedown/up that will trigger a click.`,
-    },
-    { name: 'style', type: 'React.CSSProperties' },
-    { name: 'className', type: 'string' },
-    {
-      name: 'proOptions',
-      type: 'ProOptions',
-      description: `Our pro options are configuration settings intended for our
-      Pro subscribers. Anyone is free to use them, though!`,
-    },
-    {
-      name: 'colorMode',
-      type: '"system" | "light" | "dark"',
-      default: '"system"',
-      description: `React Flow has 2 built-in color themes: light and dark.
-      By default it will try to adopt the users systems color theme.`,
-    },
-  ],
 };
