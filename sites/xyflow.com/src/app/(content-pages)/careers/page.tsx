936811ff999487692ce1fea1ce4cbcf2dc7a52f0
Christopher Möller <chrtze@gmail.com>
2025-04-07 10:05:40 +0200

    restructure team page (#717)
    * restructure team page
    
    * docs(about): update story section
    
    * change about texts, add collaborator images
    
    * shrink image
    
    * about page: add collaborator
    
    * revert to ironic about texts for the core team
    
    ---------
    
    Co-authored-by: Abbey Yacoe <abbey@xyflow.com>

--- sites/xyflow.com/src/app/(content-pages)/careers/page.tsx
+++ sites/xyflow.com/src/app/(content-pages)/careers/page.tsx
@@ -58,7 +58,7 @@ const Page: FC = () => {
   return (
     <BaseLayout className="space-y-24">
       <Hero
-        kicker="About Us"
+        kicker="Join Us"
         title="Careers at xyflow"
         subtitle="Learn about how we work and see our open positions"
         align="center"
@@ -74,9 +74,9 @@ const Page: FC = () => {
           <Link href="https://svelteflow.dev" className="text-pink-500">
             Svelte Flow
           </Link>
-          , which are used by thousands of developers. We are expanding our
-          Berlin-based product development team and are looking for talented web
-          developers to join us. Below is a list of our current job openings.
+          , which are used by thousands of developers. We are expanding our Berlin-based
+          product development team and are looking for talented web developers to join us.
+          Below is a list of our current job openings.
         </Text>
       </Section>
 
@@ -90,8 +90,8 @@ const Page: FC = () => {
         </Heading>
 
         <Text size="lg" className="leading-8">
-          We consider applications on a rolling basis, and will add and remove
-          job openings as the team grows. Here's how our hiring process works:
+          We consider applications on a rolling basis, and will add and remove job
+          openings as the team grows. Here's how our hiring process works:
         </Text>
 
         <ol className="list-decimal ml-8 space-y-2">
@@ -107,14 +107,14 @@ const Page: FC = () => {
           </li>
           <li>
             <Text className="leading-8" size="lg">
-              A small take-home assignment based on an old real-world support
-              request that involves creating a small React Flow app.
+              A small take-home assignment based on an old real-world support request that
+              involves creating a small React Flow app.
             </Text>
           </li>
           <li>
             <Text className="leading-8" size="lg">
-              We invite you for a one hour in person interview with one of the
-              founders and some of the team.
+              We invite you for a one hour in person interview with one of the founders
+              and some of the team.
             </Text>
           </li>
           <li>
@@ -131,34 +131,33 @@ const Page: FC = () => {
         </Heading>
 
         <Text size="lg" className="leading-8">
-          The xyflow core team is based in Berlin with an office in Kreuzberg.
-          We communicate over Discord and have one weekly synchronous call. We
-          also have a flexible schedule, which can work well with any
-          responsibilities you may have during the week such as student
-          obligations or family / medical needs.
+          The xyflow core team is based in Berlin with an office in Kreuzberg. We
+          communicate over Discord and have one weekly synchronous call. We also have a
+          flexible schedule, which can work well with any responsibilities you may have
+          during the week such as student obligations or family / medical needs.
         </Text>
 
         <Text size="lg" className="leading-8">
-          We work in three-week cycles followed by one-week cool-down period.
-          During the cool-down week we create “pitches” of new ideas, features,
-          edits, or improvements. At the end of our cool-down week as a team we
-          decide which ones we'll work on in the next cycle. We work on some
-          pitches collaboratively, some alone. At the end of the cycle we talk
-          about how the cycle was for us and show what we worked on.
+          We work in three-week cycles followed by one-week cool-down period. During the
+          cool-down week we create “pitches” of new ideas, features, edits, or
+          improvements. At the end of our cool-down week as a team we decide which ones
+          we'll work on in the next cycle. We work on some pitches collaboratively, some
+          alone. At the end of the cycle we talk about how the cycle was for us and show
+          what we worked on.
         </Text>
 
         <Text size="lg" className="leading-8">
-          We don't accept money from people like the military or agencies who
-          conduct mass surveillance. Our libraries are MIT Licensed which
-          translates to a core principle of transparency about our work. We do
-          make money from our Pro subscriptions, but we are not seeking investor
-          funding. We are anti-hustle culture.
+          We don't accept money from people like the military or agencies who conduct mass
+          surveillance. Our libraries are MIT Licensed which translates to a core
+          principle of transparency about our work. We do make money from our Pro
+          subscriptions, but we are not seeking investor funding. We are anti-hustle
+          culture.
         </Text>
 
         <Text size="lg" className="leading-8">
-          Diversity and inclusion are a priority for us. We are open to
-          discussions about how to make xyflow a more inclusive space and if we
-          can make accommodations in our hiring process, please reach out to us.
+          Diversity and inclusion are a priority for us. We are open to discussions about
+          how to make xyflow a more inclusive space and if we can make accommodations in
+          our hiring process, please reach out to us.
         </Text>
       </Section>
     </BaseLayout>
@@ -239,11 +238,10 @@ function JobOpening({
       <Heading size="sm">How to apply:</Heading>
 
       <Text size="lg" className="leading-8">
-        If you don't fit everything we wrote above or need some on-the-job
-        skill-building, but you feel excited about the position, we would love
-        to hear from you! To apply, send us an answer to the following questions
-        with a paragraph or two of text (no more than 300 words) or a video /
-        loom / audio clip (no more than 2 minutes):
+        If you don't fit everything we wrote above or need some on-the-job skill-building,
+        but you feel excited about the position, we would love to hear from you! To apply,
+        send us an answer to the following questions with a paragraph or two of text (no
+        more than 300 words) or a video / loom / audio clip (no more than 2 minutes):
       </Text>
 
       <ol className="list-decimal ml-8 space-y-2">
@@ -260,10 +258,10 @@ function JobOpening({
       </ol>
 
       <Text size="lg" className="leading-8">
-        Send this along with your first name and preferred pronouns (if you'd
-        like) and a document with your past working experience in any format you
-        like over to <span className="font-bold">jobs@xyflow.com</span>. We aim
-        to get back to applications within one week.
+        Send this along with your first name and preferred pronouns (if you'd like) and a
+        document with your past working experience in any format you like over to{' '}
+        <span className="font-bold">jobs@xyflow.com</span>. We aim to get back to
+        applications within one week.
       </Text>
     </Section>
   );
