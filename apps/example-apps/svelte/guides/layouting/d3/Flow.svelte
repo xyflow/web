c509b484db75761e0957a04d0ec820b4addd1345
peterkogo <peter@xyflow.com>
2025-04-24 18:37:52 +0200

    add layouting guide

---
+++ apps/example-apps/svelte/guides/layouting/d3/Flow.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { SvelteFlow, Background, Panel, useStore } from '@xyflow/svelte';
+  import { stratify, tree } from 'd3-hierarchy';
+  import { initialNodes, initialEdges } from './nodes-edges.js';
+
+  import '@xyflow/svelte/dist/style.css';
+
+  let nodes = $state.raw([...initialNodes]);
+  let edges = $state.raw([...initialEdges]);
+
+  const { fitView } = $derived(useStore());
+
+  const g = tree();
+
+  const getLayoutedElements = (nodes, edges, options) => {
+    if (nodes.length === 0) return { nodes, edges };
+
+    const { width, height } = document
+      .querySelector(`[data-id="${nodes[0].id}"]`)
+      .getBoundingClientRect();
+    const hierarchy = stratify()
+      .id((node) => node.id)
+      .parentId((node) => edges.find((edge) => edge.target === node.id)?.source);
+    const root = hierarchy(nodes);
+    const layout = g.nodeSize([width * 2, height * 2])(root);
+
+    return {
+      nodes: layout
+        .descendants()
+        .map((node) => ({ ...node.data, position: { x: node.x, y: node.y } })),
+      edges,
+    };
+  };
+
+  function onLayout() {
+    const layouted = getLayoutedElements(nodes, edges);
+
+    nodes = [...layouted.nodes];
+    edges = [...layouted.edges];
+
+    fitView();
+  }
+</script>
+
+<SvelteFlow bind:nodes bind:edges fitView>
+  <Panel position="top-right">
+    <button onclick={onLayout}>tree layout</button>
+  </Panel>
+  <Background />
+</SvelteFlow>
