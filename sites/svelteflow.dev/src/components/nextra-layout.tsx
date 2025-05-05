7e6862d89b588f754c151ce4d9d268d09369a6d0
moklick <info@moritzklack.com>
2025-05-05 14:46:09 +0200

    chore(sites): unify layouting

--- sites/svelteflow.dev/src/components/nextra-layout.tsx
+++ sites/svelteflow.dev/src/components/nextra-layout.tsx
@@ -1,79 +1,17 @@
 /* eslint react/jsx-sort-props: 'error' */
-import { FC, ReactNode } from 'react';
-import Link from 'next/link';
-import { Banner } from 'nextra/components';
-import { getPageMap } from 'nextra/page-map';
+import { ComponentProps, FC, ReactNode } from 'react';
+import { PageMapItem } from 'nextra';
 import { Layout, Navbar } from 'nextra-theme-docs';
-import { Search } from 'xy-shared';
-import { Button, defaultFooterCategories, Footer, LogoLabel } from '@xyflow/xy-ui';
+import { Banner } from 'nextra/components';
+import { Footer as XYFooter, LogoLabel, Button, Link } from '@xyflow/xy-ui';
 import { TOC, getLastChangelog } from 'xy-shared/server';
-
-import { getPageMap as getExamplesPageMap } from '../app/(content-pages)/examples/[...slug]/utils';
-import { Folder, MdxFile, MetaJsonFile } from 'nextra';
+import { Search } from 'xy-shared';
 
 export const NextraLayout: FC<{
   children: ReactNode;
-}> = async ({ children }) => {
-  const pageMap = await getPageMap();
-  const { Projects: _, ...remainingCategories } = defaultFooterCategories;
-  const apiReference = pageMap.find(
-    (item): item is Folder => 'children' in item && item.name === 'api-reference',
-  );
-  const examplesIndex = pageMap.findIndex(
-    (item): item is Folder => 'name' in item && item.name === 'examples',
-  );
-  const [examplesMeta, ...examples] = (pageMap[examplesIndex] as Folder).children;
-
-  const [catchAllExamplesMeta, ...catchAllExamples] = (await getExamplesPageMap())
-    .children;
-  // Merge on disk examples with examples from catch-all [...slug] route
-  pageMap[examplesIndex] = {
-    ...pageMap[examplesIndex],
-    children: [
-      {
-        // Merge meta records
-        data: {
-          ...(examplesMeta as MetaJsonFile).data,
-          ...(catchAllExamplesMeta as MetaJsonFile).data,
-        },
-      },
-      ...examples,
-      ...catchAllExamples,
-    ],
-  };
-
-  const folders = [...apiReference!.children, ...catchAllExamples].filter(
-    (item): item is Folder<MdxFile> => 'children' in item,
-  );
-
-  for (const folder of folders) {
-    // First filter out hidden items
-    folder.children = folder.children.filter((item: MdxFile) => {
-      // Skip items where frontMatter.hidden is true
-      return !('frontMatter' in item && item.frontMatter?.hidden === true);
-    });
-  }
-
-  const categories = {
-    Docs: [
-      { title: 'Quickstart Guide', route: '/learn' },
-      { title: 'API Reference', route: '/api-reference' },
-      { title: 'Examples', route: '/examples' },
-      { title: 'Showcase', route: '/showcase' },
-      { title: 'Support Us', route: '/support-us' },
-    ],
-    ...remainingCategories,
-    Legal: [
-      {
-        title: 'MIT License',
-        route: 'https://github.com/xyflow/xyflow/blob/main/LICENSE',
-      },
-      {
-        title: 'Code of Conduct',
-        route: 'https://github.com/xyflow/xyflow/blob/main/CODE_OF_CONDUCT.md',
-      },
-    ],
-  };
+  pageMap: PageMapItem[];
+  footerCategories: ComponentProps<typeof XYFooter>['categories'];
+}> = async ({ children, pageMap, footerCategories }) => {
   return (
     <Layout
       banner={
@@ -93,7 +31,7 @@ export const NextraLayout: FC<{
       docsRepositoryBase="https://github.com/xyflow/web/tree/main/sites/svelteflow.dev"
       editLink="Edit this page on GitHub"
       feedback={{ content: null }}
-      footer={<Footer baseUrl="https://svelteflow.dev" categories={categories} />}
+      footer={<XYFooter baseUrl="https://svelteflow.dev" categories={footerCategories} />}
       navbar={
         <Navbar align="left" logo={<LogoLabel label="Svelte Flow" />} logoLink={false}>
           <Search />
