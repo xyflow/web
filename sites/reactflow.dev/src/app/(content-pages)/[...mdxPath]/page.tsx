6dd35d8f2ba670427f8ebd0544bea1fe8c50ae1f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-07 14:45:52 +0200

    upd

--- sites/reactflow.dev/src/app/(content-pages)/[...mdxPath]/page.tsx
+++ sites/reactflow.dev/src/app/(content-pages)/[...mdxPath]/page.tsx
@@ -89,3 +89,5 @@ export async function generateMetadata(props: PageProps) {
 export const generateStaticParams = generateStaticParamsFor('mdxPath');
 
 export const dynamic = 'force-static';
+
+export const dynamicParams = false;
