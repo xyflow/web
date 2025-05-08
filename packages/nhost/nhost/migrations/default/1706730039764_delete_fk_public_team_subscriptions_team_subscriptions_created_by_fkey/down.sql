aa09c474929a99325a292f65f548a5f630387614
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-09 01:07:40 +0200

    rm nhost

--- packages/nhost/nhost/migrations/default/1706730039764_delete_fk_public_team_subscriptions_team_subscriptions_created_by_fkey/down.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."team_subscriptions"
-  add constraint "team_subscriptions_created_by_fkey"
-  foreign key ("created_by")
-  references "public"."user_subscriptions"
-  ("user_id") on update restrict on delete restrict;
--- packages/nhost/nhost/migrations/default/1706730062956_delete_fk_public_team_subscriptions_team_subscriptions_user_id_fkey/down.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."team_subscriptions"
-  add constraint "team_subscriptions_user_id_fkey"
-  foreign key ("user_id")
-  references "auth"."users"
-  ("id") on update restrict on delete restrict;
