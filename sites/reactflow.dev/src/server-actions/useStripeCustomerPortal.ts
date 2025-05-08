80703f10765ea592b4bd769413316c554e98cf9f
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-08 13:47:35 +0200

    user menu

---
+++ sites/reactflow.dev/src/server-actions/useStripeCustomerPortal.ts
@@ -0,0 +1,22 @@
+import useNhostFunction from './useNhostFunction';
+
+function useStripeCustomerPortal(): { openCustomerPortal: () => void } {
+  const callNhostFunction = useNhostFunction();
+
+  const openCustomerPortal = async () => {
+    const response = await callNhostFunction(
+      '/stripe/create-customer-portal',
+      {},
+    );
+
+    if (!response.error && response.url) {
+      window.location.href = response.url;
+    }
+  };
+
+  return {
+    openCustomerPortal,
+  };
+}
+
+export default useStripeCustomerPortal;
