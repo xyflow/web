259032c17a0820ba982ca894d3f7c1f6acec06a8
peterkogo <peter@xyflow.com>
2025-05-13 14:14:35 +0200

    make minimap slightly smaller

--- apps/example-apps/svelte/posts/announcement/App.svelte
+++ apps/example-apps/svelte/posts/announcement/App.svelte
@@ -25,11 +25,11 @@
   const smallDevice = new MediaQuery('max-width: 600px');
   const largeDevice = new MediaQuery('min-width: 800px');
 
-  let minimapSize = $derived(smallDevice.current ? 100 : largeDevice.current ? 200 : 150);
+  let minimapSize = $derived(smallDevice.current ? 100 : largeDevice.current ? 180 : 150);
 </script>
 
 <SvelteFlow bind:nodes bind:edges fitView>
   <Background />
   <Controls />
-  <MiniMap width={minimapSize} height={minimapSize} />
+  <MiniMap width={minimapSize} height={minimapSize * 0.8} />
 </SvelteFlow>
