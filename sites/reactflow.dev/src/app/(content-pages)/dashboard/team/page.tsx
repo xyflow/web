383e92fda0dd110f8f808082cebbefed4e646f84
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-14 22:04:12 +0200

    upd

--- sites/reactflow.dev/src/app/(content-pages)/dashboard/team/page.tsx
+++ sites/reactflow.dev/src/app/(content-pages)/dashboard/team/page.tsx
@@ -2,8 +2,19 @@ import DashboardHeader from '@/components/pro/DashboardHeader';
 import ManageTeamCard from './_cards/manage-team';
 import NotSubscribedNotification from '@/components/pro/Notification/not-subscribed';
 import { Subscribed } from '@/components/pro/SubscriptionStatus';
+import { getNhost } from '@/utils/nhost';
+import { getTeamMembers } from '@/server-actions/get-team-members';
 
-export default function TeamPage() {
+export default async function TeamPage() {
+  const nhost = await getNhost();
+  const [user, teamSubscriptions] = await Promise.all([
+    nhost.auth.getUser(),
+    getTeamMembers(),
+  ]);
+  if (!user) {
+    throw new Error('User not found');
+  }
+
   return (
     <div className="max-w-3xl">
       <DashboardHeader
@@ -13,7 +24,7 @@ export default function TeamPage() {
       <div className="flex-1 space-y-4">
         <NotSubscribedNotification />
         <Subscribed requireAdminSubscription>
-          <ManageTeamCard />
+          <ManageTeamCard user={user} teamSubscriptions={teamSubscriptions} />
         </Subscribed>
       </div>
     </div>
