89e369ed892d57ac038f44eccf93e2c045c2d3a3
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-14 22:03:54 +0200

    upd

--- sites/reactflow.dev/src/app/(content-pages)/dashboard/layout.tsx
+++ sites/reactflow.dev/src/app/(content-pages)/dashboard/layout.tsx
@@ -1,17 +1,24 @@
 import { FC, ReactNode } from 'react';
 import { Sidebar } from '@/components/pro/Sidebar';
-import { AuthProtected } from './auth-protected';
+import { redirect } from 'next/navigation';
+import { getNhost } from '@/utils/nhost';
 
-const DashboardLayout: FC<{ children: ReactNode }> = ({ children }) => {
+const DashboardLayout: FC<{ children: ReactNode }> = async ({ children }) => {
+  const nhost = await getNhost();
+  const session = nhost.auth.getSession();
+
+  if (!session) {
+    redirect('/signin');
+  }
   return (
-    <AuthProtected>
+    <>
       <div className="flex flex-col lg:flex-row w-full max-w-7xl mx-auto relative">
         <Sidebar />
         <div className="pt-4 pb-20 flex-1 min-h-[75vh] border-gray-200 lg:pl-6 lg:pt-0 lg:border-l min-w-0">
           {children}
         </div>
       </div>
-    </AuthProtected>
+    </>
   );
 };
 
