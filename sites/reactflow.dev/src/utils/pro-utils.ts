14b4e1ad8ad5e702e9f33e246e099315e9463d29
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-05-28 21:56:17 +0200

    rm not needed files

--- sites/reactflow.dev/src/utils/pro-utils.ts
+++ sites/reactflow.dev/src/utils/pro-utils.ts
@@ -1,32 +1,4 @@
-import { Framework, ProExampleConfig, Currency } from '@/types';
-
-export async function getExampleList({
-  framework,
-}: {
-  framework?: Framework;
-} = {}): Promise<ProExampleConfig[]> {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/examples.json`,
-    {
-      next: { tags: ['examples'] },
-    },
-  );
-  const examples = await response.json();
-  return examples
-    .filter((example) => (framework ? example.framework === framework : true))
-    .map((example) => ({ ...example, framework: 'react' }));
-}
-
-export async function getExampleConfig({ id }: { id: string }) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/${id}/config.json`,
-    {
-      next: { tags: ['examples'] },
-    },
-  );
-  const config = await response.json();
-  return config;
-}
+import { Currency } from '@/types';
 
 export function getCurrencySign(currency?: Currency) {
   switch (currency) {
