afbb4e5da7f13135b6f86c0e9db86034c92a974a
Alessandro <a@a.a>
2025-06-16 12:18:58 +0100

    WIP base node improvements

--- apps/ui-components/registry/components/base-node/component-example.tsx
+++ apps/ui-components/registry/components/base-node/component-example.tsx
@@ -1,14 +1,47 @@
 import { memo } from "react";
 
-import { NodeProps } from "@xyflow/react";
-import { BaseNode } from "@/registry/components/base-node";
+import { Handle, NodeProps, Position } from "@xyflow/react";
+import {
+  BaseNode,
+  BaseNodeContent,
+  BaseNodeFooter,
+  BaseNodeHeader,
+  BaseNodeHeaderTitle,
+} from "@/registry/components/base-node";
+import { Rocket } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-const BaseNodeDemo = memo(({ selected }: NodeProps) => {
+export const BaseNodeSimpleDemo = memo(({ selected }: NodeProps) => {
   return (
     <BaseNode selected={selected}>
-      <div>Base Node</div>
+      <BaseNodeContent>Base Node</BaseNodeContent>
     </BaseNode>
   );
 });
 
-export default BaseNodeDemo;
+export const BaseNodeFullDemo = memo(({ selected }: NodeProps) => {
+  return (
+    <BaseNode selected={selected} className="w-96">
+      <BaseNodeHeader className="border-b">
+        <Rocket className="size-4" />
+        <BaseNodeHeaderTitle>Base Node With Header</BaseNodeHeaderTitle>
+      </BaseNodeHeader>
+      <BaseNodeContent>
+        {/* TODO  HANDLES SHOULD GO IN THE CONTENT AREA, OR IN A BaseNodeHandles  COMPONENT ?
+        <Handle type={"source"} position={Position.Left} /> */}
+        <h2 className="text-xl font-bold">Base Node Content</h2>
+        <p className="text-xs">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim
+          lacus, dapibus eget tellus semper, interdum vehicula odio. Curabitur
+          in elementum nibh. In accumsan ullamcorper orci et rhoncus.
+        </p>
+      </BaseNodeContent>
+      <BaseNodeFooter>
+        <h3 className="self-start text-lg font-bold">Base Node Footer</h3>
+        <Button variant="outline" className="w-full">
+          Action 1
+        </Button>
+      </BaseNodeFooter>
+    </BaseNode>
+  );
+});
--- apps/ui-components/registry/components/base-node/index.tsx
+++ apps/ui-components/registry/components/base-node/index.tsx
@@ -1,6 +1,15 @@
-import { forwardRef, HTMLAttributes } from "react";
+import { useNodeId, useReactFlow } from "@xyflow/react";
+import { EllipsisVertical, Trash } from "lucide-react";
+import { forwardRef, HTMLAttributes, ReactNode, useCallback } from "react";
 
+import { Button, ButtonProps } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
 
 export const BaseNode = forwardRef<
   HTMLDivElement,
@@ -9,7 +18,7 @@ export const BaseNode = forwardRef<
   <div
     ref={ref}
     className={cn(
-      "relative rounded-md border bg-card p-5 text-card-foreground",
+      "relative rounded-md border bg-card text-card-foreground",
       className,
       selected ? "border-muted-foreground shadow-lg" : "",
       "hover:ring-1",
@@ -20,3 +29,222 @@ export const BaseNode = forwardRef<
 ));
 
 BaseNode.displayName = "BaseNode";
+
+/**
+ * A container for a consistent header layout intended to be used inside the
+ * `<BaseNode />` component.
+ */
+export function BaseNodeHeader({
+  className,
+  ...props
+}: React.ComponentProps<"header">) {
+  return (
+    <header
+      {...props}
+      className={cn(
+        "mx-0 my-0 -mb-1 flex flex-row items-center justify-between gap-2 px-3 py-2",
+        // Remove or modify these classes if you modify the padding in the
+        // `<BaseNode />` component.
+        className,
+      )}
+    />
+  );
+}
+
+export function BaseNodeContent({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="base-node-content"
+      className={cn("flex flex-col p-3", className)}
+      {...props}
+    />
+  );
+}
+
+export function BaseNodeFooter({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="base-node-footer"
+      className={cn(
+        "flex flex-col items-center gap-y-2 border-t px-3 pb-3 pt-2",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+
+/* NODE HEADER -------------------------------------------------------------- */
+
+/* NODE HEADER TITLE -------------------------------------------------------- */
+
+export type NodeHeaderTitleProps = HTMLAttributes<HTMLHeadingElement> & {
+  asChild?: boolean;
+};
+
+/**
+ * The title text for the node. To maintain a native application feel, the title
+ * text is not selectable.
+ */
+export const BaseNodeHeaderTitle = forwardRef<
+  HTMLHeadingElement,
+  NodeHeaderTitleProps
+>(({ className, asChild, ...props }, ref) => {
+  const Comp = asChild ? Slot : "h3";
+
+  return (
+    <Comp
+      ref={ref}
+      {...props}
+      className={cn(className, "user-select-none flex-1 font-semibold")}
+    />
+  );
+});
+
+BaseNodeHeaderTitle.displayName = "NodeHeaderTitle";
+
+/* NODE HEADER ACTIONS ------------------------------------------------------ */
+
+export type NodeHeaderActionsProps = HTMLAttributes<HTMLDivElement>;
+
+/**
+ * A container for right-aligned action buttons in the node header.
+ */
+export const NodeHeaderActions = forwardRef<
+  HTMLDivElement,
+  NodeHeaderActionsProps
+>(({ className, ...props }, ref) => {
+  return (
+    <div
+      ref={ref}
+      {...props}
+      className={cn(
+        "ml-auto flex items-center gap-1 justify-self-end",
+        className,
+      )}
+    />
+  );
+});
+
+NodeHeaderActions.displayName = "NodeHeaderActions";
+
+/* NODE HEADER ACTION ------------------------------------------------------- */
+
+export type NodeHeaderActionProps = ButtonProps & {
+  label: string;
+};
+
+/**
+ * A thin wrapper around the `<Button />` component with a fixed sized suitable
+ * for icons.
+ *
+ * Because the `<NodeHeaderAction />` component is intended to render icons, it's
+ * important to provide a meaningful and accessible `label` prop that describes
+ * the action.
+ */
+export const NodeHeaderAction = forwardRef<
+  HTMLButtonElement,
+  NodeHeaderActionProps
+>(({ className, label, title, ...props }, ref) => {
+  return (
+    <Button
+      ref={ref}
+      variant="ghost"
+      aria-label={label}
+      title={title ?? label}
+      className={cn(className, "nodrag size-6 p-1")}
+      {...props}
+    />
+  );
+});
+
+NodeHeaderAction.displayName = "NodeHeaderAction";
+
+//
+
+export type NodeHeaderMenuActionProps = Omit<
+  NodeHeaderActionProps,
+  "onClick"
+> & {
+  trigger?: ReactNode;
+};
+
+/**
+ * Renders a header action that opens a dropdown menu when clicked. The dropdown
+ * trigger is a button with an ellipsis icon. The trigger's content can be changed
+ * by using the `trigger` prop.
+ *
+ * Any children passed to the `<NodeHeaderMenuAction />` component will be rendered
+ * inside the dropdown menu. You can read the docs for the shadcn dropdown menu
+ * here: https://ui.shadcn.com/docs/components/dropdown-menu
+ *
+ */
+export const NodeHeaderMenuAction = forwardRef<
+  HTMLButtonElement,
+  NodeHeaderMenuActionProps
+>(({ trigger, children, ...props }, ref) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <NodeHeaderAction ref={ref} {...props}>
+          {trigger ?? <EllipsisVertical />}
+        </NodeHeaderAction>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>{children}</DropdownMenuContent>
+    </DropdownMenu>
+  );
+});
+
+NodeHeaderMenuAction.displayName = "NodeHeaderMenuAction";
+
+/* NODE HEADER DELETE ACTION --------------------------------------- */
+
+export const NodeHeaderDeleteAction = () => {
+  const id = useNodeId();
+  const { setNodes } = useReactFlow();
+
+  const handleClick = useCallback(() => {
+    setNodes((prevNodes) => prevNodes.filter((node) => node.id !== id));
+  }, [id, setNodes]);
+
+  return (
+    <NodeHeaderAction onClick={handleClick} variant="ghost" label="Delete node">
+      <Trash />
+    </NodeHeaderAction>
+  );
+};
+
+NodeHeaderDeleteAction.displayName = "NodeHeaderDeleteAction";
