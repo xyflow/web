937af695fed3e865735736c4692d5664a5aef9d8
Alessandro <a@a.a>
2025-06-25 11:39:57 +0100

    go legacy

--- apps/ui-components/registry/components/base-node/index.tsx
+++ apps/ui-components/registry/components/base-node/index.tsx
@@ -1,93 +1,91 @@
 import { cn } from "@/lib/utils";
+import { forwardRef, HTMLAttributes } from "react";
 
 // TODO: use again forwardRef because it works on React 18 and
 // ComponentProps works only on React 19 (to pass ref).
 
-export function BaseNode({ className, ...props }: React.ComponentProps<"div">) {
-  console.log(className);
-  return (
-    <div
-      className={cn(
-        "relative rounded-md border bg-card text-card-foreground",
-        className,
-        "hover:ring-1",
-        // React Flow displays node elements inside of a `NodeWrapper` component,
-        // which compiles down to a div with a the class `react-flow__node`.
-        // When a node is selected, the class `selected` is added to the
-        // `react-flow__node` element. This allows us to style the node when it
-        // is selected, using Tailwind's `&` selector.
-        "[.react-flow\\_\\_node.selected_&]:border-muted-foreground",
-        "[.react-flow\\_\\_node.selected_&]:shadow-lg",
-      )}
-      tabIndex={0}
-      {...props}
-    />
-  );
-}
+export const BaseNode = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "relative rounded-md border bg-card text-card-foreground",
+      className,
+      "hover:ring-1",
+      // React Flow displays node elements inside of a `NodeWrapper` component,
+      // which compiles down to a div with a the class `react-flow__node`.
+      // When a node is selected, the class `selected` is added to the
+      // `react-flow__node` element. This allows us to style the node when it
+      // is selected, using Tailwind's `&` selector.
+      "[.react-flow\\_\\_node.selected_&]:border-muted-foreground",
+      "[.react-flow\\_\\_node.selected_&]:shadow-lg",
+    )}
+    tabIndex={0}
+    {...props}
+  />
+));
 
 /**
  * A container for a consistent header layout intended to be used inside the
  * `<BaseNode />` component.
  */
-export function BaseNodeHeader({
-  className,
-  ...props
-}: React.ComponentProps<"header">) {
-  return (
-    <header
-      {...props}
-      className={cn(
-        "mx-0 my-0 -mb-1 flex flex-row items-center justify-between gap-2 px-3 py-2",
-        // Remove or modify these classes if you modify the padding in the
-        // `<BaseNode />` component.
-        className,
-      )}
-    />
-  );
-}
+export const BaseNodeHeader = forwardRef<
+  HTMLElement,
+  HTMLAttributes<HTMLElement>
+>(({ className, ...props }, ref) => (
+  <header
+    ref={ref}
+    {...props}
+    className={cn(
+      "mx-0 my-0 -mb-1 flex flex-row items-center justify-between gap-2 px-3 py-2",
+      // Remove or modify these classes if you modify the padding in the
+      // `<BaseNode />` component.
+      className,
+    )}
+  />
+));
 
 /**
  * The title text for the node. To maintain a native application feel, the title
  * text is not selectable.
  */
-export function BaseNodeHeaderTitle({
-  className,
-  ...props
-}: React.ComponentProps<"div">) {
-  return (
-    <h3
-      data-slot="base-node-title"
-      className={cn("user-select-none flex-1 font-semibold", className)}
-      {...props}
-    />
-  );
-}
+export const BaseNodeHeaderTitle = forwardRef<
+  HTMLHeadingElement,
+  HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h3
+    ref={ref}
+    data-slot="base-node-title"
+    className={cn("user-select-none flex-1 font-semibold", className)}
+    {...props}
+  />
+));
 
-export function BaseNodeContent({
-  className,
-  ...props
-}: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="base-node-content"
-      className={cn("flex flex-col gap-y-2 p-3", className)}
-      {...props}
-    />
-  );
-}
+export const BaseNodeContent = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    data-slot="base-node-content"
+    className={cn("flex flex-col gap-y-2 p-3", className)}
+    {...props}
+  />
+));
 
-export function BaseNodeFooter({
-  className,
-  ...props
-}: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="base-node-footer"
-      className={cn(
-        "flex flex-col items-center gap-y-2 border-t px-3 pb-3 pt-2",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
+export const BaseNodeFooter = forwardRef<
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    data-slot="base-node-footer"
+    className={cn(
+      "flex flex-col items-center gap-y-2 border-t px-3 pb-3 pt-2",
+      className,
+    )}
+    {...props}
+  />
+));
