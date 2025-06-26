07279bf98f78c940d6e66875d4af03c2c3016123
Alessandro <17289614+0x0f0f0f@users.noreply.github.com>
2025-06-26 12:28:21 +0100

    Draft: Make preview and file tabs toggleable in RemoteCodeViewer

--- packages/xy-shared/server/remote-code-viewer/index.tsx
+++ packages/xy-shared/server/remote-code-viewer/index.tsx
@@ -23,6 +23,7 @@ export type RemoteCodeViewerProps = {
   activeFile?: string;
   showEditor?: boolean;
   showPreview?: boolean;
+  showFiletab?: boolean;
   showOpenInCodeSandbox?: boolean;
   showOpenInStackblitz?: boolean;
   editorHeight?: string | number;
@@ -34,6 +35,8 @@ export const RemoteCodeViewer: FC<RemoteCodeViewerProps> = async ({
   route,
   framework,
   showEditor = true,
+  showPreview = true,
+  showFiletab = true,
   showOpenInCodeSandbox = framework === 'react',
   showOpenInStackblitz = true,
   sandpackOptions = {},
@@ -51,13 +54,13 @@ export const RemoteCodeViewer: FC<RemoteCodeViewerProps> = async ({
   if (!isOk) {
     throw new Error('Example code not found!');
   }
-  // 1. If showEditor is false, the layout is vertical (isHorizontal = false).
+  // 1. If only the editor or preview is shown, the layout is vertical (isHorizontal = false).
   // 2. If orientation is provided, it is horizontal only if orientation === 'horizontal'.
   // 3. If the route includes 'examples/', the layout is vertical (isHorizontal = false).
   // 4. Default fallback: the layout is horizontal (isHorizontal = true).
   const isExample = route.includes('examples/');
   const isHorizontal =
-    showEditor === false
+    !showEditor || !showPreview
       ? false
       : orientation
         ? orientation === 'horizontal'
@@ -81,70 +84,86 @@ export const RemoteCodeViewer: FC<RemoteCodeViewerProps> = async ({
     (Object.keys(snippets).includes('App.jsx') ? 'App.jsx' : Object.keys(snippets)[0]);
 
   return (
-    <div
-      className={cn(
-        'remote-code-viewer mt-5 rounded-lg flex',
-        isHorizontal ? 'flex-row' : 'flex-col',
-      )}
-    >
+    <>
+      <span>{isHorizontal}</span>
+
       <div
-        style={isHorizontal ? {} : { height: editorHeight }}
-        className={cn('relative', isHorizontal && 'w-1/2')}
+        className={cn(
+          'remote-code-viewer mt-5 rounded-lg flex',
+          isHorizontal ? 'flex-row' : 'flex-col',
+        )}
       >
-        <iframe
-          src={preview}
-          loading="lazy"
-          width="100%"
-          height="100%"
-          className="example"
-        />
-        <div className="absolute bottom-5 right-5 flex">
-          {showOpenInStackblitz && (
-            <OpenInStackblitz framework={_framework} route={route} />
-          )}
-          {showOpenInCodeSandbox && (
-            <OpenInCodesandbox
-              framework={_framework}
-              route={route}
-              sandpackOptions={sandpackOptions}
+        {showPreview && (
+          <div
+            style={isHorizontal ? {} : { height: editorHeight }}
+            className={cn('relative', isHorizontal && 'w-1/2')}
+          >
+            <iframe
+              src={preview}
+              loading="lazy"
+              width="100%"
+              height="100%"
+              className="example"
             />
-          )}
-        </div>
+            <div className="absolute bottom-5 right-5 flex">
+              {showOpenInStackblitz && (
+                <OpenInStackblitz framework={_framework} route={route} />
+              )}
+              {showOpenInCodeSandbox && (
+                <OpenInCodesandbox
+                  framework={_framework}
+                  route={route}
+                  sandpackOptions={sandpackOptions}
+                />
+              )}
+            </div>
+          </div>
+        )}
+        {showEditor && (
+          <div
+            className={cn(
+              'rounded-xl overflow-hidden',
+              isHorizontal ? 'w-1/2 rounded-l-none' : 'rounded-t-none',
+            )}
+          >
+            {snippets && (
+              <Tabs defaultValue={_initialActiveFile}>
+                {showFiletab ? (
+                  <TabsList className="tablist border-none mb-0 overflow-x-auto overflow-y-hidden text-nowrap bg-primary/5">
+                    {Object.keys(snippets).map((filename) => (
+                      <TabsTrigger
+                        key={filename}
+                        className="font-light text-sm data-[state=active]:bg-primary/5 pt-3"
+                        value={filename}
+                      >
+                        {filename}
+                      </TabsTrigger>
+                    ))}
+                  </TabsList>
+                ) : (
+                  // TODO: Adjust display of filename to make it look like a triple backtick
+                  // code snippet header.
+                  <span className="text-lg font-bold font-mono">{activeFile}</span>
+                )}
+                <div
+                  style={{ height: editorHeight }}
+                  className="tabcontent overflow-y-scroll bg-primary/5"
+                >
+                  {Object.entries(snippets).map(([filename, compiledSource]) => (
+                    <TabsContent
+                      key={filename}
+                      className="min-h-[500px]"
+                      value={filename}
+                    >
+                      <MDXRemote compiledSource={compiledSource} />
+                    </TabsContent>
+                  ))}
+                </div>
+              </Tabs>
+            )}
+          </div>
+        )}
       </div>
-      {showEditor && (
-        <div
-          className={cn(
-            'rounded-xl overflow-hidden',
-            isHorizontal ? 'w-1/2 rounded-l-none' : 'rounded-t-none',
-          )}
-        >
-          {snippets && (
-            <Tabs defaultValue={_initialActiveFile}>
-              <TabsList className="tablist border-none mb-0 overflow-x-auto overflow-y-hidden text-nowrap bg-primary/5">
-                {Object.keys(snippets).map((filename) => (
-                  <TabsTrigger
-                    key={filename}
-                    className="font-light text-sm data-[state=active]:bg-primary/5 pt-3"
-                    value={filename}
-                  >
-                    {filename}
-                  </TabsTrigger>
-                ))}
-              </TabsList>
-              <div
-                style={{ height: editorHeight }}
-                className="tabcontent overflow-y-scroll bg-primary/5"
-              >
-                {Object.entries(snippets).map(([filename, compiledSource]) => (
-                  <TabsContent key={filename} className="min-h-[500px]" value={filename}>
-                    <MDXRemote compiledSource={compiledSource} />
-                  </TabsContent>
-                ))}
-              </div>
-            </Tabs>
-          )}
-        </div>
-      )}
-    </div>
+    </>
   );
 };
