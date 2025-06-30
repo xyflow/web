caf70e8ed719ec9cf50b8c891699c65e62c9b957
Alessandro <17289614+0x0f0f0f@users.noreply.github.com>
2025-06-30 13:44:35 +0200

    move back to manual tooltip rendering

--- apps/ui-components/registry/components/node-tooltip/component-example.tsx
+++ apps/ui-components/registry/components/node-tooltip/component-example.tsx
@@ -11,14 +11,19 @@ import { BaseNode, BaseNodeContent } from "../base-node";
 const NodeTooltipDemo = memo(() => {
   return (
     <NodeTooltip>
-      <NodeTooltipContent position={Position.Top}>
-        Hidden Content
+      <NodeTooltipContent position={Position.Top} className="text-center">
+        You can display any content here, like text, images, or even components.{" "}
+        <br />
+        The tooltip will appear when you hover over the trigger.
       </NodeTooltipContent>
-      <BaseNode>
-        <BaseNodeContent>
-          <NodeTooltipTrigger className="hover:bg-red-500">
-            Hover
+      <BaseNode className="w-64 text-center">
+        <BaseNodeContent className="flex flex-col items-center">
+          <NodeTooltipTrigger className="rounded border border-gray-300 p-2 text-lg font-bold">
+            Hover me! ⭐️
           </NodeTooltipTrigger>
+          <span>
+            You can add more content, that does not trigger the tooltip.
+          </span>
         </BaseNodeContent>
       </BaseNode>
     </NodeTooltip>
--- apps/ui-components/registry/components/node-tooltip/index.tsx
+++ apps/ui-components/registry/components/node-tooltip/index.tsx
@@ -1,36 +1,39 @@
-import { NodeToolbar, NodeToolbarProps } from "@xyflow/react";
-import React, { createContext, forwardRef, useContext } from "react";
-import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import { cn } from "@/lib/utils";
+import { NodeToolbar, NodeToolbarProps } from "@xyflow/react";
+import React, {
+  createContext,
+  forwardRef,
+  HTMLAttributes,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
 
 /* TOOLTIP CONTEXT ---------------------------------------------------------- */
 
-const TooltipContext = createContext(false);
+type TooltipContextType = {
+  isVisible: boolean;
+  showTooltip: () => void;
+  hideTooltip: () => void;
+};
+
+const TooltipContext = createContext<TooltipContextType | null>(null);
 
 /* TOOLTIP NODE ------------------------------------------------------------- */
 
-export function NodeTooltipProvider({
-  delayDuration = 0,
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
-  return (
-    <TooltipPrimitive.Provider
-      data-slot="node-tooltip-provider"
-      delayDuration={delayDuration}
-      {...props}
-    />
-  );
-}
-NodeTooltipProvider.displayName = "NodeTooltipProvider";
-
 export const NodeTooltip = forwardRef<
-  React.ElementRef<typeof TooltipPrimitive.Root>,
-  React.ComponentProps<typeof TooltipPrimitive.Root>
->((props, ref) => {
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement>
+>(({ children }, ref) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showTooltip = useCallback(() => setIsVisible(true), []);
+  const hideTooltip = useCallback(() => setIsVisible(false), []);
+
   return (
-    <NodeTooltipProvider>
-      <TooltipPrimitive.Root data-slot="node-tooltip" ref={ref} {...props} />
-    </NodeTooltipProvider>
+    <TooltipContext.Provider value={{ isVisible, showTooltip, hideTooltip }}>
+      <div ref={ref}>{children}</div>
+    </TooltipContext.Provider>
   );
 });
 NodeTooltip.displayName = "NodeTooltip";
@@ -38,13 +41,36 @@ NodeTooltip.displayName = "NodeTooltip";
 /* TOOLTIP TRIGGER ---------------------------------------------------------- */
 
 export const NodeTooltipTrigger = forwardRef<
-  React.ElementRef<typeof TooltipPrimitive.Trigger>,
-  React.ComponentProps<typeof TooltipPrimitive.Trigger>
+  HTMLDivElement,
+  HTMLAttributes<HTMLDivElement>
 >((props, ref) => {
+  const tooltipContext = useContext(TooltipContext);
+  if (!tooltipContext) {
+    throw new Error("NodeTooltipTrigger must be used within NodeTooltip");
+  }
+  const { showTooltip, hideTooltip } = tooltipContext;
+
+  const onMouseEnter = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      props.onMouseEnter?.(e);
+      showTooltip();
+    },
+    [props, showTooltip],
+  );
+
+  const onMouseLeave = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      props.onMouseLeave?.(e);
+      hideTooltip();
+    },
+    [props, hideTooltip],
+  );
+
   return (
-    <TooltipPrimitive.Trigger
-      data-slot="tooltip-trigger"
+    <div
       ref={ref}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
       {...props}
     />
   );
@@ -57,27 +83,29 @@ NodeTooltipTrigger.displayName = "NodeTooltipTrigger";
 //  * A component that displays the tooltip content based on visibility context.
 //  */
 
-export type NodeTooltipContentProps = NodeToolbarProps &
-  React.ComponentProps<typeof TooltipPrimitive.Content>;
+export const NodeTooltipContent = forwardRef<HTMLDivElement, NodeToolbarProps>(
+  ({ children, position, className, ...props }, ref) => {
+    const tooltipContext = useContext(TooltipContext);
+    if (!tooltipContext) {
+      throw new Error("NodeTooltipContent must be used within NodeTooltip");
+    }
+    const { isVisible } = tooltipContext;
 
-export const NodeTooltipContent = forwardRef<
-  React.ElementRef<typeof TooltipPrimitive.Content>,
-  NodeTooltipContentProps
->(({ sideOffset = 0, children, position, ...props }, ref) => (
-  <TooltipPrimitive.Portal>
-    <TooltipPrimitive.Content
-      data-slot="tooltip-content"
-      sideOffset={sideOffset}
-      ref={ref}
-      {...props}
-    >
-      <NodeToolbar
-        className="rounded-sm bg-primary p-2 text-primary-foreground"
-        tabIndex={1}
-        position={position}
-      >
-        {children}
-      </NodeToolbar>
-    </TooltipPrimitive.Content>
-  </TooltipPrimitive.Portal>
-));
+    return (
+      <div ref={ref}>
+        <NodeToolbar
+          isVisible={isVisible}
+          className={cn(
+            "rounded-sm bg-primary p-2 text-primary-foreground",
+            className,
+          )}
+          tabIndex={1}
+          position={position}
+          {...props}
+        >
+          {children}
+        </NodeToolbar>
+      </div>
+    );
+  },
+);
