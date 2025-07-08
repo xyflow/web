4fcb49580db014a04d61879cc6c3b58b73d28fb6
omes <36600384+omesome@users.noreply.github.com>
2025-07-08 14:49:38 +0200

    code viewer rework (#922)
    * add simple tooltip wrapper
    
    * align tabs design with nextra
    
    * revamp code viewer
    
    * fix ts error
    
    * rename tooltip-simple
    
    * remove code viewer orientation
    
    * increase default code viewer height
    
    * scrollable tabs and polish
    
    * add copy button to code viewer

--- packages/xy-shared/server/remote-code-viewer/open-in-stackblitz.tsx
+++ packages/xy-shared/server/remote-code-viewer/open-in-stackblitz.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import sdk, { OpenOptions, Project, ProjectTemplate } from '@stackblitz/sdk';
 import { useCallback } from 'react';
-import sdk, { Project, ProjectTemplate, OpenOptions } from '@stackblitz/sdk';
 
-import { Button, Framework } from '@xyflow/xy-ui';
+import { Framework, TooltipSimple } from '@xyflow/xy-ui';
 import { fetchFiles } from './fetchFiles';
 
 type OpenInStackblitzProps = {
@@ -28,13 +28,17 @@ export function OpenInStackblitz({ framework, route }: OpenInStackblitzProps) {
   }, [framework, route]);
 
   return (
-    <Button
-      onClick={openInStackblitz}
-      size="sm"
-      className="font-medium text-[10px] h-6 bg-gray-100 hover:bg-gray-200 text-gray-700"
-    >
-      Open in Stackblitz
-    </Button>
+    <TooltipSimple label="Open in StackBlitz">
+      <button title="Open in Stackblitz" onClick={openInStackblitz}>
+        <svg
+          className="size-4 fill-slate-700 stroke-slate-700"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 56 78"
+        >
+          <path d="M23.4273 48.2853C23.7931 47.5845 23.0614 46.8837 22.3298 46.8837H1.11228C0.0148224 46.8837 -0.350997 45.8326 0.380642 45.1318L40.9866 0.282084C41.7182 -0.418693 43.1815 0.282084 42.8157 1.33325L32.9386 30.0651C32.5727 30.7659 32.9386 31.4666 33.6702 31.4666H54.8877C55.9852 31.4666 56.351 32.5178 55.6194 33.2186L15.0134 77.7179C14.2818 78.4187 12.8185 77.7179 13.1843 76.6667L23.4273 48.2853Z" />
+        </svg>
+      </button>
+    </TooltipSimple>
   );
 }
 
