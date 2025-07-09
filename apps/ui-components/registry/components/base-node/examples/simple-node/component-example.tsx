0b6c212de2697fd6edff88afa9918ab29249908d
Alessandro <17289614+0x0f0f0f@users.noreply.github.com>
2025-07-09 14:33:51 +0200

    cleanup

--- apps/ui-components/registry/components/base-node/examples/simple-node/component-example.tsx
+++ apps/ui-components/registry/components/base-node/examples/simple-node/component-example.tsx
@@ -5,7 +5,7 @@ import { BaseNode, BaseNodeContent } from "@/registry/components/base-node";
 export const SimpleBaseNode = memo(() => {
   return (
     <BaseNode>
-      <BaseNodeContent className="bg-red-100">simple node</BaseNodeContent>
+      <BaseNodeContent>Simple Node</BaseNodeContent>
     </BaseNode>
   );
 });
