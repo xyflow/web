0fc3a1044ca255ee7f0d4eadc1ae343bcba35f52
Christopher Möller <chrtze@gmail.com>
2025-07-15 12:14:31 +0200

    add use client to price display

--- packages/xy-ui/components/ui/pricing-table/subscription-plan.tsx
+++ packages/xy-ui/components/ui/pricing-table/subscription-plan.tsx
@@ -1,9 +1,8 @@
+'use client';
+
 import Link from 'next/link';
 import cn from 'clsx';
-import {
-  CheckCircleIcon,
-  InformationCircleIcon,
-} from '@heroicons/react/24/outline';
+import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/react/24/outline';
 
 import { Button, Tooltip, TooltipContent, TooltipTrigger } from '../../../';
 
@@ -75,8 +74,7 @@ function PricingDisplay({
   billingInterval: BillingInterval;
 }) {
   const currentPrice = pricing?.find(
-    (price) =>
-      price.currency === currency && price.interval === billingInterval,
+    (price) => price.currency === currency && price.interval === billingInterval,
   );
 
   if (!currentPrice || !pricing) {
@@ -86,8 +84,7 @@ function PricingDisplay({
   const priceStrikeThrough =
     billingInterval === BillingInterval.YEAR
       ? pricing.find(
-          (price) =>
-            price.currency === currency && price.interval !== billingInterval,
+          (price) => price.currency === currency && price.interval !== billingInterval,
         )
       : undefined;
 
@@ -95,9 +92,7 @@ function PricingDisplay({
     <div className="ml-4 leading-tight">
       <div className="flex">
         {priceStrikeThrough && (
-          <div className="mr-2 text-light line-through">
-            {priceStrikeThrough.label}
-          </div>
+          <div className="mr-2 text-light line-through">{priceStrikeThrough.label}</div>
         )}
         <div suppressHydrationWarning className="font-bold">
           {currentPrice.label}
@@ -123,9 +118,7 @@ export default function Plan({
   onSelect,
 }: PlanProps) {
   const btnLink =
-    id === PlanId.ENTERPRISE
-      ? '/pro/enterprise'
-      : 'https://pro.reactflow.dev/signup';
+    id === PlanId.ENTERPRISE ? '/pro/enterprise' : 'https://pro.reactflow.dev/signup';
 
   return (
     <div
@@ -138,9 +131,7 @@ export default function Plan({
       <div className={cn('font-bold text-4xl', highlighted && 'text-primary')}>
         {label}
       </div>
-      <div className="text-lg leading-tight h-[120px] relative mt-6">
-        {description}
-      </div>
+      <div className="text-lg leading-tight h-[120px] relative mt-6">{description}</div>
 
       <div className="mb-8 lg:mb-20">
         <div className="flex">
@@ -152,11 +143,7 @@ export default function Plan({
             asChild
           >
             {onSelect ? (
-              <Button
-                onClick={() =>
-                  onSelect({ plan: id, currency, billingInterval })
-                }
-              >
+              <Button onClick={() => onSelect({ plan: id, currency, billingInterval })}>
                 {buttonLabel}
               </Button>
             ) : (
