0009a9f7b70e03f26c29fb1868f40828d0da0517
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-08-25 15:52:22 +0300

    try to patch nhost

--- sites/reactflow.dev/src/utils/nhost.ts
+++ sites/reactflow.dev/src/utils/nhost.ts
@@ -1,38 +1,30 @@
 import { cookies } from 'next/headers';
-import { cache } from 'react';
 import { NhostClient, NhostSession } from '@nhost/nhost-js';
 import type { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies';
 import type { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
-import { connection } from 'next/server';
 // If manually saved, e.g., in server actions
 export const NHOST_SESSION_KEY = 'nhostSession';
 // Saved by sign-in
 export const NHOST_REFRESH_KEY = 'nhostRefreshToken';
 
-const $getNhost = cache(
-  (_key: unknown) =>
-    async (
-      cookieStore?: RequestCookies | ReadonlyRequestCookies,
-    ): Promise<NhostClient> => {
-      await connection();
-      cookieStore ??= await cookies();
+export async function getNhost(
+  cookieStore?: RequestCookies | ReadonlyRequestCookies,
+): Promise<NhostClient> {
+  cookieStore ??= await cookies();
 
-      const nhost = new NhostClient({
-        subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN,
-        region: process.env.NEXT_PUBLIC_NHOST_REGION,
-        start: false,
-      });
-      const sessionCookieValue = cookieStore.get(NHOST_SESSION_KEY)?.value;
-      const initialSession: NhostSession = sessionCookieValue
-        ? JSON.parse(atob(sessionCookieValue))
-        : // auth.initWithSession` must be called even with `null`, otherwise will get an error from @nhost/hasura-auth-js - Auth interpreter not set
-          null;
-      await nhost.auth.initWithSession({ session: initialSession });
-      return nhost;
-    },
-);
-
-export const getNhost = $getNhost(Symbol());
+  const nhost = new NhostClient({
+    subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN,
+    region: process.env.NEXT_PUBLIC_NHOST_REGION,
+    start: false,
+  });
+  const sessionCookieValue = cookieStore.get(NHOST_SESSION_KEY)?.value;
+  const initialSession: NhostSession = sessionCookieValue
+    ? JSON.parse(atob(sessionCookieValue))
+    : // auth.initWithSession` must be called even with `null`, otherwise will get an error from @nhost/hasura-auth-js - Auth interpreter not set
+      null;
+  await nhost.auth.initWithSession({ session: initialSession });
+  return nhost;
+}
 
 export function prettifyError(
   error: { message: string } | { message: string }[],
