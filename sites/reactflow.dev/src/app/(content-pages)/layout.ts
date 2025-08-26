cb9576af7d31de2efdc30150980bdcfa228a9dd6
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-08-26 14:39:06 +0300

    try experimental_ppr

--- sites/reactflow.dev/src/app/(content-pages)/layout.ts
+++ sites/reactflow.dev/src/app/(content-pages)/layout.ts
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
 
 // TODO check if we can have static layout here
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 export const dynamicParams = false;
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => children;
