9930ed8b71fb96ecd57c77e6b5cceb19debe748c
Alessandro <17289614+0x0f0f0f@users.noreply.github.com>
2025-09-17 12:33:07 +0200

    fix build

--- apps/ui-components/registry/components/search-bar/app-example.tsx
+++ apps/ui-components/registry/components/search-bar/app-example.tsx
@@ -2,37 +2,40 @@ import { Background, Node, Edge, ReactFlow, useReactFlow } from "@xyflow/react";
 import { SearchBar } from "@/registry/components/search-bar/";
 import { useCallback } from "react";
 
+type NodeData = {
+  label: string;
+};
+
 const graphSize = 20;
 
-const initNodes = Array<Node>(graphSize)
-  .fill(0)
-  .map((_, index) => ({
+const initNodes: Node<NodeData>[] = Array.from(
+  { length: graphSize },
+  (_, index) => ({
     id: `node-${index}`,
     data: { label: `Node ${index}` },
     position: { x: Math.random() * 1000, y: Math.random() * 1000 },
-  }));
+  }),
+);
 
-const initEdges = Array<Edge>(graphSize)
-  .fill(0)
-  .map((_, index) => ({
-    id: `edge-${index}`,
-    source: `node-${Math.floor(Math.random() * graphSize)}`,
-    target: `node-${Math.floor(Math.random() * graphSize)}`,
-  }));
+const initEdges: Edge[] = Array.from({ length: graphSize }, (_, index) => ({
+  id: `edge-${index}`,
+  source: `node-${Math.floor(Math.random() * graphSize)}`,
+  target: `node-${Math.floor(Math.random() * graphSize)}`,
+}));
 
 export default function App() {
-  const onSearch = (nodes: Node[], searchString: string) => {
-    console.log(searchString);
-    console.log(nodes);
+  const onSearch = useCallback(
+    (nodes: Node<NodeData>[], searchString: string) =>
+      nodes.filter((node) =>
+        node.data.label.toLowerCase().includes(searchString.toLowerCase()),
+      ),
+    [],
+  );
 
-    return nodes.filter((node) =>
-      node.data.label.toLowerCase().includes(searchString.toLowerCase()),
-    );
-  };
   const { fitView, setNodes } = useReactFlow();
 
   const onSelectNode = useCallback(
-    (node: Node) => {
+    (node: Node<NodeData>) => {
       setNodes((nodes) =>
         nodes.map((n) => (n.id === node.id ? { ...n, selected: true } : n)),
       );
@@ -47,8 +50,10 @@ export default function App() {
         <Background />
         <SearchBar
           position="top-left"
-          onSearch={onSearch}
-          onSelectNode={onSelectNode}
+          onSearch={(nodes, searchString) =>
+            onSearch(nodes as Node<NodeData>[], searchString)
+          }
+          onSelectNode={(node) => onSelectNode(node as Node<NodeData>)}
         />
       </ReactFlow>
     </div>
