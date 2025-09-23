17c889e65fe6cdbae0e15fcd3ec1815e394c6dd8
Alessandro <17289614+0x0f0f0f@users.noreply.github.com>
2025-09-23 11:37:58 +0200

    Use SVG directly as edge child and apply review suggestions

--- apps/example-apps/react/examples/edges/markers/App.tsx
+++ apps/example-apps/react/examples/edges/markers/App.tsx
@@ -126,29 +126,6 @@ export default function MarkersExample() {
       <svg style={{ position: 'absolute', top: 0, left: 0 }}>
         <defs>
           <marker
-            className="react-flow__arrowhead"
-            id="selected-marker"
-            markerWidth="20"
-            markerHeight="20"
-            viewBox="-10 -10 20 20"
-            markerUnits="userSpaceOnUse"
-            orient="auto-start-reverse"
-            refX="0"
-            refY="0"
-          >
-            <polyline
-              className="arrowclosed"
-              style={{
-                strokeWidth: 1,
-                stroke: '#FFCC00',
-                fill: '#FFCC00',
-              }}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              points="-5,-4 0,0 -5,4 -5,-4"
-            />
-          </marker>
-          <marker
             id="logo"
             viewBox="0 0 40 40"
             markerHeight={20}
