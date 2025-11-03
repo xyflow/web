3fd61c73cfa523e5455fdf954bd7915bb9d57551
Alessandro <17289614+0x0f0f0f@users.noreply.github.com>
2025-11-03 10:06:22 +0100

    use radix-ui in components tutorial

--- apps/example-apps/react/tutorials/components/sum-node/components/ui/button.tsx
+++ apps/example-apps/react/tutorials/components/sum-node/components/ui/button.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Slot } from '@radix-ui/react-slot';
+import { Slot } from 'radix-ui';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '../../lib/utils';
@@ -9,8 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
