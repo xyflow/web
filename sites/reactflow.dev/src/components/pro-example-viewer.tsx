0c803c35964a7f8bdb8fcb14e45a489118f8182a
moklick <info@moritzklack.com>
2026-02-03 11:31:25 +0100

    chore(pro-example-viewer): cleanup

--- sites/reactflow.dev/src/components/pro-example-viewer.tsx
+++ sites/reactflow.dev/src/components/pro-example-viewer.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import Link from 'next/link';
 import { Container, ContainerProps, Text, Button, cn } from '@xyflow/xy-ui';
 
+const iframeClassName = cn('block h-[645px] bg-white w-full');
+
 const ProExampleViewer: FC<{
   slug: string;
   variant?: ContainerProps['variant'];
@@ -18,86 +20,77 @@ const ProExampleViewer: FC<{
   className,
   innerClassName,
   sideBySide = false,
-  queryParams = {},
+  queryParams,
 }) => {
-    const isLightMode = variant === 'default';
+  const isLightMode = variant === 'default';
+  const teaserClasses = cn(
+    'px-6 py-8 flex flex-wrap items-center justify-between gap-x-4 gap-y-2',
+    {
+      'bg-gradient': isLightMode,
+      'bg-[length:200%]': isLightMode,
+      'bg-center': isLightMode,
+    },
+  );
 
-    const teaserClasses = cn(
-      'px-6 py-8 flex flex-wrap items-center justify-between gap-x-4 gap-y-2',
-      {
-        'bg-gradient': isLightMode,
-        'bg-[length:200%]': isLightMode,
-        'bg-center': isLightMode,
-      },
-    );
+  const iframeBasePath = `${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/react/${slug}`;
+  const iframeSrcUrl = new URL(iframeBasePath);
 
+  if (queryParams) {
+    Object.entries(queryParams).forEach(([key, value]) => {
+      iframeSrcUrl.searchParams.set(key, value);
+    });
+  }
 
-    const iframeSrcUrl = new URL(`${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/react/${slug}`);
-    if (Object.keys(queryParams).length > 0) {
-      Object.entries(queryParams).forEach(([key, value]) => {
-        iframeSrcUrl.searchParams.set(key, value);
-      });
-    }
+  let iframeSrc = iframeSrcUrl.toString();
+  let signInLink = `https://pro.reactflow.dev/examples/react/${slug}`;
 
-    let iframeSrc = iframeSrcUrl.toString();
+  if (type === 'template') {
+    const config = await fetch(`${iframeBasePath}/config.json`);
+    const { previewUrl } = await config.json();
 
-    if (type === 'template') {
-      const config = await fetch(
-        `${process.env.NEXT_PUBLIC_PRO_EXAMPLES_URL}/react/${slug}/config.json`,
-      );
+    iframeSrc = previewUrl;
+    signInLink = `https://pro.reactflow.dev/templates/${slug}`;
+  }
 
-      const { previewUrl } = await config.json();
-      iframeSrc = previewUrl;
-    }
+  return (
+    <Container
+      className={cn(['mt-7', className])}
+      variant={variant}
+      innerClassName={innerClassName}
+    >
+      <div className={teaserClasses}>
+        <Text className="flex-1 basis-full max-w-xl">
+          <strong>This is a Pro {type}.</strong> Get{' '}
+          <Link className="underline" href="/pro/examples">
+            all pro examples
+          </Link>
+          , templates, 1:1 support from the xyflow team and prioritized Github issues with
+          a React Flow Pro subscription.
+        </Text>
+        <div className="flex space-x-4">
+          <Button asChild className="shrink-0">
+            <Link href="/pro">See Pricing Plans</Link>
+          </Button>
+          <Button asChild variant="secondary" className="text-primary shrink-0">
+            <a href={signInLink}>Sign In</a>
+          </Button>
+        </div>
+      </div>
 
-    const signInLink =
-      type === 'template'
-        ? `https://pro.reactflow.dev/templates/${slug}`
-        : `https://pro.reactflow.dev/examples/react/${slug}`;
-
-    const iframe = (index: number) => (
-      <iframe
-        key={index}
-        src={`${iframeSrc}`}
-        className={cn('block h-[645px] bg-white w-full')}
-      />
-    );
-
-    return (
-      <Container
-        className={cn(['mt-7', className])}
-        variant={variant}
-        innerClassName={innerClassName}
-      >
-        <div className={teaserClasses}>
-          <Text className="flex-1 basis-full max-w-xl">
-            <strong>This is a Pro {type}.</strong> Get{' '}
-            <Link className="underline" href="/pro/examples">
-              all pro examples
-            </Link>
-            , templates, 1:1 support from the xyflow team and prioritized Github issues with
-            a React Flow Pro subscription.
-          </Text>
-          <div className="flex space-x-4">
-            <Button asChild className="shrink-0">
-              <Link href="/pro">See Pricing Plans</Link>
-            </Button>
-            <Button asChild variant="secondary" className="text-primary shrink-0">
-              <a href={signInLink}>Sign In</a>
-            </Button>
+      {sideBySide ? (
+        <div className="flex gap-2">
+          <div className="w-1/2">
+            <iframe src={iframeSrc} className={iframeClassName} />
+          </div>
+          <div className="border-l-gray-200 border-l-2 w-1/2">
+            <iframe src={iframeSrc} className={iframeClassName} />
           </div>
         </div>
-
-        {sideBySide ? (
-          <div className="flex gap-2">
-            <div className="w-1/2">{iframe(0)}</div>
-            <div className=" border-l-gray-200 border-l-2 w-1/2">{iframe(1)}</div>{' '}
-          </div>
-        ) : (
-          iframe(0)
-        )}
-      </Container>
-    );
-  };
+      ) : (
+        <iframe src={iframeSrc} className={iframeClassName} />
+      )}
+    </Container>
+  );
+};
 
 export default ProExampleViewer;
