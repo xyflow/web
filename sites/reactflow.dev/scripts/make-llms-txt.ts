0c7964b130634dfc2f852b3339f9063dfb5c6425
Hayleigh Thompson <me@hayleigh.dev>
2026-02-16 08:39:39 +0000

    :recycle: Remove old llms-txt generation scripts.

--- sites/reactflow.dev/scripts/make-llms-txt.ts
+++
@@ -1,4 +0,0 @@
-// Small version of the llms.txt file that only includes the most important sections of the docs as links.
-// Uses nextra's getPageMap via the app's /api/page-map route (getPageMap only works inside Next.js context).
-
-// TODO implement.
