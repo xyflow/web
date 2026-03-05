f106f6de94a20d2246f88e83a3251777501614df
Abbey Yacoe <abbey@xyflow.com>
2026-03-05 20:30:18 +0100

    chore: fix routes for pro examples

--- packages/xy-shared/routes/pro/(auth)/dashboard/page.tsx
+++ packages/xy-shared/routes/pro/(auth)/dashboard/page.tsx
@@ -21,7 +21,7 @@ export default async function DashboardPage() {
               SubscriptionPlan.STUDENT,
               SubscriptionPlan.OSS,
             ]}
-            button={{ label: 'Browse Examples', href: '/examples/pro' }}
+            button={{ label: 'Browse Examples', href: '/pro/examples' }}
           />
 
           <SubscriptionFeature
