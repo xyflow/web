dbdf4eeb157e49199c827069c1b9a91ad73fee9f
peterkogo <peter@xyflow.com>
2026-03-11 15:51:14 +0100

    update algolia and fix styles

--- packages/xy-shared/components/html/index.tsx
+++ packages/xy-shared/components/html/index.tsx
@@ -1,16 +1,42 @@
 'use client';
 
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
 import { ntDapperFont } from '../../fonts/index';
 import { cn } from '../../lib/utils';
 
 export const Html: FC<{ children: ReactNode }> = ({ children }) => {
+  // TODO: remove this workaround once this is fixed: https://github.com/shuding/nextra/pull/4891
+  const htmlRef = useRef<HTMLHtmlElement>(null);
+  const [theme, setTheme] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const html = htmlRef.current;
+    if (!html) return;
+
+    const syncTheme = () => {
+      const colorScheme = html.style.colorScheme;
+      setTheme(colorScheme);
+    };
+
+    syncTheme();
+
+    const observer = new MutationObserver(syncTheme);
+    observer.observe(html, {
+      attributes: true,
+      attributeFilter: ['style'],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <html
+      ref={htmlRef}
       className={cn(ntDapperFont.className)}
       dir="ltr"
       lang="en"
       suppressHydrationWarning
+      data-theme={theme}
     >
       {children}
     </html>
