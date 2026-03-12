fce6b468df653151f325c78441d2b385515e1e11
peterkogo <peter@xyflow.com>
2026-03-12 10:45:22 +0100

    fix color hues to be based on primary

--- packages/xy-shared/routes/pro/(no-auth)/layout.tsx
+++ packages/xy-shared/routes/pro/(no-auth)/layout.tsx
@@ -5,14 +5,14 @@ export async function NoAuthLayout({ children }: { children: React.ReactNode }) 
         className="absolute opacity-10 w-full h-[70vh] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10 dark:hidden"
         style={{
           background:
-            'radial-gradient(rgba(68,91,222,1) 0%, rgba(215,78,243,1) 25%, rgba(255,255,255,1) 50%)',
+            'radial-gradient(hsl(var(--primary) / 0.8) 0%, hsl(var(--primary) / 0.45) 25%, rgba(255,255,255,1) 50%)',
         }}
       />
       <div
         className="hidden dark:block absolute opacity-10 w-full h-[70vh] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10"
         style={{
           background:
-            'radial-gradient(rgba(68,91,222,1) 0%, rgba(215,78,243,1) 25%, rgba(15,23,42,1) 50%)',
+            'radial-gradient(hsl(var(--primary) / 0.8) 0%, hsl(var(--primary) / 0.45) 25%, rgba(15,23,42,1) 50%)',
         }}
       />
       <div className="max-w-6xl mx-auto pt-10 mb-20">{children}</div>
