a8f3e256af97bc11345b04a1086e1124a3d42170
peterkogo <peter@xyflow.com>
2026-03-19 13:27:46 +0100

    debug middleware

--- packages/xy-shared/server-actions/proxy.ts
+++ packages/xy-shared/server-actions/proxy.ts
@@ -5,10 +5,27 @@ const protectedRoutes = ['/pro/dashboard', '/pro/team', '/pro/account', '/pro/su
 
 const redirectToDashboard = ['/pro/sign-in', '/pro/sign-up', '/pro/'];
 
+function isDocumentNavigation(req: NextRequest): boolean {
+  const mode = req.headers.get('sec-fetch-mode');
+  const dest = req.headers.get('sec-fetch-dest');
+
+  return mode === 'navigate' && dest === 'document';
+}
+
 export async function proxy(request: NextRequest) {
   // Handle Nhost authentication and token refresh
   // Always call this to ensure session is up-to-date
   // even for public routes, so that session changes are detected
+
+  const nextRouterPrefetch = request.headers.get('next-router-prefetch');
+  const purpose = request.headers.get('purpose');
+  const rsc = request.headers.get('rsc');
+  const _rsc = request.headers.get('_rsc');
+  const nextJsData = request.headers.get('x-nextjs-data');
+
+  console.log(isDocumentNavigation(request));
+  console.log({ nextRouterPrefetch, purpose, rsc, _rsc, nextJsData });
+
   const response = NextResponse.next();
   let session = null;
 
