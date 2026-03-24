7a8f29b903fb9b19edf405fcc63f978ba63f4e10
Alessandro <17289614+0x0f0f0f@users.noreply.github.com>
2026-03-24 12:25:09 +0100

    add zoom pan flows to svelte flow

--- apps/example-apps/svelte/learn/zoom-pan-2/App.svelte
+++ apps/example-apps/svelte/learn/zoom-pan-2/App.svelte
@@ -1,36 +1,46 @@
 <script>
-  import { SvelteFlow, Background, Controls, MiniMap } from '@xyflow/svelte';
+  import { SvelteFlow, Background, SelectionMode } from '@xyflow/svelte';
 
   import '@xyflow/svelte/dist/style.css';
 
-  const initialNodes = [
-    {
-      id: '1',
-      position: { x: 0, y: 0 },
-      data: { label: 'Hello' },
-    },
-    {
-      id: '2',
-      position: { x: 300, y: 0 },
-      data: { label: 'World' },
-    },
-  ];
+  export const initialNodes = [
+  {
+    id: '1',
+    data: { label: 'Node 1' },
+    position: { x: 150, y: 0 },
+  },
+  {
+    id: '2',
+    data: { label: 'Node 2' },
+    position: { x: 0, y: 150 },
+  },
+  {
+    id: '3',
+    data: { label: 'Node 3' },
+    position: { x: 300, y: 150 },
+  },
+];
 
-  const initialEdges = [
-    {
-      id: '1->2',
-      source: '1',
-      target: '2',
-      type: 'smoothstep',
-    },
-  ];
+export const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e1-3', source: '1', target: '3' },
+];
+
+  const panOnDrag = [1, 2];
 
   let nodes = $state.raw(initialNodes);
   let edges = $state.raw(initialEdges);
+
 </script>
 
-<SvelteFlow bind:nodes bind:edges minZoom={0} fitView colorMode="system">
+<SvelteFlow
+  bind:nodes
+  bind:edges
+  panOnScroll
+  selectionOnDrag
+  panOnDrag={panOnDrag}
+  selectionMode={SelectionMode.Partial}
+  fitView
+>
   <Background />
-  <MiniMap />
-  <Controls />
 </SvelteFlow>
