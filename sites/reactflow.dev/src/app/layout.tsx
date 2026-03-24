417d4eff21b6d7ecb4e5496e7d4dc858cd8c2d02
peterkogo <peter@xyflow.com>
2026-03-24 08:50:46 +0100

    try to fix slow sites

--- sites/reactflow.dev/src/app/layout.tsx
+++ sites/reactflow.dev/src/app/layout.tsx
@@ -13,9 +13,11 @@ import { createNormalizePageMap } from 'xy-shared/server/normalize-page-map';
 
 import { SubscriptionProvider } from 'xy-shared/components/pro/Providers';
 import { SiteNavbarContent } from 'xy-shared/components/navigation/SiteNavbar';
-import { getHasNhostSession } from 'xy-shared/lib/nhost';
+
 import './global.css';
 import { SessionRefresher } from 'xy-shared/components/session-refresher';
+
+export const revalidate = false;
 
 export const metadata = generateRootMetadata('React Flow', {
   description:
@@ -36,13 +38,12 @@ export default async function RootLayout({ children }: LayoutProps<'/'>) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { Projects, ...remainingCategories } = defaultFooterCategories;
   const normalizePageMap = createNormalizePageMap();
-  const [pageMap, lastChangelog, initialHasSession] = await Promise.all([
+  const [pageMap, lastChangelog] = await Promise.all([
     normalizePageMap().catch((e) => {
       console.error('error in normalizePageMap', e);
       return [{ data: {} }];
     }),
     getLastChangelog(),
-    getHasNhostSession(),
   ]);
 
   return (
@@ -92,7 +93,7 @@ export default async function RootLayout({ children }: LayoutProps<'/'>) {
               }
               logoLink={false}
             >
-              <SiteNavbarContent initialHasSession={initialHasSession} />
+              <SiteNavbarContent initialHasSession={false} />
             </NextraNavbar>
           }
           pageMap={pageMap}
