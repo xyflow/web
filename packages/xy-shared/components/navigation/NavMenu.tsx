ed19e87f33dc6ce01826d9384427d191acce4332
peterkogo <peter@xyflow.com>
2026-03-24 11:12:56 +0100

    fix navmenu

--- packages/xy-shared/components/navigation/NavMenu.tsx
+++ packages/xy-shared/components/navigation/NavMenu.tsx
@@ -4,6 +4,7 @@ import { createNhostClient } from '@nhost/nhost-js';
 import { CookieStorage } from '@nhost/nhost-js/session';
 import { NavMenuLoggedIn } from './NavMenuLoggedIn';
 import { NavMenuNotLoggedIn } from './NavMenuNotLoggedIn';
+import { startTransition, useEffect, useState } from 'react';
 
 const nhost = createNhostClient({
   subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN!,
@@ -14,7 +15,13 @@ const nhost = createNhostClient({
 });
 
 export function NavMenu() {
-  const hasSession = !!nhost.getUserSession();
+  const [hasSession, setHasSession] = useState(false);
+
+  useEffect(() => {
+    startTransition(() => {
+      setHasSession(!!nhost.getUserSession());
+    });
+  }, []);
 
   return hasSession ? <NavMenuLoggedIn /> : <NavMenuNotLoggedIn />;
 }
