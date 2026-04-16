cb6e5ef9294544753c754b6a22995ecafeb3286c
peterkogo <peter@xyflow.com>
2026-04-16 14:00:19 +0200

    fix account menu

--- packages/xy-shared/components/navigation/dynamic-cta-account-menu.tsx
+++ packages/xy-shared/components/navigation/dynamic-cta-account-menu.tsx
@@ -106,7 +106,6 @@ export function DynamicCTAAcountMenu() {
   }, []);
 
   const { component, width, disableOutline } = getButtonContent(session, pathname);
-
   return (
     <div
       style={{ width: `${width}px` }}
