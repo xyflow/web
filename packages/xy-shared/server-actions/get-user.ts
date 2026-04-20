7379951a3e6ccd87dd6bb95e59713264677a7500
peterkogo <peter@xyflow.com>
2026-04-20 12:44:29 +0200

    remove unused components

--- packages/xy-shared/server-actions/get-user.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import { createNhostClient } from '../lib/nhost';
-
-export async function getUser() {
-  const nhost = await createNhostClient();
-  return nhost.getUserSession()?.user;
-}
